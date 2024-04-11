7c3f3b8552d0eaa8969e980cfc1334857dbd1767
guanghechen <me.guanghechen@gmail.com>
2024-04-11 10:48:29 +0800

    fix neo-tree config

---
+++ lua/ghc/plugin-override.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+  { -- Add telescope-fzf-native
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
--- lua/ghc/plugin/nvim-window-picker/setup.lua
+++ lua/ghc/plugin/nvim-window-picker/setup.lua
@@ -2,6 +2,14 @@ local function setup()
   require("window-picker").setup({
     hint = "floating-big-letter",
     show_prompt = true,
+    filter_rules = {
+      autoselect_one = true,
+      include_current_win = false,
+      bo = {
+        filetype = { "neo-tree", "neo-tree-popup", "notify" },
+        buftype = { "terminal", "quickfix" },
+      },
+    },
   })
 end
 
