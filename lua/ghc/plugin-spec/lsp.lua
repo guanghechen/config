6e96ad627371b405ebb48338bad0e3df3bad0d92
guanghechen <me.guanghechen@gmail.com>
2024-04-12 11:23:15 +0800

    update extra config

--- lua/ghc/plugin-spec/lsp.lua
+++ lua/ghc/plugin-spec/lsp.lua
@@ -1,13 +1,4 @@
 return {
-  -- Use Eslint for fix on save and prettier for formatting
-  -- https://www.lazyvim.org/configuration/recipes#use-eslint-for-fix-on-save-and-prettier-for-formatting
-  { import = "lazyvim.plugins.extras.linting.eslint" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-
-  -- Add extra langauges support.
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-
   -- Patch lspconfig
   {
     "neovim/nvim-lspconfig",
