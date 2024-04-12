5a47a4be2efb61f95b3b3ee14278ab4387f779b6
guanghechen <me.guanghechen@gmail.com>
2024-04-12 14:37:07 +0800

    improve: tweak telescope and gitsigns

--- lua/ghc/autocmd/init.lua
+++ lua/ghc/autocmd/init.lua
@@ -1,2 +1,5 @@
 require("ghc.autocmd.filetype")
 require("ghc.autocmd.format")
+
+-- Run plugin autocmd
+require("ghc.plugin.telescope.autocmd")
---
+++ lua/ghc/plugin/telescope/autocmd.lua
@@ -0,0 +1,2 @@
+-- Set number for previewer
+vim.cmd("autocmd User TelescopePreviewerLoaded setlocal number")
