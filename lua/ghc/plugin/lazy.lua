c0acf8963957c42e0e581b870fbbafd90e0199bf
guanghechen <me.guanghechen@gmail.com>
2024-04-19 18:27:56 +0800

    refactor: make the nvchad default configs in chadrc.lua and init.lua and ghc.plugin.lazy.lua

---
+++ lua/ghc/plugin/lazy.lua
@@ -0,0 +1,68 @@
+local opts = {
+  spec = {
+    {
+      "NvChad/NvChad",
+      lazy = false,
+      branch = "v2.5",
+      import = "nvchad.plugins",
+      config = function()
+        require("ghc.option")
+      end,
+    },
+    { import = "ghc.plugin.spec" },
+  },
+  defaults = {
+    lazy = true,
+  },
+  install = {
+    colorscheme = { "nvchad" },
+  },
+  checker = {
+    enabled = false, -- set true to automatically check for plugin updates
+  },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "2html_plugin",
+        "bugreport",
+        "compiler",
+        "ftplugin",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "matchit",
+        "matchparen",
+        "netrw",
+        "netrwFileHandlers",
+        "netrwPlugin",
+        "netrwSettings",
+        "optwin",
+        "rplugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "synmenu",
+        "syntax",
+        "tar",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+      },
+    },
+  },
+  ui = {
+    icons = {
+      ft = "",
+      lazy = "󰂠 ",
+      loaded = "",
+      not_loaded = "",
+    },
+  },
+}
+
+return opts
