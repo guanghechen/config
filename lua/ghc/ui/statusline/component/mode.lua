e407c751d7490768949b7cf3de1553f17229318f
guanghechen <me.guanghechen@gmail.com>
2024-04-26 19:59:01 +0800

    improve: tweak statusline

--- lua/ghc/ui/statusline/component/mode.lua
+++ lua/ghc/ui/statusline/component/mode.lua
@@ -5,6 +5,50 @@ local M = {
   name = "ghc_statusline_mode",
 }
 
+M.modes_map = {
+  ["n"] = { "NORMAL", "normal" },
+  ["no"] = { "NORMAL (no)", "normal" },
+  ["nov"] = { "NORMAL (nov)", "normal" },
+  ["noV"] = { "NORMAL (noV)", "normal" },
+  ["noCTRL-V"] = { "NORMAL", "normal" },
+  ["niI"] = { "NORMAL i", "normal" },
+  ["niR"] = { "NORMAL r", "normal" },
+  ["niV"] = { "NORMAL v", "normal" },
+  ["nt"] = { "NTERMINAL", "nterminal" },
+  ["ntT"] = { "NTERMINAL (ntT)", "nterminal" },
+
+  ["v"] = { "VISUAL", "visual" },
+  ["vs"] = { "V-CHAR (Ctrl O)", "visual" },
+  ["V"] = { "V-LINE", "visual" },
+  ["Vs"] = { "V-LINE", "visual" },
+  [""] = { "V-BLOCK", "visual" },
+
+  ["i"] = { "INSERT", "insert" },
+  ["ic"] = { "INSERT (completion)", "insert" },
+  ["ix"] = { "INSERT completion", "insert" },
+
+  ["t"] = { "TERMINAL", "terminal" },
+
+  ["R"] = { "REPLACE", "replace" },
+  ["Rc"] = { "REPLACE (Rc)", "replace" },
+  ["Rx"] = { "REPLACEa (Rx)", "replace" },
+  ["Rv"] = { "V-REPLACE", "replace" },
+  ["Rvc"] = { "V-REPLACE (Rvc)", "replace" },
+  ["Rvx"] = { "V-REPLACE (Rvx)", "replace" },
+
+  ["s"] = { "SELECT", "select" },
+  ["S"] = { "S-LINE", "select" },
+  [""] = { "S-BLOCK", "select" },
+  ["c"] = { "COMMAND", "command" },
+  ["cv"] = { "COMMAND", "command" },
+  ["ce"] = { "COMMAND", "command" },
+  ["r"] = { "PROMPT", "confirm" },
+  ["rm"] = { "MORE", "confirm" },
+  ["r?"] = { "CONFIRM", "confirm" },
+  ["x"] = { "CONFIRM", "confirm" },
+  ["!"] = { "SHELL", "terminal" },
+}
+
 ---@param name? string
 ---@param fg? string|nil
 ---@param bg? string|nil
@@ -20,15 +64,15 @@ function M.gen_color_withmodes(name, fg, bg, bold)
     }
   end
 
-  gen_color_withmode("NORMAL", "nord_blue")
-  gen_color_withmode("VISUAL", "cyan")
-  gen_color_withmode("INSERT", "dark_purple")
-  gen_color_withmode("TERMINAL", "green")
-  gen_color_withmode("NTERMINAL", "yellow")
-  gen_color_withmode("REPLACE", "orange")
-  gen_color_withmode("CONFIRM", "teal")
-  gen_color_withmode("COMMAND", "green")
-  gen_color_withmode("SELECT", "blue")
+  gen_color_withmode("normal", "nord_blue")
+  gen_color_withmode("visual", "cyan")
+  gen_color_withmode("insert", "dark_purple")
+  gen_color_withmode("terminal", "green")
+  gen_color_withmode("nterminal", "yellow")
+  gen_color_withmode("replace", "orange")
+  gen_color_withmode("confirm", "teal")
+  gen_color_withmode("command", "vibrant_green")
+  gen_color_withmode("select", "blue")
 
   return result
 end
@@ -59,9 +103,8 @@ end
 function M.renderer_left(opts)
   local is_rightest = opts.is_rightest
   local m = vim.api.nvim_get_mode().mode
-  local modes = require("nvchad.stl.utils").modes
-  local mode_name = modes[m][2]
-  local mode_display_name = modes[m][1]
+  local mode_name = M.modes_map[m][2]
+  local mode_display_name = M.modes_map[m][1]
 
   local color_icon = "%#" .. M.name .. "_icon" .. "_" .. mode_name .. "#"
   local color_text = "%#" .. M.name .. "_text" .. "_" .. mode_name .. "#"
