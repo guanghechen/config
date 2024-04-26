7103df607152ba3d991dd94463855f35ea68020a
guanghechen <me.guanghechen@gmail.com>
2024-04-26 13:14:23 +0800

    add lazygit

---
+++ lua/ghc/keymap/git.lua
@@ -0,0 +1,20 @@
+local path = require("ghc.core.util.path")
+
+local lazygit = {
+  open = function()
+    local cmds = {
+      "cd " .. '"' .. path.workspace() .. '"',
+      "lazygit",
+    }
+
+    require("nvchad.term").toggle({
+      id = "lazygit",
+      pos = "float",
+      cmd = table.concat(cmds, " && "),
+    })
+  end,
+}
+
+if path.findGitRepoFromPath(vim.uv.cwd()) ~= nil then
+  vim.keymap.set("n", "<leader>gg", lazygit.open, { noremap = true, silent = true, desc = "Lazygit (workspace)" })
+end
