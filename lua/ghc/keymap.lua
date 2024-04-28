a78dac1e2cd2442c8e646166db61ef7ec5e8f8ca
guanghechen <me.guanghechen@gmail.com>
2024-04-28 15:26:29 +0800

    improve: polish keymaps

--- lua/ghc/keymap.lua
+++ lua/ghc/keymap.lua
@@ -42,11 +42,9 @@ vim.keymap.set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Pr
 vim.keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 vim.keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 
--- Clear search with <esc>
-vim.keymap.set("n", "<Esc>", "<cmd>noh<cr><esc>", { noremap = true, silent = true, desc = "Remove search highlights" })
+mapkey("n", "<Esc>", "<cmd>noh<cr><esc>", "Remove search highlights", true) -- Clear search with <esc>
 
--- Exit terminal
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { noremap = true, silent = true, desc = "terminal: Exit terminal mode" })
+mapkey("t", "<Esc><Esc>", "<C-\\><C-n>", "terminal: Exit terminal mode", true) -- Exit terminal
 
 -- keywordprg
 vim.keymap.set("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
@@ -76,12 +74,12 @@ mapkey("n", "[b", actions.buffer.open_buffer_left, "buffer: Open left buffer", t
 mapkey("n", "]b", actions.buffer.open_buffer_right, "buffer: Open right buffer", true)
 
 ----- diagnostic -----
-mapkey("n", "[d", actions.diagnostic.goto_prev_diagnostic, "Prev Diagnostic")
-mapkey("n", "]d", actions.diagnostic.goto_next_diagnostic, "Next Diagnostic")
-mapkey("n", "[e", actions.diagnostic.goto_prev_error, "Prev Error")
-mapkey("n", "]e", actions.diagnostic.goto_next_error, "Next Error")
-mapkey("n", "[w", actions.diagnostic.goto_prev_warn, "Prev Warning")
-mapkey("n", "]w", actions.diagnostic.goto_next_warn, "Next Warning")
+mapkey("n", "[d", actions.diagnostic.goto_prev_diagnostic, "Prev Diagnostic", true)
+mapkey("n", "]d", actions.diagnostic.goto_next_diagnostic, "Next Diagnostic", true)
+mapkey("n", "[e", actions.diagnostic.goto_prev_error, "Prev Error", true)
+mapkey("n", "]e", actions.diagnostic.goto_next_error, "Next Error", true)
+mapkey("n", "[w", actions.diagnostic.goto_prev_warn, "Prev Warning", true)
+mapkey("n", "]w", actions.diagnostic.goto_next_warn, "Next Warning", true)
 
 ----- window -----
 mapkey("n", "<leader>h", actions.window.focus_window_left, "window: Focus on the left window", true)
@@ -128,55 +126,55 @@ mapkey("n", "<leader>et", actions.explorer.toggle_explorer, "explorer: Toggle")
 ---------------------------------------------------------------------------------------#[e]xplorer--
 
 --#[f]ile-------------------------------------------------------------------------------------------
-mapkey("n", "<leader>fn", actions.file.new_file, "File: New File", false)
+mapkey("n", "<leader>fn", actions.file.new_file, "File: New File")
 -------------------------------------------------------------------------------------------#[f]ile--
 
 --#[f]ind-------------------------------------------------------------------------------------------
-mapkey("n", "<leader>fb", actions.find.find_buffers, "find: Buffers", false)
-mapkey("n", "<leader>fE", actions.find.find_explorer_workspace, "find: File explorer (from workspace)", false)
-mapkey("n", "<leader>fe", actions.find.find_explorer_current, "find: File explorer (from current directory)", false)
-mapkey("n", "<leader>fF", actions.find.find_files_workspace, "find: files (workspace)", false)
-mapkey("n", "<leader>ff", actions.find.find_files_cwd, "find: Files (cwd)", false)
-mapkey("n", "<leader>fg", actions.find.find_files_git, "find: Files (git)", false)
-mapkey("n", "<leader>fR", actions.find.find_frecency_workspace, "find: Recent (repo)", false)
-mapkey("n", "<leader>fr", actions.find.find_frecency_cwd, "find: Recent (cwd)", false)
-mapkey("n", "<leader><leader>", actions.find.find_frecency_cwd, "find: Recent (cwd)", false)
+mapkey("n", "<leader>fb", actions.find.find_buffers, "find: Buffers")
+mapkey("n", "<leader>fE", actions.find.find_explorer_workspace, "find: File explorer (from workspace)")
+mapkey("n", "<leader>fe", actions.find.find_explorer_current, "find: File explorer (from current directory)")
+mapkey("n", "<leader>fF", actions.find.find_files_workspace, "find: files (workspace)")
+mapkey("n", "<leader>ff", actions.find.find_files_cwd, "find: Files (cwd)")
+mapkey("n", "<leader>fg", actions.find.find_files_git, "find: Files (git)")
+mapkey("n", "<leader>fR", actions.find.find_frecency_workspace, "find: Recent (repo)")
+mapkey("n", "<leader>fr", actions.find.find_frecency_cwd, "find: Recent (cwd)")
+mapkey("n", "<leader><leader>", actions.find.find_frecency_cwd, "find: Recent (cwd)")
 -------------------------------------------------------------------------------------------#[f]ind--
 
 --#[g]it--------------------------------------------------------------------------------------------
-mapkey("n", "<leader>gG", actions.git.open_lazygit_workspace, "git: Open lazygit (workspace)")
-mapkey("n", "<leader>gg", actions.git.open_lazygit_cwd, "git: Open lazygit (cwd)")
-mapkey("n", "<leader>gf", actions.git.open_lazygit_file_history, "git: Open lazygit file history")
+mapkey("n", "<leader>gG", actions.git.open_lazygit_workspace, "git: Open lazygit (workspace)", true)
+mapkey("n", "<leader>gg", actions.git.open_lazygit_cwd, "git: Open lazygit (cwd)", true)
+mapkey("n", "<leader>gf", actions.git.open_lazygit_file_history, "git: Open lazygit file history", true)
 -------------------------------------------------------------------------------------------#[g]it---
 
 --#[q]uit-------------------------------------------------------------------------------------------
-mapkey("n", "<leader>qq", actions.quit.quit_all, "quit: Quit all")
+mapkey("n", "<leader>qq", actions.quit.quit_all, "quit: Quit all", true)
 -------------------------------------------------------------------------------------------#[q]uit--
 
 --#[s]earch-----------------------------------------------------------------------------------------
-mapkey("n", "<leader>sG", actions.search.live_grep_with_args_workspace, "search: Grep (workspace)", false)
-mapkey("n", "<leader>sg", actions.search.live_grep_with_args_cwd, "search: Grep (cwd)", false)
+mapkey("n", "<leader>sG", actions.search.live_grep_with_args_workspace, "search: Grep (workspace)")
+mapkey("n", "<leader>sg", actions.search.live_grep_with_args_cwd, "search: Grep (cwd)")
 -----------------------------------------------------------------------------------------#[s]earch--
 
 --#[t]ab--------------------------------------------------------------------------------------------
-mapkey("n", "<leader>t1", actions.tab.goto_tab_1, "tab: Goto tab 1")
-mapkey("n", "<leader>t2", actions.tab.goto_tab_2, "tab: Goto tab 2")
-mapkey("n", "<leader>t3", actions.tab.goto_tab_3, "tab: Goto tab 3")
-mapkey("n", "<leader>t4", actions.tab.goto_tab_4, "tab: Goto tab 4")
-mapkey("n", "<leader>t5", actions.tab.goto_tab_5, "tab: Goto tab 5")
-mapkey("n", "<leader>t6", actions.tab.goto_tab_6, "tab: Goto tab 6")
-mapkey("n", "<leader>t7", actions.tab.goto_tab_7, "tab: Goto tab 7")
-mapkey("n", "<leader>t8", actions.tab.goto_tab_8, "tab: Goto tab 8")
-mapkey("n", "<leader>t9", actions.tab.goto_tab_9, "tab: Goto tab 9")
-mapkey("n", "<leader>t0", actions.tab.goto_tab_10, "tab: Goto tab 10")
-mapkey("n", "<leader>t[", actions.tab.goto_tab_left, "tab: Goto left tab")
-mapkey("n", "<leader>t]", actions.tab.goto_tab_right, "tab: Goto right tab")
-mapkey("n", "<leader>tN", actions.tab.open_tab_new, "tab: New tab")
-mapkey("n", "<leader>tn", actions.tab.open_tab_new_with_current_buf, "tab: New tab with current buf")
-mapkey("n", "<leader>td", actions.tab.close_tab_current, "tab: Close current")
-mapkey("n", "<leader>th", actions.tab.close_tab_to_leftest, "tab: Close tabs to the leftest")
-mapkey("n", "<leader>tl", actions.tab.close_tab_to_rightest, "tab: Close tabs to the rightest")
-mapkey("n", "<leader>to", actions.tab.close_tab_others, "tab: Close other tabs")
+mapkey("n", "<leader>t1", actions.tab.goto_tab_1, "tab: Goto tab 1", true)
+mapkey("n", "<leader>t2", actions.tab.goto_tab_2, "tab: Goto tab 2", true)
+mapkey("n", "<leader>t3", actions.tab.goto_tab_3, "tab: Goto tab 3", true)
+mapkey("n", "<leader>t4", actions.tab.goto_tab_4, "tab: Goto tab 4", true)
+mapkey("n", "<leader>t5", actions.tab.goto_tab_5, "tab: Goto tab 5", true)
+mapkey("n", "<leader>t6", actions.tab.goto_tab_6, "tab: Goto tab 6", true)
+mapkey("n", "<leader>t7", actions.tab.goto_tab_7, "tab: Goto tab 7", true)
+mapkey("n", "<leader>t8", actions.tab.goto_tab_8, "tab: Goto tab 8", true)
+mapkey("n", "<leader>t9", actions.tab.goto_tab_9, "tab: Goto tab 9", true)
+mapkey("n", "<leader>t0", actions.tab.goto_tab_10, "tab: Goto tab 10", true)
+mapkey("n", "<leader>t[", actions.tab.goto_tab_left, "tab: Goto left tab", true)
+mapkey("n", "<leader>t]", actions.tab.goto_tab_right, "tab: Goto right tab", true)
+mapkey("n", "<leader>tN", actions.tab.open_tab_new, "tab: New tab", true)
+mapkey("n", "<leader>tn", actions.tab.open_tab_new_with_current_buf, "tab: New tab with current buf", true)
+mapkey("n", "<leader>td", actions.tab.close_tab_current, "tab: Close current", true)
+mapkey("n", "<leader>th", actions.tab.close_tab_to_leftest, "tab: Close tabs to the leftest", true)
+mapkey("n", "<leader>tl", actions.tab.close_tab_to_rightest, "tab: Close tabs to the rightest", true)
+mapkey("n", "<leader>to", actions.tab.close_tab_others, "tab: Close other tabs", true)
 --------------------------------------------------------------------------------------------#[t]ab--
 
 --#[t]merinal---------------------------------------------------------------------------------------
@@ -190,21 +188,21 @@ mapkey("n", "<leader>un", actions.ui.dismiss_notifications, "ui: Dismiss all not
 ---------------------------------------------------------------------------------------------#[u]i--
 
 --#[w]indow-----------------------------------------------------------------------------------------
-mapkey("n", "<leader>wh", actions.window.focus_window_left, "window: Focus on the left window")
-mapkey("n", "<leader>wj", actions.window.focus_window_bottom, "window: Focus on the bottom window")
-mapkey("n", "<leader>wk", actions.window.focus_window_top, "window: Focus on the top window")
-mapkey("n", "<leader>wl", actions.window.focus_window_right, "window: Focus on the right window")
-mapkey("n", "<leader>ww", actions.window.focus_window_with_picker, "window: Focus window (with picker)")
-mapkey("n", "<leader>ws", actions.window.swap_window_with_picker, "window: Swap window (with picker)")
-mapkey("n", "<leader>wp", actions.window.project_window_with_picker, "window: Project window (with picker)")
-mapkey("n", "<leader>w-", actions.window.split_window_horizontal, "window: Split window horizontally")
-mapkey("n", "<leader>w|", actions.window.split_window_vertical, "window: Split window vertically")
-mapkey("n", "<leader>wJ", actions.window.split_window_horizontal, "window: Split window horizontally")
-mapkey("n", "<leader>wL", actions.window.split_window_vertical, "window: Split window vertically")
-mapkey("n", "<leader>w<Left>", actions.window.resize_window_vertical_minus, "window: Resize -(v:count) vertically.")
-mapkey("n", "<leader>w<Down>", actions.window.resize_window_horizontal_minus, "window: Resize -(v:count) horizontally.")
-mapkey("n", "<leader>w<Up>", actions.window.resize_window_horizontal_plus, "window: Resize +(v:count) horizontally.")
-mapkey("n", "<leader>w<Right>", actions.window.resize_window_vertical_plus, "window: Resize +(v:count) vertically.")
-mapkey("n", "<leader>wd", actions.window.close_window_current, "window: close current window")
-mapkey("n", "<leader>wo", actions.window.close_window_others, "window: close others")
+mapkey("n", "<leader>wh", actions.window.focus_window_left, "window: Focus on the left window", true)
+mapkey("n", "<leader>wj", actions.window.focus_window_bottom, "window: Focus on the bottom window", true)
+mapkey("n", "<leader>wk", actions.window.focus_window_top, "window: Focus on the top window", true)
+mapkey("n", "<leader>wl", actions.window.focus_window_right, "window: Focus on the right window", true)
+mapkey("n", "<leader>ww", actions.window.focus_window_with_picker, "window: Focus window (with picker)", true)
+mapkey("n", "<leader>ws", actions.window.swap_window_with_picker, "window: Swap window (with picker)", true)
+mapkey("n", "<leader>wp", actions.window.project_window_with_picker, "window: Project window (with picker)", true)
+mapkey("n", "<leader>w-", actions.window.split_window_horizontal, "window: Split window horizontally", true)
+mapkey("n", "<leader>w|", actions.window.split_window_vertical, "window: Split window vertically", true)
+mapkey("n", "<leader>wJ", actions.window.split_window_horizontal, "window: Split window horizontally", true)
+mapkey("n", "<leader>wL", actions.window.split_window_vertical, "window: Split window vertically", true)
+mapkey("n", "<leader>w<Left>", actions.window.resize_window_vertical_minus, "window: Resize -(v:count) vertically.", true)
+mapkey("n", "<leader>w<Down>", actions.window.resize_window_horizontal_minus, "window: Resize -(v:count) horizontally.", true)
+mapkey("n", "<leader>w<Up>", actions.window.resize_window_horizontal_plus, "window: Resize +(v:count) horizontally.", true)
+mapkey("n", "<leader>w<Right>", actions.window.resize_window_vertical_plus, "window: Resize +(v:count) vertically.", true)
+mapkey("n", "<leader>wd", actions.window.close_window_current, "window: close current window", true)
+mapkey("n", "<leader>wo", actions.window.close_window_others, "window: close others", true)
 -----------------------------------------------------------------------------------------#[w]indow--
