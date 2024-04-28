601dc452aab8f4a271671d99aa7715c44f1ddff2
guanghechen <me.guanghechen@gmail.com>
2024-04-28 15:08:16 +0800

    improve: add dressing.nvim for better ui

--- lua/ghc/spec/ui-fancy.lua
+++ lua/ghc/spec/ui-fancy.lua
@@ -1,3 +1,8 @@
+---@class ghc.spec.common.setting
+local setting = {
+  icons = require("ghc.core.setting.icons"),
+}
+
 return {
   -- Active indent guide and indent text objects. When you're browsing
   -- code, this highlights the current level of indentation, and animates
@@ -68,6 +73,56 @@ return {
           }),
         },
       }
+    end,
+  },
+
+  -- better vim.ui input/select
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
+
+  -- Better `vim.notify()`
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      stages = "static",
+      timeout = 3000,
+      fps = 20,
+      level = "INFO",
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      on_open = function(win)
+        vim.api.nvim_set_option_value("winblend", 0, { scope = "local", win = win })
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+      icons = {
+        ERROR = setting.icons.diagnostics.Error,
+        WARN = setting.icons.diagnostics.Warning,
+        INFO = setting.icons.diagnostics.Information,
+        DEBUG = setting.icons.ui.Bug,
+        TRACE = setting.icons.ui.Pencil,
+      },
+    },
+    config = function(_, opts)
+      require("notify").setup(opts)
+      vim.notify = require("notify")
     end,
   },
 }
