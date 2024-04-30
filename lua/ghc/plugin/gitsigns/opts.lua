91d13a39ba8d13e7192f3e37a97e9263e9659690
guanghechen <me.guanghechen@gmail.com>
2024-04-30 16:34:02 +0800

    improve: tweak gitsigns config and keymap

--- lua/ghc/plugin/gitsigns/opts.lua
+++ lua/ghc/plugin/gitsigns/opts.lua
@@ -1,28 +1,76 @@
 local opts = {
   current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+  },
+  current_line_blame_formatter = "    <author>, <author_time:%Y-%m-%d %H:%M:%S> - <summary>",
+  current_line_blame_formatter_opts = {
+    relative_time = false,
+  },
+  signcolumn = true,
+  numhl = true,
+  linehl = false,
+  word_diff = false,
+  max_file_length = 3000, -- Disable if file is longer than this (in lines)
   signs = {
-    add = { text = "│" },
-    change = { text = "│" },
-    delete = { text = "󰍵" },
+    add = { text = "┃" },
+    change = { text = "┃" },
+    delete = { text = "_" },
     topdelete = { text = "‾" },
     changedelete = { text = "~" },
-    untracked = { text = "│" },
+    untracked = { text = "┆" },
   },
   on_attach = function(buffer)
-    local gs = package.loaded.gitsigns
+    ---@param mode string
+    ---@param key string
+    ---@param action any
+    ---@param desc string
+    ---@param silent? boolean
+    local function mapkey(mode, key, action, desc, silent)
+      vim.keymap.set(mode, key, action, { buffer = buffer, noremap = true, silent = silent ~= nil and silent or false, desc = desc })
+    end
 
-    vim.keymap.set("n", "]h", gs.next_hunk, { buffer = buffer, desc = "Next Hunk" })
-    vim.keymap.set("n", "[h", gs.prev_hunk, { buffer = buffer, desc = "Prev Hunk" })
-    vim.keymap.set({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<cr>", { buffer = buffer, desc = "Stage Hunk" })
-    vim.keymap.set({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<cr>", { buffer = buffer, desc = "Reset Hunk" })
-    vim.keymap.set("n", "<leader>ghS", gs.stage_buffer, { buffer = buffer, desc = "Stage Buffer" })
-    vim.keymap.set("n", "<leader>ghu", gs.undo_stage_hunk, { buffer = buffer, desc = "Undo Stage Hunk" })
-    vim.keymap.set("n", "<leader>ghR", gs.reset_buffer, { buffer = buffer, desc = "Reset Buffer" })
-    vim.keymap.set("n", "<leader>ghp", gs.preview_hunk_inline, { buffer = buffer, desc = "Preview Hunk Inline" })
-    vim.keymap.set("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, { buffer = buffer, desc = "Blame Line" })
-    vim.keymap.set("n", "<leader>ghd", gs.diffthis, { buffer = buffer, desc = "Diff This" })
-    vim.keymap.set("n", "<leader>ghD", function() gs.diffthis("~") end, { buffer = buffer, desc = "Diff This ~" })
-    vim.keymap.set({ "o", "x" }, "<leader>ghi", ":<C-U>Gitsigns select_hunk<cr>", { buffer = buffer, desc = "GitSigns Select Hunk"})
+    local gitsigns = require("gitsigns")
+
+    ---@class ghc.plugin.gitsigns.opts.action
+    local action = {
+      goto_prev_hunk = function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gitsigns.nav_hunk("prev")
+        end
+      end,
+      goto_next_hunk = function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gitsigns.nav_hunk("next")
+        end
+      end,
+      blame_line = function()
+        gitsigns.blame_line({ full = true })
+      end,
+      diff_current_file = function()
+        gitsigns.diffthis("~")
+      end,
+      preview_hunk_inline = function()
+        gitsigns.preview_hunk_inline()
+      end,
+      undo_stage_hunk = function()
+        gitsigns.undo_stage_hunk()
+      end,
+    }
+
+    mapkey("n", "[h", action.goto_prev_hunk, "git: Prev hunk", true)
+    mapkey("n", "]h", action.goto_next_hunk, "git: Next hunk", true)
+    mapkey("n", "<leader>gb", action.blame_line, "git: Preview hunk inline", true)
+    mapkey("n", "<leader>gd", action.diff_current_file, "git: Diff current file", true)
+    mapkey("n", "<leader>gp", action.preview_hunk_inline, "git: Preview hunk inline", true)
   end,
 }
 
