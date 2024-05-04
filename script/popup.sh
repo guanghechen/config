579079053506d1785ddd62ff91ec62e8cf04ca32
guanghechen <me.guanghechen@gmail.com>
2024-05-04 14:27:25 +0800

    improve: tweak popup session

--- script/popup.sh
+++ script/popup.sh
@@ -1,24 +1,25 @@
 #! /usr/bin/env bash
 
 function _ghc_tmux_popup_ {
-  local popup_session_name="_popup"
-  local popup_window_name=$(tmux display-message -p -F "#{window_name}")
-  local popup_pane_path=$(tmux display-message -p -F "#{pane_current_path}")
+  local current_session_name=$(tmux display-message -p -F "#{session_name}")
+  local current_window_name=$(tmux display-message -p -F "#{window_name}")
+  local current_pane_path=$(tmux display-message -p -F "#{pane_current_path}")
+  local popup_session_name="_popup@${current_session_name}"
+  local popup_window_name="${current_window_name}"
 
-  if [ "$(tmux display-message -p -F "#{session_name}")" = "${popup_session_name}" ];then
+  if [[ "${current_session_name}" = _popup@* ]];then
     tmux detach-client
   else
     tmux has-session -t "${popup_session_name}" 2> /dev/null
 
     if [ $? != 0 ]; then
-      tmux new-session -d -s "${popup_session_name}"
-      tmux new-window -t "${popup_session_name}" -n "${popup_window_name}" -c "${popup_pane_path}"
+      tmux new-session -d -s "${popup_session_name}" -n "${popup_window_name}" -c "${current_pane_path}"
     else
 
       tmux select-window -t "${popup_session_name}:${popup_window_name}" 2> /dev/null
 
       if [ $? != 0 ]; then
-        tmux new-window -t "${popup_session_name}" -n "${popup_window_name}" -c "${popup_pane_path}"
+        tmux new-window -t "${popup_session_name}" -n "${popup_window_name}" -c "${current_pane_path}"
         tmux select-window -t "${popup_session_name}:${popup_window_name}"
       fi
     fi
