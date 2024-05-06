5061582ef0afdbebc90cf0db6fac7bce6189a724
guanghechen <me.guanghechen@gmail.com>
2024-05-06 23:26:08 +0800

    tweak plugin spec

--- lua/ghc/spec/code.lua
+++ lua/ghc/spec/code.lua
@@ -36,13 +36,13 @@ return {
     },
     opts = {
       mappings = {
-        add = "gsa",
-        delete = "gsd",
-        find = "gsf",
-        find_left = "gsF",
-        highlight = "gsh",
-        replace = "gsr",
-        update_n_lines = "gsn",
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
       },
     },
   },
