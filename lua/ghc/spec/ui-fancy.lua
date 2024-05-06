57e759c69ae417acee3969c0b8fc90dcba6a38c5
guanghechen <me.guanghechen@gmail.com>
2024-05-06 23:02:37 +0800

    improve: use mirror

--- lua/ghc/spec/ui-fancy.lua
+++ lua/ghc/spec/ui-fancy.lua
@@ -103,7 +103,10 @@ return {
   },
 
   {
-    "alexghergh/nvim-tmux-navigation",
+    "guanghechen/mirror",
+    name = "nvim-tmux-navigation",
+    main = "nvim-tmux-navigation",
+    branch = "nvim@nvim-tmux-navigation", -- "alexghergh/nvim-tmux-navigation",
     lazy = false,
     opts = {
       disable_when_zoomed = true,
