a44c1fe80b94579a82724d4a8891ae88de1672d0
guanghechen <me.guanghechen@gmail.com>
2024-05-13 16:51:37 +0800

    tweak

--- lua/ghc/ui/statusline/component/find_recent.lua
+++ lua/ghc/ui/statusline/component/find_recent.lua
@@ -37,15 +37,16 @@ function M.renderer()
   ---@type ghc.core.types.enum.SEARCH_SCOPE
   local scope = context.repo.find_recent_scope:get_snapshot()
 
-  ---@type boolean
-  local enable_case_sensitive = context.repo.find_recent_enable_case_sensitive:get_snapshot()
-
+  -- ---@type boolean
+  -- local enable_case_sensitive = context.repo.find_recent_enable_case_sensitive:get_snapshot()
+  --
   local color_scope = "%#" .. M.name .. "_flag_scope#"
-  local color_enable_case_sensitive = enable_case_sensitive and "%#" .. M.name .. "_flag_enabled#" or "%#" .. M.name .. "_flag#"
+  -- local color_enable_case_sensitive = enable_case_sensitive and "%#" .. M.name .. "_flag_enabled#" or "%#" .. M.name .. "_flag#"
   local text_scope = " " .. scope .. " "
-  local text_enable_ignore_case = "  "
-
-  return color_scope .. text_scope .. color_enable_case_sensitive .. text_enable_ignore_case
+  -- local text_enable_ignore_case = "  "
+  --
+  -- return color_scope .. text_scope .. color_enable_case_sensitive .. text_enable_ignore_case
+  return color_scope .. text_scope
 end
 
 return M
