ffb9caa373d26c573f2fcbb55294f44e6af52948
guanghechen <me.guanghechen@gmail.com>
2024-05-17 20:34:07 +0800

    improve: tweak

--- lua/ghc/plugin/noice.lua
+++ lua/ghc/plugin/noice.lua
@@ -26,7 +26,7 @@ return {
       bottom_search = true,
       command_palette = true,
       long_message_to_split = true,
-      inc_rename = true,
+      inc_rename = false,
     },
   },
   -- stylua: ignore
