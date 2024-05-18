2725c0ebe9193244da416110bc0a36ca67e74e31
guanghechen <me.guanghechen@gmail.com>
2024-05-18 11:15:56 +0800

    improve: tweak which-key

--- lua/ghc/plugin/which-key.lua
+++ lua/ghc/plugin/which-key.lua
@@ -31,7 +31,7 @@ return {
     dofile(vim.g.base46_cache .. "whichkey")
 
     vim.o.timeout = true
-    vim.o.timeoutlen = 300
+    vim.o.timeoutlen = 700
 
     require("which-key").setup(opts)
     require("which-key").register(opts.defaults)
