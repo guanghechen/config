bf9fe03f2937c2976cc1cb3bb68cde878fc02897
guanghechen <me.guanghechen@gmail.com>
2024-05-22 16:56:30 +0800

    improve: tweak config

--- init.lua
+++ init.lua
@@ -1,4 +1,19 @@
 local load = {
+  bootstrap = function()
+    vim.g.mapleader = " "
+    vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
+
+    -- bootstrap lazy and all plugins
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.uv.fs_stat(lazypath) then
+      local repo = "https://github.com/folke/lazy.nvim.git"
+      vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    -- load plugins
+    require("lazy").setup(require("ghc.plugin.lazy"))
+  end,
   autocmd = function()
     require("ghc.autocmd")
 
@@ -26,28 +41,15 @@ local load = {
 
     pcall(require, "local.option")
   end,
+  theme = function()
+    dofile(vim.g.base46_cache .. "defaults")
+    dofile(vim.g.base46_cache .. "statusline")
+  end,
 }
 
-vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
-vim.g.mapleader = " "
-
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
+load.bootstrap()
 load.option()
-
--- load plugins
-require("lazy").setup(require("ghc.plugin.lazy"))
-
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
+load.theme()
 load.autocmd()
 
 vim.schedule(function()
