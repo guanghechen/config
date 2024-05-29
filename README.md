e08a3f959363e4f28e04edbdf057cf56fa898965
guanghechen <me.guanghechen@gmail.com>
2024-05-29 15:55:08 +0800

    improve: add fake-clipboard

--- README.md
+++ README.md
@@ -9,6 +9,20 @@ https://github.com/tmux-plugins/tmux-yank
     brew install reattach-to-user-namespace
     ```
 
+  - For use clipboard between different users in same mac machine
+
+    1. Install fswatch 
+
+        ```bash
+        brew install fswatch
+        ```
+    
+    2. Watch a fake file change 
+      
+        ```bash
+        bash script/fake-clipboard.sh /tmp/fake-clipboard.txt
+        ```
+
 ## Lazy git
 
 https://github.com/jesseduffield/lazygit
---
+++ script/fake-clipboard.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+function _ghc_watch_fake_clipboard {
+	local _fake_clipboard_file=$1
+	local _fake_clipboard_dir=$(dirname "$_fake_clipboard_file")
+
+	# Check if the directory exists
+	if [ ! -d "$_fake_clipboard_dir" ]; then
+		mkdir -p "$_fake_clipboard_dir"
+	fi
+
+	if [ ! -f "$_fake_clipboard_file" ]; then
+		touch "$_fake_clipboard_file"
+	fi
+
+	echo "Watching fake clipboard: $_fake_clipboard_file"
+
+	# Watch the fake clipboard change and write the content into system clipboard
+	fswatch -0 $_fake_clipboard_file | xargs -0 -n1 sh -c "cat $_fake_clipboard_file | pbcopy"
+}
+
+_ghc_watch_fake_clipboard "$1"
