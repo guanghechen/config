174c38038bfe5b71fed6fcfe0e10c93cdd28ea87
guanghechen <me.guanghechen@gmail.com>
2024-06-23 17:57:32 +0800

    improve: tweak window picker

--- lua/fml/api/window.lua
+++ lua/fml/api/window.lua
@@ -3,11 +3,12 @@ local M = {}
 
 ---@param ignored_buftypes string[]
 ---@param ignored_filetypes string[]
----@return integer
-function M.find_any_window_choose(ignored_buftypes, ignored_filetypes)
+---@return integer[]
+function M.list_all_other_aviable_windows(ignored_buftypes, ignored_filetypes)
   local current_tabpage = vim.api.nvim_get_current_tabpage()
   local winnrs = vim.api.nvim_tabpage_list_wins(current_tabpage)
   local winnr_current = vim.api.nvim_get_current_win()
+  local results = {} ---@type integer[]
 
   for _, winnr in ipairs(winnrs) do
     if winnr ~= winnr_current then
@@ -15,11 +16,11 @@ function M.find_any_window_choose(ignored_buftypes, ignored_filetypes)
       local buftype = vim.api.nvim_get_option_value("buftype", { buf = bufnr })
       local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
       if not vim.tbl_contains(ignored_buftypes, buftype) and not vim.tbl_contains(ignored_filetypes, filetype) then
-        return bufnr
+        table.insert(results, bufnr)
       end
     end
   end
-  return 0
+  return results
 end
 
 ---@param winnr number
@@ -52,11 +53,11 @@ function M.pick_window(opts)
     }
   end
 
-  local fast_found_winnr = M.find_any_window_choose(bo.buftype, bo.filetype)
-  if fast_found_winnr > 0 then
+  local all_other_windows = M.list_all_other_aviable_windows(bo.buftype, bo.filetype)
+  if #all_other_windows > 0 then
     local ok, window_picker = pcall(require, "window-picker")
     if not ok then
-      return fast_found_winnr
+      return all_other_windows[1]
     end
 
     return window_picker.pick_window({
