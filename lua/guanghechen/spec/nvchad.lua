65678ffa8c3db3b22a6230a204cc22025e115279
guanghechen <me.guanghechen@gmail.com>
2024-06-26 14:00:47 +0800

    improve: don't depend on nvchad directly

--- lua/guanghechen/spec/nvchad.lua
+++ lua/guanghechen/spec/nvchad.lua
@@ -4,7 +4,46 @@ return {
     branch = "v2.5",
     lazy = false,
     config = function()
-      require("nvchad")
+      ---require("nvchad")
+
+      local config = require("nvconfig").ui
+      vim.o.statusline = "%!v:lua.require('nvchad.stl." .. config.statusline.theme .. "')()"
+
+      if config.tabufline.enabled then
+        require("nvchad.tabufline.lazyload")
+      end
+
+      vim.api.nvim_create_autocmd("LspProgress", {
+        callback = function(args)
+          if string.find(args.match, "end") then
+            vim.cmd("redrawstatus")
+          end
+          vim.cmd("redrawstatus")
+        end,
+      })
+
+      vim.schedule(function()
+        vim.api.nvim_create_autocmd("BufWritePost", {
+          pattern = vim.tbl_map(function(path)
+            return vim.fs.normalize(vim.loop.fs_realpath(path))
+          end, vim.fn.glob(vim.fn.stdpath("config") .. "/lua/**/*.lua", true, true, true)),
+          group = vim.api.nvim_create_augroup("ReloadNvChad", {}),
+          callback = function(opts)
+            local fp = vim.fn.fnamemodify(vim.fs.normalize(vim.api.nvim_buf_get_name(opts.buf)), ":r") --[[@as string]]
+            local app_name = vim.env.NVIM_APPNAME and vim.env.NVIM_APPNAME or "nvim"
+            local module = string.gsub(fp, "^.*/" .. app_name .. "/lua/", ""):gsub("/", ".")
+
+            require("plenary.reload").reload_module("nvconfig")
+            require("plenary.reload").reload_module("chadrc")
+            require("plenary.reload").reload_module("base46")
+            require("plenary.reload").reload_module("nvchad")
+            require("plenary.reload").reload_module(module)
+
+            require("nvchad")
+            require("base46").load_all_highlights()
+          end,
+        })
+      end)
     end,
   },
   {
