d8bcb425096fbe7b83eb3286cb648fc463ea2d31
guanghechen <me.guanghechen@gmail.com>
2024-07-07 18:23:29 +0800

    improve: update sessionoptions

--- lua/fml/constant.lua
+++ lua/fml/constant.lua
@@ -2,8 +2,8 @@
 local M = {}
 
 M.BUF_UNTITLED = "untitled"
-M.SESSION_SAVE_OPTION = table.concat({ "blank", "buffers", "curdir", "folds", "help", "resize", "tabpages", "winpos", "winsize" }, ",")
-M.SESSION_AUTOSAVE_OPTION = table.concat({ "blank", "buffers", "curdir", "folds", "help", "resize", "tabpages", "unix", "winpos", "winsize" }, ",")
+M.SESSION_SAVE_OPTION = "buffers,curdir,folds,help,resize,tabpages,unix,winpos,winsize"
+M.SESSION_AUTOSAVE_OPTION = "buffers,curdir,folds,help,resize,tabpages,unix,winpos,winsize"
 M.TAB_UNNAMED = "unnamed"
 M.TAB_HISTORY_CAPACITY = 100
 M.WIN_HISTORY_CAPACITY = 100
