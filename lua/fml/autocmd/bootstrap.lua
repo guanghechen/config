f1572950690fb02f27b93c3c5ab713b511de349a
guanghechen <me.guanghechen@gmail.com>
2024-07-23 16:32:01 +0800

    improve: tweak comments

--- lua/fml/autocmd/bootstrap.lua
+++ lua/fml/autocmd/bootstrap.lua
@@ -11,16 +11,14 @@ local augroups = {
 }
 
 -- Auto cd the directory:
--- 1. the opend file is under a git repo, let's remember the the git repo path as A, and assume the
---    git repo directory of the shell cwd is B.
---
---    a) If A is different from B, then auto cd the A.
---    b) If A is the same as B, then no action needed.
+-- 1. the opend file is under a git repo, let's remember the the git repo path as A,
+--    and assume the git repo directory of the shell cwd is B.
+--      a) If A is different from B, then auto cd the A.
+--      b) If A is the same as B, then no action needed.
 -- 2. the opened file is not under a git repo, then auto cd the directory of the opened file.
 if vim.fn.expand("%") ~= "" then
   local cwd = vim.fn.getcwd()
   local p = vim.fn.expand("%:p:h")
-
   local A = path.locate_git_repo(p)
   local B = path.locate_git_repo(cwd)
 
