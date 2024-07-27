094ed2fae2179a60bfb057d21897f2150d0780e1
guanghechen <me.guanghechen@gmail.com>
2024-07-27 12:30:13 +0800

    improve: tweak keymap

--- lua/ghc/command/refresh.lua
+++ lua/ghc/command/refresh.lua
@@ -4,7 +4,11 @@ local M = {}
 ---@return nil
 function M.refresh_all()
   vim.cmd("checktime")
+  fml.api.state.refresh_all()
+
+  vim.cmd("LspRestart")
   vim.cmd("redraw")
+
   fml.reporter.info({
     from = "ghc.command.refresh",
     subject = "refresh_all",
