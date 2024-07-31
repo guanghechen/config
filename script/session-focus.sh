a55fe6b641f16c90c3f3b0fe3177933dbab5dcce
guanghechen <me.guanghechen@gmail.com>
2024-07-31 11:09:24 +0800

    improve: select prev/next session group by whether if current session is a popup session

--- script/session-focus.sh
+++ script/session-focus.sh
@@ -3,16 +3,20 @@
 # Get the direction from the command line argument (prev or next)
 direction=$1
 
-# Get the list of sessions, excluding those that start with "_popup"
-sessions=$(tmux list-sessions -F "#{session_name}" | grep -v "^_popup@")
-
 # Get the name of the current session
-current_session=$(tmux display-message -p '#S')
+current_session_name=$(tmux display-message -p '#S')
+
+# Get the list of sessions, excluding those that start with "_popup"
+if [[ "${current_session_name}" == _popup@* ]]; then
+  sessions=$(tmux list-sessions -F "#{session_name}" | grep "^_popup@")
+else
+  sessions=$(tmux list-sessions -F "#{session_name}" | grep -v "^_popup@")
+fi
 
 # Find the index of the current session in the list of sessions
 index=0
 for session in $sessions; do
-  if [ "$session" == "$current_session" ]; then
+  if [ "$session" == "$current_session_name" ]; then
     break
   fi
   index=$((index + 1))
@@ -29,9 +33,9 @@ else
 fi
 
 # Get the name of the target session
-target_session=$(echo "$sessions" | sed -n "$((target_index + 1))p")
+target_session_name=$(echo "$sessions" | sed -n "$((target_index + 1))p")
 
 # Switch to the target session
-if [ "${current_session}" != "${target_session}" ]; then
-  tmux switch-client -t "${target_session}"
+if [ "${current_session_name}" != "${target_session_name}" ]; then
+  tmux switch-client -t "${target_session_name}"
 fi
