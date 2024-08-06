986e1c5212c10f30e8644bad03da012ea53617ac
guanghechen <me.guanghechen@gmail.com>
2024-08-06 11:37:35 +0800

    chore: disable auto toggle relative number

--- lua/ghc/autocmd/bootstrap.lua
+++ lua/ghc/autocmd/bootstrap.lua
@@ -11,32 +11,6 @@ fml.disposable:add_disposable(fml.collection.Disposable.new({
   end,
 }))
 
----! Auto toggle realtive linenumber.
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-  pattern = "*",
-  group = augroups.toggle_relative,
-  callback = function()
-    if vim.o.nu and vim.api.nvim_get_mode().mode == "n" then
-      if ghc.context.client.relativenumber:snapshot() then
-        vim.opt.relativenumber = true
-      end
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-  pattern = "*",
-  group = augroups.toggle_relative,
-  callback = function()
-    if vim.o.nu then
-      vim.opt.relativenumber = false
-      vim.schedule(function()
-        vim.cmd("redraw")
-      end)
-    end
-  end,
-})
-
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = "*",
   group = augroups.redraw_when_mode,
@@ -46,3 +20,30 @@ vim.api.nvim_create_autocmd("ModeChanged", {
     end)
   end,
 })
+
+-- ---! Auto toggle realtive linenumber.
+-- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+--   pattern = "*",
+--   group = augroups.toggle_relative,
+--   callback = function()
+--     if vim.o.nu and vim.api.nvim_get_mode().mode == "n" then
+--       if ghc.context.client.relativenumber:snapshot() then
+--         vim.opt.relativenumber = true
+--       end
+--     end
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+--   pattern = "*",
+--   group = augroups.toggle_relative,
+--   callback = function()
+--     if vim.o.nu then
+--       vim.opt.relativenumber = false
+--       vim.schedule(function()
+--         vim.cmd("redraw")
+--       end)
+--     end
+--   end,
+-- })
+--
