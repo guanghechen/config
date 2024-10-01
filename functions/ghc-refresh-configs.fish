a8010f19d0c1b5100c4da742de69826af35e7bfb
guanghechen <me.guanghechen@gmail.com>
2024-10-01 11:41:57 +0800

    improve: add ghc-refresh-configs

---
+++ functions/ghc-refresh-configs.fish
@@ -0,0 +1,34 @@
+function ghc-refresh-configs
+  set primary_configs alacritty btop fish fzf guanghechen helix lazygit nvim ripgrep tmux
+  set develop_configs nvim-nvchad
+  set base_path "$HOME/.config"
+
+  for config in $primary_configs
+    set dir "$base_path/$config"
+    if test -d "$dir"
+      set cmd "cd $dir && git pull origin $config"
+    else
+      set cmd "git clone https://github.com/guanghechen/config.git --single-branch --branch=$config $dir"
+    end
+
+    set_color $fish_color_gray
+    printf "\n%s\n" $cmd
+
+    set_color normal
+    fish -c "$cmd"
+  end
+
+  for config in $develop_configs
+    set dir "$base_path/$config"
+    if test -d "$dir"
+      set cmd "cd $dir && git pull origin $config"
+
+      set_color $fish_color_gray
+      printf "\n%s\n" $cmd
+
+      set_color normal
+      fish -c "$cmd"
+    end
+  end
+end
+
