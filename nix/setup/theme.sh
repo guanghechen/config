ecaffb18057d3deaee13104ea5cd94e6cb59a3f0
guanghechen <me.guanghechen@gmail.com>
2024-10-30 11:05:24 +0800

    improve: fix setup/theme

--- nix/setup/theme.sh
+++ nix/setup/theme.sh
@@ -4,4 +4,4 @@ printf "\n\e[34m  [setup config] gen themes...\e[0m\n"
 fish -c "node ~/.config/guanghechen/config/theme/gen_themes.mjs"
 
 printf "\n\e[34m  [setup config] set default theme...\e[0m\n"
-fish -c "node ~/.config/guanghechen/config/theme/toggle_theme.mjs one_half_dark"
+fish -c "node ~/.config/guanghechen/config/theme/apply_theme.mjs one_half_dark"
