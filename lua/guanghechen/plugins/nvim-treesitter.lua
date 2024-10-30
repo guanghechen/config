53218f01261d16a5d6d1e0d4785108b3397e4b8a
guanghechen <me.guanghechen@gmail.com>
2024-10-30 18:02:27 +0800

    improve(treesitter): disable when the file big than 300KB

--- lua/guanghechen/plugins/nvim-treesitter.lua
+++ lua/guanghechen/plugins/nvim-treesitter.lua
@@ -32,7 +32,7 @@ return {
       additional_vim_regex_highlighting = false,
       ---@diagnostic disable-next-line: unused-local
       disable = function(lang, buf)
-        local max_filesize = 100 * 1024 * 1024 -- 100 MB
+        local max_filesize = 300 * 1024 -- 300 KB
         local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
