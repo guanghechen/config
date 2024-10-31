ebcecc9b3360a788d414bae5f873397ec578d998
guanghechen <me.guanghechen@gmail.com>
2024-10-31 11:16:06 +0800

    improve: tweak theme

--- conf.d/theme.fish
+++ conf.d/theme.fish
@@ -1,59 +1,41 @@
-if test -f "$HOME/.config/fish/local/theme.fish"
-  source "$HOME/.config/fish/local/theme.fish"
-else
-  source ~/.config/fish/theme/gruvbox_light.fish
-end
+# Use below command to show all available terminal colors.
+#
+# set_color --print-colors
+#
 
-set -gx black                                    $color_bg0
-set -gx red                                      $color_neutral_red
-set -gx green                                    $color_neutral_green
-set -gx yellow                                   $color_neutral_yellow
-set -gx blue                                     $color_neutral_blue
-set -gx magenta                                  $color_neutral_purple
-set -gx cyan                                     $color_neutral_aqua
-set -gx white                                    $color_fg4
-set -gx brblack                                  $color_bg4
-set -gx brred                                    $color_red
-set -gx brgreen                                  $color_green
-set -gx bryellow                                 $color_yellow
-set -gx brblue                                   $color_blue
-set -gx brmagenta                                $color_purple
-set -gx brcyan                                   $color_aqua
-set -gx brwhite                                  $color_fg
 
-set -gx fish_color_autosuggestion                $color_fg4
-set -gx fish_color_command                       $color_blue
-set -gx fish_color_comment                       $color_fg4
-set -gx fish_color_cancel                        --reverse
-set -gx fish_color_date                          $color_fg4
-set -gx fish_color_end                           $color_orange
-set -gx fish_color_error                         $color_neutral_red
-set -gx fish_color_escape                        $color_aqua
-set -gx fish_color_gray                          $color_fg4
-set -gx fish_color_history_current               --bold
-set -gx fish_color_host_remote                   ''
-set -gx fish_color_keyword                       ''
-set -gx fish_color_operator                      $color_yellow
-set -gx fish_color_option                        ''
-set -gx fish_color_quote                         $color_green
-set -gx fish_color_match                         $color_orange
-set -gx fish_color_normal                        $color_fg
-set -gx fish_color_param                         $color_aqua
-set -gx fish_color_redirection                   $color_purple
-set -gx fish_color_search_match                  --background=$color_yellow
-set -gx fish_color_search_selection              --background=$color_yellow
-set -gx fish_color_status                        $color_red
-set -gx fish_color_valid_path                    --underline
-set -gx fish_pager_color_background              ''
-set -gx fish_pager_color_completion              normal
-set -gx fish_pager_color_description             $fg3
-set -gx fish_pager_color_prefix                  normal --bold --underline
-set -gx fish_pager_color_progress                brwhite --background=cyan
-set -gx fish_pager_color_secondary_background    ''
-set -gx fish_pager_color_secondary_completion    ''
-set -gx fish_pager_color_secondary_description   ''
-set -gx fish_pager_color_secondary_prefix        ''
-set -gx fish_pager_color_selected_background     --background=$color_yellow
-set -gx fish_pager_color_selected_completion     ''
-set -gx fish_pager_color_selected_description    ''
-set -gx fish_pager_color_selected_prefix         ''
+set -U  fish_color_autosuggestion                (set_color white)
+set -U  fish_color_command                       (set_color brblue)
+set -U  fish_color_comment                       (set_color white)
+set -U  fish_color_cancel                        (set_color --reverse)
+set -U  fish_color_end                           (set_color bryellow)
+set -U  fish_color_error                         (set_color red)
+set -U  fish_color_escape                        (set_color brcyan)
+set -U  fish_color_gray                          (set_color white)
+set -U  fish_color_history_current               (set_color --bold)
+set -U  fish_color_host_remote                   (set_color normal)
+set -U  fish_color_keyword                       (set_color normal)
+set -U  fish_color_operator                      (set_color bryellow)
+set -U  fish_color_option                        (set_color normal)
+set -U  fish_color_quote                         (set_color brgreen)
+set -U  fish_color_match                         (set_color bryellow)
+set -U  fish_color_normal                        (set_color brwhite)
+set -U  fish_color_param                         (set_color brcyan)
+set -U  fish_color_redirection                   (set_color brmagenta)
+set -U  fish_color_search_match                  (set_color --background=bryellow)
+set -U  fish_color_search_selection              (set_color --background=bryellow)
+set -U  fish_color_status                        (set_color brred)
+set -U  fish_color_valid_path                    (set_color --underline)
+set -U  fish_pager_color_background              (set_color normal)
+set -U  fish_pager_color_completion              (set_color normal)
+set -U  fish_pager_color_description             (set_color white)
+set -U  fish_pager_color_prefix                  (set_color normal --bold --underline)
+set -U  fish_pager_color_progress                (set_color brwhite --background=cyan)
+set -U  fish_pager_color_secondary_background    (set_color normal)
+set -U  fish_pager_color_secondary_completion    (set_color normal)
+set -U  fish_pager_color_secondary_description   (set_color normal)
+set -U  fish_pager_color_secondary_prefix        (set_color normal)
+set -U  fish_pager_color_selected_background     (set_color --background=bryellow)
+set -U  fish_pager_color_selected_completion     (set_color normal)
+set -U  fish_pager_color_selected_description    (set_color normal)
+set -U  fish_pager_color_selected_prefix         (set_color normal)
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,76 +1,68 @@
+## Change the git prompt style
+## See https://fishshell.com/docs/current/cmds/fish_git_prompt.html
+#
+set -g __fish_git_prompt_char_cleanstate          ''
+set -g __fish_git_prompt_char_dirtystate          '●'
+set -g __fish_git_prompt_char_invalidstate        '✗'
+set -g __fish_git_prompt_char_stagedstate         '+'
+set -g __fish_git_prompt_char_untrackedfiles      '?'
+set -g __fish_git_prompt_color_branch             magenta   --bold
+set -g __fish_git_prompt_color_cleanstate         green     --bold
+set -g __fish_git_prompt_color_dirtystate         yellow
+set -g __fish_git_prompt_color_invalidstate       red
+set -g __fish_git_prompt_color_stagedstate        blue
+set -g __fish_git_prompt_color_untrackedfiles     normal
+set -g __fish_git_prompt_show_informative_status  true
+set -g __fish_git_prompt_showdirtystate           true
+set -g __fish_git_prompt_showstashstate           true
+set -g __fish_git_prompt_showuntrackedfiles       true
+set -g __fish_git_prompt_showupstream             informative
+
 function fish_prompt --description 'Write out the prompt'
-  source ~/.config/fish/conf.d/theme.fish
-
-  ## Change the git prompt style
-  ## See https://fishshell.com/docs/current/cmds/fish_git_prompt.html
-  set -g __fish_git_prompt_char_cleanstate          ''
-  set -g __fish_git_prompt_char_dirtystate          '●'
-  set -g __fish_git_prompt_char_invalidstate        '✗'
-  set -g __fish_git_prompt_char_stagedstate         '+'
-  set -g __fish_git_prompt_char_untrackedfiles      '?'
-  set -g __fish_git_prompt_color_branch             magenta   --bold
-  set -g __fish_git_prompt_color_cleanstate         green     --bold
-  set -g __fish_git_prompt_color_dirtystate         yellow
-  set -g __fish_git_prompt_color_invalidstate       red
-  set -g __fish_git_prompt_color_stagedstate        blue
-  set -g __fish_git_prompt_color_untrackedfiles     $fish_color_normal
-  set -g __fish_git_prompt_show_informative_status  true
-  set -g __fish_git_prompt_showdirtystate           true
-  set -g __fish_git_prompt_showstashstate           true
-  set -g __fish_git_prompt_showuntrackedfiles       true
-  set -g __fish_git_prompt_showupstream             informative
-
   set -l last_pipestatus $pipestatus
   set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
 
-  set -l color_cwd
   set -l suffix
   set -l user_separator
   if functions -q fish_is_root_user; and fish_is_root_user
-    if set -q fish_color_cwd_root
-      set color_cwd $color_red
-    else
-      set color_cwd $color_blue
-    end
-    set suffix '#'
-    set user_separator '#'
+    set suffix          '#'
+    set user_separator  '#'
   else
-    set color_cwd $color_blue
-    set suffix '$'
-    set user_separator '@'
+    set suffix          '$'
+    set user_separator  '@'
   end
 
   # PWD
   set -l pwd $PWD
   if not test -w $pwd
-    set_color $color_red
+    set_color $brred
     echo -n ' '
   end
 
-  set_color $color_red
+  set_color brred --bold
   echo -n (whoami)
 
-  set_color $color_aqua
+  set_color brcyan
   echo -n $user_separator
 
-  set_color $color_fg
+  set_color brwhite
   printf "%s " (hostname)
 
   set -l pwd (string replace -r "^$HOME" "~" $pwd)
-  set_color $color_cwd
-  echo -n  $pwd
+  set_color blue
+  echo -n $pwd
 
-  set_color $color_fg
+  set_color brwhite
   printf '%s ' (fish_vcs_prompt)
 
-  set -l status_color (set_color $fish_color_status)
-  set -l statusb_color (set_color --bold $fish_color_status)
+  set -l status_color (set_color brred)
+  set -l statusb_color (set_color brred --bold)
   set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
   echo -n $prompt_status
 
   # current time
   set -l current_time (date "+%H:%M:%S")
-  set_color $fish_color_date
+  set_color white
   printf '%s ' $current_time
   set_color normal
 
