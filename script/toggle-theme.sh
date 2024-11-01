cc99eb812d3e0f94b37dc7c2ef6b6523a316d0a5
guanghechen <me.guanghechen@gmail.com>
2024-11-01 13:48:04 +0800

    improve: tweak tmux theme

--- script/toggle-theme.sh
+++ script/toggle-theme.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env bash
 
 function _ghc_tmux_toggle_theme_ {
-	local current_theme=$(tmux show-environment -g TMUX_THEME 2>/dev/null | cut -d '=' -f 2)
-	if [ "$current_theme" == "LIGHTEN" ]; then
-		tmux set-environment -g TMUX_THEME DARKEN
-	elif [ "$current_theme" == "DARKEN" ]; then
-		tmux set-environment -g TMUX_THEME LIGHTEN
-	else
-		tmux set-environment -g TMUX_THEME DARKEN
-	fi
-	tmux source-file ~/.config/tmux/tmux.conf
+  local current_theme=$(tmux show-environment -g @GHC_TMUX_THEME 2>/dev/null | cut -d '=' -f 2)
+  local current_mode=$(echo $current_theme | rg -o '_([a-zA-Z0-9]+)$' -r '$1')
+
+  local next_theme=$current_theme
+  if [ "$current_mode" == "light" ]; then
+    next_theme=$(echo $current_theme | rg '_light$' -r '_dark')
+  elif [ "$current_mode" == "dark" ]; then
+    next_theme=$(echo $current_theme | rg '_dark$' -r '_light')
+  fi
+
+  node $HOME/.config/guanghechen/config/theme/apply_theme.mjs "$next_theme"
 }
 
 _ghc_tmux_toggle_theme_
