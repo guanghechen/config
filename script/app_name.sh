8a1a026d67125e2852fa32ae4e35c69f014f19aa
guanghechen <me.guanghechen@gmail.com>
2024-11-01 13:36:14 +0800

    improve: show exact the app name

---
+++ script/app_name.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function _ghc_tmux_app_name_ {
+  local pane_tty=$1
+  local app_name=$(ps -o comm= -t "${pane_tty}" | rg -o '[\w-]+' | tail -1)
+
+  if [ "${app_name}" = "nvim" ]; then
+    local app_pid=$(ps -o pid= -t "${pane_tty}" | rg -o '\d+' | tail -1)
+    local nvim_app_name=$(cat "/proc/${app_pid}/environ" 2>/dev/null | rg -ao 'NVIM_APPNAME=nvim-([\w-]+)' -r '$1')
+  fi
+
+  if [ -n "${nvim_app_name}" ]; then
+    echo "${nvim_app_name}"
+  else
+    echo "${app_name}"
+  fi
+}
+
+_ghc_tmux_app_name_ "$1"
