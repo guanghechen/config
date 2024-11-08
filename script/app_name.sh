2b875b660a65d51b0dd47dac87e771ba9f52eb6d
guanghechen <me.guanghechen@gmail.com>
2024-11-08 11:34:34 +0800

    improve: avoid unncessary api call

--- script/app_name.sh
+++ script/app_name.sh
@@ -4,9 +4,11 @@ function _ghc_tmux_app_name_ {
   local pane_tty=$1
   local app_name=$(ps -o comm= -t "${pane_tty}" | rg -o '[\w-]+' | tail -1)
 
-  if [ "${app_name}" = "nvim" ]; then
-    local app_pid=$(ps -o pid= -t "${pane_tty}" | rg -o '\d+' | tail -1)
-    local nvim_app_name=$(cat "/proc/${app_pid}/environ" 2>/dev/null | rg -ao 'NVIM_APPNAME=nvim-([\w-]+)' -r '$1')
+  if [ -d "/proc" ]; then
+    if [ "${app_name}" = "nvim" ]; then
+      local app_pid=$(ps -o pid= -t "${pane_tty}" | rg -o '\d+' | tail -1)
+      local nvim_app_name=$(cat "/proc/${app_pid}/environ" 2>/dev/null | rg -ao 'NVIM_APPNAME=nvim-([\w-]+)' -r '$1')
+    fi
   fi
 
   if [ -n "${nvim_app_name}" ]; then
