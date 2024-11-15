d9ba0361c8a3764d3cdafc8bbe08ca7f9c2673d0
guanghechen <me.guanghechen@gmail.com>
2024-11-15 11:30:42 +0800

    improve: only install node when it's not installed

--- nix/setup/node.sh
+++ nix/setup/node.sh
@@ -2,5 +2,9 @@
 
 source $HOME/.config/guanghechen/nix/setup/path.sh
 
-fnm install 20
-fish -c "npm install -g npm yarn"
+if fnm list | grep -q "v20"; then
+  printf "\n\e[33;5;214m  [setup node] node@20 is already installed. (skipped)\e[0m\n"
+else
+  fnm install 20
+  fish -c "npm install -g npm yarn"
+fi
