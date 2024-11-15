3b97730f94ecfec67ee07f3228130f6dad6c44c7
guanghechen <me.guanghechen@gmail.com>
2024-11-15 18:37:41 +0800

    improve: update nvim config

--- nix/setup/nvim.sh
+++ nix/setup/nvim.sh
@@ -2,7 +2,17 @@
 
 source $HOME/.config/guanghechen/nix/setup/path.sh
 
-fish -c "cd $HOME/.config/nvim/rust/nvim_tools/ && bash build.sh && nvim --headless -c 'source ~/.config/nvim/init.lua' +q"
+fish -c "\
+  cd $HOME/.config/nvim\
+  && bash rust/nvim_tools/build.sh\
+  && nvim --headless -c 'source ~/.config/nvim/init.lua' +q\
+  && nvim --headless -c 'source ~/.config/nvim/init.lua | Lazy update' +q\
+"
 if [ -d "$HOME/.config/nvim-nvchad/" ]; then
-  fish -c "cd $HOME/.config/nvim-nvchad/rust/nvim_tools/ && bash build.sh && nvchad --headless -c 'source ~/.config/nvim-nvchad/init.lua' +q"
+  fish -c "\
+    cd $HOME/.config/nvim-nvchad\
+    && bash rust/nvim_tools/build.sh\
+    && nvchad --headless -c 'source ~/.config/nvim-nvchad/init.lua' +q\
+    && nvchad --headless -c 'source ~/.config/nvim-nvchad/init.lua | Lazy update' +q\
+  "
 fi
