39b2f10cb936e168e1341b0d17e894a93f6565a9
guanghechen <me.guanghechen@gmail.com>
2024-11-18 15:35:31 +0800

    improve: tweak foldexpr

--- lua/fml/fn/foldexpr.lua
+++ lua/fml/fn/foldexpr.lua
@@ -1,41 +1,21 @@
-local skip_check = assert(vim.uv.new_check())
-local skip_foldexpr = {} ---@type table<number,boolean>
-
 ---@return string
 local function foldexpr()
   local bufnr = vim.api.nvim_get_current_buf() ---@type integer
-
-  -- still in the same tick and no parser
-  if skip_foldexpr[bufnr] then
-    return "0"
-  end
 
   -- don't use treesitter folds for terminal
   if vim.bo[bufnr].buftype == "terminal" then
     return "0"
   end
 
-  -- as long as we don't have a filetype, don't bother
-  -- checking if treesitter is available (it won't)
   if vim.bo[bufnr].filetype == "" then
     return "0"
   end
 
-  local ok = pcall(vim.treesitter.get_parser, bufnr)
-
-  if ok then
-    return vim.treesitter.foldexpr()
+  if vim.b[bufnr].ts_folds == nil then
+    vim.b[bufnr].ts_folds = pcall(vim.treesitter.get_parser, bufnr)
   end
 
-  -- no parser available, so mark it as skip
-  -- in the next tick, all skip marks will be reset
-  skip_foldexpr[bufnr] = true
-  skip_check:start(function()
-    skip_foldexpr = {}
-    skip_check:stop()
-  end)
-
-  return "0"
+  return vim.b[bufnr].ts_folds and vim.treesitter.foldexpr() or "0"
 end
 
 return foldexpr
