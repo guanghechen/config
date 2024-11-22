1c3fa623d3dbaac639a9e16ce045466ad4f7ec26
guanghechen <me.guanghechen@gmail.com>
2024-11-22 09:09:15 +0800

    improve: apply/gen theme in parallel for lower latency

--- config/theme/apply_theme.mjs
+++ config/theme/apply_theme.mjs
@@ -1,4 +1,5 @@
-import fs from "node:fs";
+import { existsSync, mkdirSync } from "node:fs";
+import fs from "node:fs/promises";
 import path from "node:path";
 import {
   HOME_CONFIG,
@@ -10,40 +11,40 @@ import {
 
 const theme = process.argv[2]?.toLowerCase();
 if (themes.includes(theme)) {
-  await apply_theme(theme);
+  const tasks = apps.map((app) => apply_theme_per_app(theme, app));
+  await Promise.allSettled(tasks);
 } else {
   console.error("[toggle_theme] Cannot find the given theme:", theme);
 }
 
 /**
- * @param {string} theme
+ * @param {string}      theme
+ * @param {IAppConfig}  app
  * @return {Promise<void>}
  */
-async function apply_theme(theme) {
-  for (const app of apps) {
-    if (!app.active(app)) continue;
+async function apply_theme_per_app(theme, app) {
+  if (!app.active(app)) return;
 
-    if (app.local) {
-      const template_filepath = path.join(HOME_THEME_APP, `${app.name}.hbs`);
-      if (!fs.existsSync(template_filepath)) {
-        console.error("[gen_theme] Cannot find the template.", { app });
-        continue;
-      }
-      const template = fs.readFileSync(template_filepath, "utf8");
-
-      const scheme_filepath = path.join(HOME_THEME_SCHEME, `${theme}.json`);
-      if (!fs.existsSync(scheme_filepath)) {
-        console.error("[gen_theme] Cannot find the scheme.", { app, theme });
-        continue;
-      }
-      const scheme = fs.readFileSync(scheme_filepath, "utf8");
-      const content = app.render(app, template, scheme);
-
-      const theme_filepath = path.resolve(HOME_CONFIG, app.name, app.local);
-      fs.mkdirSync(path.dirname(theme_filepath), { recursive: true });
-      fs.writeFileSync(theme_filepath, content, "utf8");
+  if (app.local) {
+    const template_filepath = path.join(HOME_THEME_APP, `${app.name}.hbs`);
+    if (!existsSync(template_filepath)) {
+      console.error("[gen_theme] Cannot find the template.", { app });
+      return;
     }
+    const template = await fs.readFile(template_filepath, "utf8");
 
-    await app.after_apply?.(app, theme);
+    const scheme_filepath = path.join(HOME_THEME_SCHEME, `${theme}.json`);
+    if (!existsSync(scheme_filepath)) {
+      console.error("[gen_theme] Cannot find the scheme.", { app, theme });
+      return;
+    }
+    const scheme = await fs.readFile(scheme_filepath, "utf8");
+    const content = app.render(app, template, scheme);
+
+    const theme_filepath = path.resolve(HOME_CONFIG, app.name, app.local);
+    mkdirSync(path.dirname(theme_filepath), { recursive: true });
+    await fs.writeFile(theme_filepath, content, "utf8");
   }
+
+  await app.after_apply?.(app, theme);
 }
--- config/theme/gen_themes.mjs
+++ config/theme/gen_themes.mjs
@@ -1,4 +1,5 @@
-import fs from "node:fs";
+import { existsSync, mkdirSync } from "node:fs";
+import fs from "node:fs/promises";
 import path from "node:path";
 import {
   HOME_CONFIG,
@@ -8,28 +9,43 @@ import {
   themes,
 } from "./util.mjs";
 
-for (const app of apps) {
-  if (!app.active(app) || !app.themes) continue;
+const tasks = apps.map((app) => gen_themes_per_app(app));
+await Promise.allSettled(tasks);
+
+/**
+ * @param {IAppConfig}  app
+ * @return {Promise<void>}
+ */
+
+async function gen_themes_per_app(app) {
+  if (!app.active(app) || !app.themes) return;
 
   const template_filepath = path.join(HOME_THEME_APP, `${app.name}.hbs`);
-  if (!fs.existsSync(template_filepath)) {
+  if (!existsSync(template_filepath)) {
     console.error("[gen_theme] Cannot find the template.", { app });
-    continue;
+    return;
   }
-  const template = fs.readFileSync(template_filepath, "utf8");
+  const template = await fs.readFile(template_filepath, "utf8");
 
-  const theme_home = path.join(HOME_CONFIG, app.name, app.themes);
-  for (const theme of themes) {
+  const THEME_HOME = path.join(HOME_CONFIG, app.name, app.themes);
+  const tasks_gen_theme = themes.map((theme) => gen_theme(theme));
+  await Promise.allSettled(tasks_gen_theme);
+
+  /**
+   * @param {string}      theme
+   * @return {Promise<void>}
+   */
+  async function gen_theme(theme) {
     const scheme_filepath = path.join(HOME_THEME_SCHEME, `${theme}.json`);
-    if (!fs.existsSync(scheme_filepath)) {
+    if (!existsSync(scheme_filepath)) {
       console.error("[gen_theme] Cannot find the scheme.", { app, theme });
-      continue;
+      return;
     }
-    const scheme = fs.readFileSync(scheme_filepath, "utf8");
+    const scheme = await fs.readFile(scheme_filepath, "utf8");
     const content = app.render(app, template, scheme);
 
-    const theme_filepath = path.resolve(theme_home, `${theme}${app.extname}`);
-    fs.mkdirSync(path.dirname(theme_filepath), { recursive: true });
-    fs.writeFileSync(theme_filepath, content, "utf8");
+    const theme_filepath = path.resolve(THEME_HOME, `${theme}${app.extname}`);
+    mkdirSync(path.dirname(theme_filepath), { recursive: true });
+    await fs.writeFile(theme_filepath, content, "utf8");
   }
 }
