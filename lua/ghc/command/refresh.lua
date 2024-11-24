4d99a7454c48fef72a7acfbf842de3a2721ff028
guanghechen <me.guanghechen@gmail.com>
2024-11-24 10:56:17 +0800

    improve: tweak refresh_all command

--- lua/ghc/command/refresh.lua
+++ lua/ghc/command/refresh.lua
@@ -4,19 +4,31 @@ eve.commander.register({
   uuid = uuids.refresh_all,
   desc = "refresh: all",
   action = function()
+    local bufnr = vim.api.nvim_get_current_buf() ---@type integer
+    local devmode = eve.context.state.flight.devmode:snapshot() ---@type boolean
+
     vim.cmd.checktime()
     fml.fn.refresh_state()
 
-    vim.cmd("LspRestart")
-    vim.cmd.redraw()
-
-    local devmode = eve.context.state.flight.devmode:snapshot() ---@type boolean
-    if devmode then
-      eve.commander.execute(eve.commander.uuids.reload_theme, "force")
-    end
-
     pcall(function()
       require("gitsigns").refresh()
+    end)
+
+    pcall(function()
+      if vim.treesitter then
+        local parser = vim.treesitter.get_parser(bufnr)
+        if parser ~= nil then
+          parser:invalidate()
+        end
+      end
+    end)
+
+    vim.schedule(function()
+      if devmode then
+        eve.commander.execute(eve.commander.uuids.reload_theme, "force")
+      end
+      vim.cmd("LspRestart")
+      vim.cmd.redraw()
     end)
 
     eve.reporter.info({
