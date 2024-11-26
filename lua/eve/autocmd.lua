a1d8d2401841b18c2ecd3b05b08f3a1d1238d0cf
guanghechen <me.guanghechen@gmail.com>
2024-11-26 20:46:16 +0800

    improve: only auto set to english input method from insert mode to normal mode

--- lua/eve/autocmd.lua
+++ lua/eve/autocmd.lua
@@ -13,17 +13,11 @@ end
 if std_os.is_mac() then
   local im = require("eve.std.im")
   local previous_mode = nil ---@type t.eve.e.VimMode|nil
-  local previous_input_method = nil ---@type t.eve.e.InputMethod|nil
   vim.api.nvim_create_autocmd({ "ModeChanged" }, {
     callback = function()
       local current_mode = vim.fn.mode() ---@type t.eve.e.VimMode|nil
       if previous_mode == "i" and current_mode == "n" then
-        previous_input_method = im.get_input_method() ---@type t.eve.e.InputMethod|nil
         im.set_input_method("English")
-      elseif previous_mode == "n" and current_mode == "i" then
-        if previous_input_method ~= nil then
-          im.set_input_method(previous_input_method)
-        end
       end
       previous_mode = current_mode
     end,
