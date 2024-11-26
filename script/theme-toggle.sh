82126625e057f520743bc494a101d2bfb92ff1a5
guanghechen <me.guanghechen@gmail.com>
2024-11-26 10:57:48 +0800

    improve: tweak tmux themes

--- script/theme-toggle.sh
+++ script/theme-toggle.sh
@@ -1,17 +1,11 @@
 #! /usr/bin/env bash
 
 function _ghc_tmux_theme_toggle_ {
-  local current_theme=$(tmux show -gqv @GHC_TMUX_THEME)
-  local current_mode=$(echo $current_theme | rg -o '_([a-zA-Z0-9]+)$' -r '$1')
-
-  local next_theme=$current_theme
-  if [ "$current_mode" == "light" ]; then
-    next_theme=$(echo $current_theme | rg '_light$' -r '_dark')
-  elif [ "$current_mode" == "dark" ]; then
-    next_theme=$(echo $current_theme | rg '_dark$' -r '_light')
+  local theme=$(tmux show -gqv @GHC_TMUX_THEME)
+  if [ -z "$theme" ]; then
+    theme="gruvbox_light"
   fi
-
-  node $HOME/.config/guanghechen/config/theme/apply_theme.mjs "$next_theme"
+  node $HOME/.config/guanghechen/config/theme/toggle_theme.mjs "$theme"
 }
 
 _ghc_tmux_theme_toggle_
