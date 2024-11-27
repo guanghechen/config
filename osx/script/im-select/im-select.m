9a2edc957c0cdca151dfbaa42def5aff5acc007c
guanghechen <me.guanghechen@gmail.com>
2024-11-27 10:24:53 +0800

    improve(im-select): avoid to set the input-source when the new value is same with current one

--- osx/script/im-select/im-select.m
+++ osx/script/im-select/im-select.m
@@ -28,23 +28,27 @@
 int main(int argc, const char * argv[]) {
     int ret = 0;
     @autoreleasepool {
+        TISInputSourceRef currentInputSource = TISCopyCurrentKeyboardInputSource();
+        NSString *sourceId = (__bridge NSString *)(TISGetInputSourceProperty(currentInputSource, kTISPropertyInputSourceID));
+
         if (argc > 1) {
             NSString *inputSource = [NSString stringWithUTF8String:argv[1]];
-            NSDictionary *filter = [NSDictionary dictionaryWithObject:inputSource forKey:(NSString *)kTISPropertyInputSourceID];
-            CFArrayRef keyboards = TISCreateInputSourceList((__bridge CFDictionaryRef)filter, false);
-            if (keyboards) {
-                TISInputSourceRef selected = (TISInputSourceRef)CFArrayGetValueAtIndex(keyboards, 0);
-                ret = TISSelectInputSource(selected);
-                CFRelease(keyboards);
-            } else {
-                ret = 1;
+            if (![inputSource isEqualToString:sourceId]) {
+                NSDictionary *filter = [NSDictionary dictionaryWithObject:inputSource forKey:(NSString *)kTISPropertyInputSourceID];
+                CFArrayRef keyboards = TISCreateInputSourceList((__bridge CFDictionaryRef)filter, false);
+                if (keyboards) {
+                    TISInputSourceRef selected = (TISInputSourceRef)CFArrayGetValueAtIndex(keyboards, 0);
+                    ret = TISSelectInputSource(selected);
+                    CFRelease(keyboards);
+                } else {
+                    ret = 1;
+                }
             }
         } else {
-            TISInputSourceRef currentInputSource = TISCopyCurrentKeyboardInputSource();
-            NSString *sourceId = (__bridge NSString *)(TISGetInputSourceProperty(currentInputSource, kTISPropertyInputSourceID));
-            printf("%s\n", [sourceId UTF8String]);
-            CFRelease(currentInputSource);
+          printf("%s\n", [sourceId UTF8String]);
         }
+
+        CFRelease(currentInputSource);
     }
     return ret;
 }
