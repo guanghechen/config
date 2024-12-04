e81654c94c539db95333789977bb3833aff5d491
guanghechen <me.guanghechen@gmail.com>
2024-12-04 15:55:07 +0800

    make code robust

--- lua/eve/builtin/checks.lua
+++ lua/eve/builtin/checks.lua
@@ -30,10 +30,10 @@ local TEXT_FILENAME_SET = {
 ---@class eve.builtin.checks
 local M = {}
 
----@param bufnr                         integer
+---@param bufnr                         integer|nil
 ---@return boolean
 function M.is_buf_valid(bufnr)
-  if bufnr < 1 or not vim.api.nvim_buf_is_valid(bufnr) then
+  if bufnr == nil or bufnr < 1 or not vim.api.nvim_buf_is_valid(bufnr) then
     return false
   end
 
@@ -45,30 +45,39 @@ function M.is_buf_valid(bufnr)
   return ft.is_plain_file(filetype)
 end
 
----@param tabnr                         integer
+---@param tabnr                         integer|nil
 ---@return boolean
 function M.is_tab_valid(tabnr)
-  if tabnr < 1 or not vim.api.nvim_tabpage_is_valid(tabnr) then
+  if tabnr == nil or tabnr < 1 or not vim.api.nvim_tabpage_is_valid(tabnr) then
     return false
   end
 
   return true
 end
 
----@param winnr                         integer
+---@param winnr                         integer|nil
 ---@return boolean
 function M.is_win_floating(winnr)
+  if winnr == nil or winnr < 1 or not vim.api.nvim_win_is_valid(winnr) then
+    return false
+  end
   local config = vim.api.nvim_win_get_config(winnr) ---@type vim.api.keyset.win_config
   return config.relative ~= nil and config.relative ~= ""
 end
 
----@param winnr                         integer
+---@param winnr                         integer|nil
 ---@return boolean
 function M.is_win_valid(winnr)
-  if winnr < 1 or not vim.api.nvim_win_is_valid(winnr) then
+  if winnr == nil or winnr < 1 or not vim.api.nvim_win_is_valid(winnr) then
     return false
   end
-  return not M.is_win_floating(winnr)
+
+  local config = vim.api.nvim_win_get_config(winnr) ---@type vim.api.keyset.win_config
+  if config.relative ~= nil and config.relative ~= "" then
+    return false
+  end
+
+  return true
 end
 
 ---@param value                         any
