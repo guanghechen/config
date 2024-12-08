7f1f45ce5e640a2fb628aade5ff7f7a172d85a7c
guanghechen <me.guanghechen@gmail.com>
2024-12-08 18:55:25 +0800

    improve: format codes

--- lua/fml/fn/foldexpr.lua
+++ lua/fml/fn/foldexpr.lua
@@ -2,19 +2,19 @@
 local function foldexpr()
   local bufnr = vim.api.nvim_get_current_buf() ---@type integer
 
-  -- don't use treesitter folds for terminal
-  if vim.bo[bufnr].buftype == "terminal" then
+  -- as long as we don't have a filetype, don't bother checking if treesitter is available (it won't)
+  if vim.bo[bufnr].filetype == "" then
     return "0"
   end
 
-  if vim.bo[bufnr].filetype == "" then
+  -- don't use treesitter folds for terminal
+  if vim.bo[bufnr].buftype == "terminal" then
     return "0"
   end
 
   if vim.b[bufnr].ts_folds == nil then
     vim.b[bufnr].ts_folds = pcall(vim.treesitter.get_parser, bufnr)
   end
-
   return vim.b[bufnr].ts_folds and vim.treesitter.foldexpr() or "0"
 end
 
