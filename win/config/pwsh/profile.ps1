f8cd1ffb6d260c261ab15131672bb44a5e377434
guanghechen <me.guanghechen@gmail.com>
2024-12-09 04:06:05 +0800

    improve: tweak powershell profile

--- win/config/pwsh/profile.ps1
+++ win/config/pwsh/profile.ps1
@@ -18,25 +18,14 @@ fnm env --use-on-cd --shell power-shell | Out-String | Invoke-Expression
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 function prompt {
-  Write-Host
-  Write-Host "  " -nonewline -foregroundcolor Blue
-  Write-Host "`e[1m$env:USERNAME" -nonewline -foregroundcolor Red
-  Write-Host "@" -nonewline -foregroundcolor Cyan
-  Write-Host "`e[1m$env:COMPUTERNAME" -nonewline -foregroundcolor White
-  Write-Host " `e[1m$PWD" -nonewline -foregroundcolor DarkBlue
-  Write-Host " " -nonewline
-
+  $gitBranch = ""
+  $addedCount = 0
+  $modifiedCount = 0
+  $untrackedCount = 0
   if (Test-Path ".git") {
     try {
       $gitBranch = git rev-parse --abbrev-ref HEAD 2>$null
-      Write-Host "(" -nonewline -foregroundcolor White
-      Write-Host "`e[1m$gitBranch" -nonewline -foregroundcolor Magenta
-      Write-Host "|" -nonewline -foregroundcolor White
-
       $gitStatus = git status --porcelain 2>$null
-      $addedCount = 0
-      $modifiedCount = 0
-      $untrackedCount = 0
       foreach ($line in $gitStatus) {
         $line_status = $line.TrimStart()
         if ($line_status.StartsWith("A")) {
@@ -49,22 +38,34 @@ function prompt {
           $untrackedCount++
         }
       }
-
-      if ($addedCount -gt 0) {
-        Write-Host "+$addedCount" -nonewline -foregroundcolor DarkBlue
-      }
-      if ($modifiedCount -gt 0) {
-        Write-Host "✗$modifiedCount" -nonewline -foregroundcolor Yellow
-      }
-      if ($untrackedCount -gt 0) {
-        Write-Host "?$untrackedCount" -nonewline -foregroundcolor White
-      }
-      if ($addedCount -eq 0 -and $modifiedCount -eq 0 -and $untrackedCount -eq 0) {
-        Write-Host "" -nonewline -foregroundcolor Green
-      }
-
-      Write-Host ") " -nonewline -foregroundcolor White
     } catch {}
+  }
+
+  Write-Host
+  Write-Host "  " -nonewline -foregroundcolor Blue
+  Write-Host "`e[1m$env:USERNAME" -nonewline -foregroundcolor Red
+  Write-Host "@" -nonewline -foregroundcolor Cyan
+  Write-Host "`e[1m$env:COMPUTERNAME" -nonewline -foregroundcolor White
+  Write-Host " `e[1m$PWD" -nonewline -foregroundcolor DarkBlue
+  Write-Host " " -nonewline
+
+  if ($gitBranch -ne "") {
+    Write-Host "(" -nonewline -foregroundcolor White
+    Write-Host "`e[1m$gitBranch" -nonewline -foregroundcolor Magenta
+    Write-Host "|" -nonewline -foregroundcolor White
+    if ($addedCount -gt 0) {
+      Write-Host "+$addedCount" -nonewline -foregroundcolor DarkBlue
+    }
+    if ($modifiedCount -gt 0) {
+      Write-Host "●$modifiedCount" -nonewline -foregroundcolor Yellow
+    }
+    if ($untrackedCount -gt 0) {
+      Write-Host "?$untrackedCount" -nonewline -foregroundcolor White
+    }
+    if ($addedCount -eq 0 -and $modifiedCount -eq 0 -and $untrackedCount -eq 0) {
+      Write-Host "" -nonewline -foregroundcolor Green
+    }
+    Write-Host ") " -nonewline -foregroundcolor White
   }
 
   Write-Host (Get-Date -Format "HH:mm:ss") -nonewline -foregroundcolor DarkGray
