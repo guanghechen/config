2d21d62d4fea59c2be8766a306f758d61f601fd8
guanghechen <me.guanghechen@gmail.com>
2024-12-28 13:25:14 +0800

    improve: tweak init-update

--- init-update.lua
+++ init-update.lua
@@ -3,12 +3,25 @@ _G.eve = require("eve")
 eve.setup_workspace()
 require("eve.option")
 
-eve.setup_state()
+local default_storage = eve.get_default_storage() ---@type eve.state.storage
+local storage = { editor = default_storage.editor, workspace = default_storage.workspace } ---@type eve.state.storage
+eve.setup_state(storage)
 
 require("guanghechen.option")
 require("guanghechen.plugin")
 
-require("lazy").sync()
-require("guanghechen.action.mason").install_all(false, function()
-  vim.cmd("qa")
-end)
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "LazyDone",
+--   callback = function()
+--     require("guanghechen.action.mason").install_all(false, function()
+--       vim.cmd("qa")
+--     end)
+--   end,
+-- })
+-- local ok = pcall(function()
+--   require("lazy").update()
+-- end)
+-- if not ok then
+--   vim.cmd("qa")
+-- end
+vim.cmd("qa")
