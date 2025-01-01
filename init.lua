f676606dc74ca69d49dac65b186188fd4f67228c
guanghechen <me.guanghechen@gmail.com>
2025-01-01 16:03:43 +0800

    improve: tweak yazi config

---
+++ init.lua
@@ -0,0 +1,24 @@
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+})
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+function Status:name()
+	local h = self._current.hovered
+	if not h then
+		return ""
+	end
+
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
