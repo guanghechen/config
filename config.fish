bd112b01ccedd92f3f33a7c0346302e344c98dc3
guanghechen <me.guanghechen@gmail.com>
2025-01-02 09:47:27 +0800

    update lang settings

--- config.fish
+++ config.fish
@@ -4,6 +4,7 @@ fish_vi_key_bindings
 set -gx TZ                              'Asia/Shanghai'
 set -gx LC_CTYPE                        en_US.UTF-8
 set -gx LC_ALL                          en_US.UTF-8
+set -gx LANG                            en_US.UTF-8
 set -gx PYTHONIOENCODING                utf8
 set -gx PYTHONUTF8                      1
 set -gx XDG_CONFIG_HOME                 "$HOME/.config"
