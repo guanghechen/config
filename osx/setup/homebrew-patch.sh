8f3b33f7290cf7b9f6d709a067ae471e3472dc6a
guanghechen <me.guanghechen@gmail.com>
2025-01-02 14:37:44 +0800

    improve: tweak osx homebrew app list

--- osx/setup/homebrew-patch.sh
+++ osx/setup/homebrew-patch.sh
@@ -5,3 +5,9 @@ source $HOME/.config/guanghechen/nix/setup/path.sh
 ### Install git
 brew tap microsoft/git
 brew install --cask git-credential-manager
+
+### Install kitty
+brew install --cask kitty
+
+### Install wezterm
+brew install --cask wezterm
