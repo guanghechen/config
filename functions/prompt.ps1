b662057df5cb23ab513aabb11766cd74c3b221e2
guanghechen <me.guanghechen@gmail.com>
2025-01-06 20:12:46 +0800

    improve: tweak profile

--- functions/prompt.ps1
+++ functions/prompt.ps1
@@ -35,28 +35,28 @@ function prompt {
   }
 
   Write-Host
-  Write-Host "  " -nonewline -foregroundcolor Blue
-  Write-Host "`e[1m$env:USERNAME" -nonewline -foregroundcolor Red
-  Write-Host "@" -nonewline -foregroundcolor Cyan
-  Write-Host "`e[1m$env:COMPUTERNAME" -nonewline -foregroundcolor White
+  Write-Host "  " -nonewline -foregroundcolor DarkBlue
+  Write-Host "`e[1m$env:USERNAME" -nonewline -foregroundcolor DarkRed
+  Write-Host "@" -nonewline -foregroundcolor DarkGray
+  Write-Host "`e[1m$env:COMPUTERNAME" -nonewline -foregroundcolor DarkCyan
   Write-Host " `e[1m$PWD" -nonewline -foregroundcolor DarkBlue
   Write-Host " " -nonewline
 
   if ($gitBranch -ne "") {
     Write-Host "(" -nonewline -foregroundcolor White
-    Write-Host "`e[1m$gitBranch" -nonewline -foregroundcolor Magenta
+    Write-Host "`e[1m$gitBranch" -nonewline -foregroundcolor DarkMagenta
     Write-Host "|" -nonewline -foregroundcolor White
     if ($addedCount -gt 0) {
       Write-Host "+$addedCount" -nonewline -foregroundcolor DarkBlue
     }
     if ($modifiedCount -gt 0) {
-      Write-Host "●$modifiedCount" -nonewline -foregroundcolor Yellow
+      Write-Host "●$modifiedCount" -nonewline -foregroundcolor DarkYellow
     }
     if ($untrackedCount -gt 0) {
       Write-Host "?$untrackedCount" -nonewline -foregroundcolor White
     }
     if ($addedCount -eq 0 -and $modifiedCount -eq 0 -and $untrackedCount -eq 0) {
-      Write-Host "" -nonewline -foregroundcolor Green
+      Write-Host "" -nonewline -foregroundcolor DarkGreen
     }
     Write-Host ") " -nonewline -foregroundcolor White
   }
@@ -65,10 +65,10 @@ function prompt {
 
   $conda_env_name = $env:CONDA_DEFAULT_ENV
   if ($conda_env_name -and $conda_env_name -ne "") {
-    Write-Host " `e[1m($conda_env_name) " -nonewline -foregroundcolor Green
+    Write-Host " `e[1m($conda_env_name) " -nonewline -foregroundcolor DarkGreen
   }
 
   Write-Host
-  Write-Host "PS>" -nonewline -foregroundcolor Cyan
+  Write-Host "PS>" -nonewline -foregroundcolor DarkCyan
   return " "
 }
