1ae3f7d6f20bf51340c5eff8c57d11f4cbcfe622
guanghechen <me.guanghechen@gmail.com>
2025-01-06 21:37:52 +0800

    fix zoxide

--- profile.ps1
+++ profile.ps1
@@ -21,9 +21,9 @@ If (Test-Path "$env:APP_HOME_MINIFORGE\Scripts\conda.exe") {
 ## Setup fnm
 fnm env --use-on-cd --shell power-shell | Out-String | Invoke-Expression
 
-## Setup zoxide (need move to the last line. see https://github.com/ajeetdsouza/zoxide/issues/707#issuecomment-1959685345)
-zoxide init powershell | Out-String | Invoke-Expression
-
 . "$env:XDG_CONFIG_HOME\pwsh\functions\prompt.ps1"
 . "$env:XDG_CONFIG_HOME\pwsh\functions\ghc.ps1"
 . "$env:XDG_CONFIG_HOME\pwsh\functions\yazi.ps1"
+
+## Setup zoxide (need ensure executed at the last line. see https://github.com/ajeetdsouza/zoxide/issues/707#issuecomment-1959685345)
+zoxide init powershell | Out-String | Invoke-Expression
