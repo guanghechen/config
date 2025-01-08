50fd4b3893cc3e9f8fa0ee05427d50cdb0c61aaf
guanghechen <me.guanghechen@gmail.com>
2025-01-08 19:12:52 +0800

    update keymap

--- ghc/platform/win.lua
+++ ghc/platform/win.lua
@@ -1,7 +1,31 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = {
-	keys = {},
+	keys = {
+		{ key = "Insert", mods = "CTRL", action = act.CopyTo("Clipboard") },
+		{ key = "Insert", mods = "SHIFT", action = act.PasteFrom("Clipboard") },
+		{ key = "v", mods = "ALT", action = act.PasteFrom("Clipboard") },
+		{ key = "F", mods = "ALT|CTRL|SHIFT", action = act.ToggleFullScreen },
+		{ key = "N", mods = "ALT|CTRL|SHIFT", action = act.SpawnCommandInNewWindow({ args = { "wezterm" } }) },
+		{ key = "T", mods = "ALT|CTRL|SHIFT", action = act.SpawnTab("CurrentPaneDomain") },
+		{ key = "W", mods = "ALT|CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
+		{ key = "Z", mods = "ALT|CTRL|SHIFT", action = act.ToggleFullScreen },
+		{ key = "0", mods = "ALT|CTRL|SHIFT", action = act.ResetFontSize },
+		{ key = "=", mods = "ALT|CTRL|SHIFT", action = act.IncreaseFontSize },
+		{ key = "-", mods = "ALT|CTRL|SHIFT", action = act.DecreaseFontSize },
+
+		---
+
+		{
+			key = "[",
+			mods = "ALT",
+			action = act.Multiple({
+				act.SendKey({ key = "a", mods = "CTRL" }),
+				act.SendKey({ key = "[" }),
+			}),
+		},
+	},
 }
 
 return config
