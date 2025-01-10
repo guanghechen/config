cdeb08c29dc8f59bd7ef0909d2a338ae723d2312
guanghechen <me.guanghechen@gmail.com>
2025-01-10 16:26:50 +0800

    update nix app list

--- nix/setup.sh
+++ nix/setup.sh
@@ -6,7 +6,7 @@ source $HOME/.config/guanghechen/nix/setup/path.sh
 sudo apt update
 sudo apt dist-upgrade -y
 sudo apt remove -y tmux
-sudo apt install -y curl file fontconfig gcc git locales vim wget
+sudo apt install -y curl file fontconfig gcc git locales make vim wget
 sudo apt autoremove
 sudo apt autoclean
 
