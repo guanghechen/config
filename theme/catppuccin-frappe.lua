1a495b417112fffa0d754f28e12ea05f0e6abcb3
guanghechen <me.guanghechen@gmail.com>
2025-01-18 20:08:21 +0800

    feat: add new themes

---
+++ theme/catppuccin-frappe.lua
@@ -0,0 +1,43 @@
+-- catppuccin-frappe
+local config = {
+  colors = {
+    -- Primary colors
+    background    = "#303446",
+    foreground    = "#B5BFE2",
+
+    -- Cursor colors
+    cursor_bg     = "#F4B8E4",
+    cursor_fg     = "#292C3C",
+    cursor_border = "#292C3C",
+
+    -- Selection colors
+    selection_bg  = "#E5C890",
+    selection_fg  = "#292C3C",
+
+    -- Normal colors
+    ansi = {
+      "#303446",
+      "#E78284",
+      "#A6D189",
+      "#E5C890",
+      "#8CAAEE",
+      "#CA9EE6",
+      "#81C8BE",
+      "#838BA7",
+    },
+
+    -- Bright colors
+    brights = {
+      "#737994",
+      "#E78284",
+      "#A6D189",
+      "#E5C890",
+      "#8CAAEE",
+      "#CA9EE6",
+      "#81C8BE",
+      "#B5BFE2",
+    },
+  },
+}
+
+return config
---
+++ theme/catppuccin-macchiato.lua
@@ -0,0 +1,43 @@
+-- catppuccin-macchiato
+local config = {
+  colors = {
+    -- Primary colors
+    background    = "#24273A",
+    foreground    = "#B8C0E0",
+
+    -- Cursor colors
+    cursor_bg     = "#F5BDE6",
+    cursor_fg     = "#1E2030",
+    cursor_border = "#1E2030",
+
+    -- Selection colors
+    selection_bg  = "#EED49F",
+    selection_fg  = "#1E2030",
+
+    -- Normal colors
+    ansi = {
+      "#24273A",
+      "#ED8796",
+      "#A6DA95",
+      "#EED49F",
+      "#8AADF4",
+      "#C6A0F6",
+      "#8BD5CA",
+      "#8087A2",
+    },
+
+    -- Bright colors
+    brights = {
+      "#6E738D",
+      "#ED8796",
+      "#A6DA95",
+      "#EED49F",
+      "#8AADF4",
+      "#C6A0F6",
+      "#8BD5CA",
+      "#B8C0E0",
+    },
+  },
+}
+
+return config
