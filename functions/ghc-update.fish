b3472eb488a07664656a05fbda58080afbeb8d03
guanghechen <me.guanghechen@gmail.com>
2025-01-22 21:48:36 +0800

    improve: update ghc-update config list

--- functions/ghc-update.fish
+++ functions/ghc-update.fish
@@ -1,34 +1,33 @@
 function ghc-update
-  set primary_configs alacritty btop fish fzf guanghechen helix lazygit lsd nvim ripgrep tmux yazi
-  set develop_configs nvim-nvchad
-  set base_path "$HOME/.config"
+    set primary_configs btop fish fzf guanghechen helix lazygit lsd nvim ripgrep tmux yazi
+    set develop_configs alacritty kitty neovide nvim-nvchad
+    set base_path "$HOME/.config"
 
-  for config in $primary_configs
-    set dir "$base_path/$config"
-    if test -d "$dir"
-      set cmd "cd $dir && git pull origin $config"
-    else
-      set cmd "git clone https://github.com/guanghechen/config.git --single-branch --branch=$config $dir"
+    for config in $primary_configs
+        set dir "$base_path/$config"
+        if test -d "$dir"
+            set cmd "cd $dir && git pull origin $config"
+        else
+            set cmd "git clone https://github.com/guanghechen/config.git --single-branch --branch=$config $dir"
+        end
+
+        set_color white
+        printf "\n%s\n" $cmd
+
+        set_color normal
+        fish -c "$cmd"
     end
 
-    set_color white
-    printf "\n%s\n" $cmd
+    for config in $develop_configs
+        set dir "$base_path/$config"
+        if test -d "$dir"
+            set cmd "cd $dir && git pull origin $config"
 
-    set_color normal
-    fish -c "$cmd"
-  end
+            set_color white
+            printf "\n%s\n" $cmd
 
-  for config in $develop_configs
-    set dir "$base_path/$config"
-    if test -d "$dir"
-      set cmd "cd $dir && git pull origin $config"
-
-      set_color white
-      printf "\n%s\n" $cmd
-
-      set_color normal
-      fish -c "$cmd"
+            set_color normal
+            fish -c "$cmd"
+        end
     end
-  end
 end
-
