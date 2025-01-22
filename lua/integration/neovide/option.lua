3a4c0f5fb0613909225fed299a851ca7a039d693
guanghechen <me.guanghechen@gmail.com>
2025-01-22 21:36:35 +0800

    improve(neovide): set term colors

--- lua/integration/neovide/option.lua
+++ lua/integration/neovide/option.lua
@@ -15,8 +15,7 @@ vim.o.linespace = 0
 vim.g.neovide_scale_factor = 1.0
 
 -- Transparency
-vim.g.transparency = 0.8
-vim.g.neovide_background_color = "#252525" .. string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+vim.g.transparency = 0.85
 vim.g.neovide_floating_blur = false
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
@@ -26,13 +25,39 @@ vim.g.neovide_floating_z_height = 10
 vim.g.neovide_light_angle_degrees = 45
 vim.g.neovide_light_radius = 5
 vim.g.neovide_show_border = true
-vim.g.neovide_transparency = 0
+vim.g.neovide_transparency = 1
 vim.g.neovide_window_blurred = true
 
 -- Window
-vim.g.neovide_fullscreen = false
+vim.g.neovide_fullscreen = true
 vim.g.neovide_padding_top = 0
 vim.g.neovide_padding_bottom = 0
 vim.g.neovide_padding_right = 0
 vim.g.neovide_padding_left = 0
 vim.g.neovide_remember_window_size = true
+
+do
+  local state = require("eve.state")
+  local scheme = state.theme.get_scheme(state.theme.theme:snapshot()) ---@type eve.t.theme.IScheme | nil
+  if scheme ~= nil then
+    local c = scheme.palette ---@type eve.t.theme.IPalette
+    vim.g.terminal_color_0 = c.bg0
+    vim.g.terminal_color_1 = c.red
+    vim.g.terminal_color_2 = c.green
+    vim.g.terminal_color_3 = c.yellow
+    vim.g.terminal_color_4 = c.blue
+    vim.g.terminal_color_5 = c.purple
+    vim.g.terminal_color_6 = c.aqua
+    vim.g.terminal_color_7 = c.fg1
+    vim.g.terminal_color_8 = c.bg0
+    vim.g.terminal_color_9 = c.brightRed
+    vim.g.terminal_color_10 = c.brightGreen
+    vim.g.terminal_color_11 = c.brightYellow
+    vim.g.terminal_color_12 = c.brightBlue
+    vim.g.terminal_color_13 = c.brightPurple
+    vim.g.terminal_color_14 = c.brightAqua
+    vim.g.terminal_color_15 = c.fg1
+
+    vim.g.neovide_background_color = c.bg0 .. string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+  end
+end
