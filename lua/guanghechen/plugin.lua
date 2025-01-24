aae42cc96b8a6810e42d73cb10aa3c4a12b70edb
guanghechen <me.guanghechen@gmail.com>
2025-01-24 10:31:17 +0800

    improve: support to toggle smear-cursor at runtime

--- lua/guanghechen/plugin.lua
+++ lua/guanghechen/plugin.lua
@@ -40,7 +40,7 @@ local conds = {
     return not vim.g.vscode
   end,
   smear_cursor = function()
-    return not vim.g.vscode and not vim.g.neovide and state.flight.smear_cursor:snapshot()
+    return not vim.g.vscode and not vim.g.neovide and (env.IS_WSL or env.IS_WIN)
   end,
   treesitter_context = function()
     return not vim.g.vscode and state.flight.treesitter_context:snapshot()
--- lua/guanghechen/plugins/smear-cursor.lua
+++ lua/guanghechen/plugins/smear-cursor.lua
@@ -1,3 +1,7 @@
+local ft = require("eve.constant.filetype")
+local state = require("eve.state")
+local Subscriber = require("eve.collection.subscriber")
+
 return {
   "smear-cursor.nvim",
   event = { "VeryLazy" },
@@ -8,4 +12,29 @@ return {
     stiffness = 0.8,
     trailing_stiffness = 0.5,
   },
+  config = function(_, opts)
+    local smear = require("smear_cursor")
+    smear.setup(opts)
+
+    state.flight.smear_cursor:subscribe(
+      Subscriber.new({
+        on_next = function(flag)
+          smear.enabled = flag
+
+          vim.defer_fn(function()
+            if not flag then
+              local bufnrs = vim.api.nvim_list_bufs() ---@type integer[]
+              for _, bufnr in ipairs(bufnrs) do
+                local filetype = vim.bo[bufnr].filetype ---@type string
+                if filetype == ft.SMEAR_CURSOR then
+                  vim.api.nvim_buf_delete(bufnr, { force = true })
+                end
+              end
+            end
+          end, 200)
+        end,
+      }),
+      false
+    )
+  end,
 }
