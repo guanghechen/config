aa367ded20e4823f54c2089d1d5156a6f34f624a
guanghechen <me.guanghechen@gmail.com>
2025-01-26 13:43:03 +0800

    chore(node): install node-gyp by default

--- nix/setup/node.sh
+++ nix/setup/node.sh
@@ -6,5 +6,5 @@ if fnm list | grep -q "v20"; then
   printf "\n\e[33;5;214m  [setup node] node@20 is already installed. (skipped)\e[0m\n"
 else
   fnm install 20
-  fish -c "npm install -g npm yarn"
+  fish -c "npm install -g npm yarn node-gyp"
 fi
--- win/setup/node.ps1
+++ win/setup/node.ps1
@@ -2,5 +2,5 @@ if (fnm list | Select-String -Quiet "v20") {
   Write-Host "[setup node] node@20 is already installed. (skipped)" -ForegroundColor DarkYellow
 } else {
   fnm install 20
-  npm install -g npm yarn
+  npm install -g npm yarn node-gyp
 }
