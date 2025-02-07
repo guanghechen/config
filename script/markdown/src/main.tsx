79226469747f51c4fee1acf125da9ac942828492
guanghechen <me.guanghechen@gmail.com>
2025-02-07 15:46:57 +0800

    improve(markdown): add basic page

---
+++ script/markdown/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { MarkdownView } from '@/view/markdown'
+
+const App: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <React.Suspense fallback={<div>loading...</div>}>
+        <MarkdownView />
+      </React.Suspense>
+    </React.StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')!).render((<App />))
---
+++ script/markdown/src/view/markdown/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const MarkdownView: React.FC = () => {
+  return <div>shit!</div>
+}
