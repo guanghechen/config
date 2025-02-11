36736afa759068025861d6fc3b830da4a20bd7c2
guanghechen <me.guanghechen@gmail.com>
2025-02-11 15:02:05 +0800

    chore(avante): upgrade avante.nvim

--- lua/guanghechen/plugins/avante.lua
+++ lua/guanghechen/plugins/avante.lua
@@ -184,10 +184,6 @@ return {
 
       ------------------------------------------------------------------------------------------------
 
-      -- file_selector = {
-      -- provider = get_file_selector(),
-      -- },
-
       mappings = {
         ask = "<leader>aa",
         edit = "<leader>ae",
@@ -231,17 +227,5 @@ return {
       }),
       true
     )
-
-    --- override the file_selector ---
-    local file_selector = get_file_selector()
-    local FileSelector = require("avante.file_selector")
-    function FileSelector:native_ui(handler)
-      local filepaths = self:get_filepaths() ---@type string[]
-      file_selector({
-        title = "(Avante) Add a file",
-        filepaths = filepaths,
-        handler = handler,
-      })
-    end
   end,
 }
