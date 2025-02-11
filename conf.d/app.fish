fcdd565c97b42af3a57dd9c6591c8060468f3b90
guanghechen <me.guanghechen@gmail.com>
2025-02-11 09:19:17 +0800

    chore(conda): enable lemon by default

--- conf.d/app.fish
+++ conf.d/app.fish
@@ -14,6 +14,10 @@ if test -f "$HOME/.app/miniforge3/bin/conda"
   set -gx CONDA_CHANGEPS1 false
   set -gx CONDA_PROMPT_MODIFIER ""
   eval "$HOME/.app/miniforge3/bin/conda" "shell.fish" "hook" $argv | source
+
+  if status is-interactive
+    conda activate lemon
+  end
 end
 
 ### tmux
