3c46c7d07e1cfad95a40653299365e9cfe937cf5
guanghechen <me.guanghechen@gmail.com>
2025-02-13 21:19:04 +0800

    improve: support mix path between win and wsl & support path tag

--- server/util/path.ts
+++ server/util/path.ts
@@ -1,9 +1,42 @@
+import os from 'node:os'
 import path from 'node:path'
 
-export const resolveRealFilepath = (filepath: string): string => {
-  const p: string = filepath
-    .replace(/^([a-zA-Z]):([\s\S]*)$/, (_, m1, m2) => `/mnt/${m1.toLowerCase()}/${m2}`)
-    .replace(/[/\\]+/g, '/')
-    .replace(/^~/, process.env.HOME || '')
-  return path.normalize(p).replace(/[/\\]+/g, '/')
+const platform: 'win' | 'wsl' | 'mac' | 'nix' | 'unknown' = (() => {
+  if (os.platform() === 'win32') {
+    if (os.release().toLowerCase().includes('microsoft')) return 'wsl'
+    return 'win'
+  }
+  if (os.platform() === 'darwin') return 'mac'
+  if (os.platform() === 'linux') return 'nix'
+  return 'unknown'
+})()
+
+export function normalizeFilepath(filepath: string): string {
+  const resolvedFilepath: string = path.normalize(
+    filepath
+      .replace(/^@([\w-]+):/, (_, m1) => {
+        const envName: string = `YOZORA_WORKSPACE_${m1.replace(/-/g, '_').toUpperCase()}`
+        return process.env[envName] || m1
+      })
+      .replace(/^~/, process.env.HOME || '~'),
+  )
+
+  if (platform === 'mac' || platform === 'nix') {
+    return resolvedFilepath.replace(/[/\\]+/g, '/')
+  }
+
+  if (platform === 'wsl') {
+    return resolvedFilepath
+      .replace(/^([a-zA-Z]):/, (_, m1) => `/mnt/${m1.toLowerCase()}`)
+      .replace(/[/\\]+/g, '/')
+  }
+
+  if (platform === 'win') {
+    return resolvedFilepath.replace(/^\/mnt\/([a-zA-Z])\//, (_, m1) => `${m1.toUpperCase()}:/`)
+  }
+  return resolvedFilepath
+}
+
+export function resolveRealFilepath(filepath: string): string {
+  return path.normalize(normalizeFilepath(filepath))
 }
