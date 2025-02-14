5359fdbff7ea9a0b4ab8d80770209673c72a96a6
guanghechen <me.guanghechen@gmail.com>
2025-02-14 19:34:41 +0800

    improve: tweak alias

--- conf.d/alias.fish
+++ conf.d/alias.fish
@@ -3,6 +3,11 @@ alias ..='cd ../'
 alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
+alias .1='cd ../'
+alias .2='cd ../../'
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
 alias cd..='cd ../'
 alias cd...='cd ../../'
 alias cd....='cd ../../../'
@@ -19,9 +24,10 @@ alias ln='ln -i'
 alias ls='ls --color=auto'
 alias mkdir='mkdir -pv'
 alias mv='mv -i'
+alias ports='netstat -tulanp'
 alias rm='rm -i -I'
 alias sss='source ~/.config/fish/config.fish'
-alias tree-list='tree --dirsfirst -aCF'
+alias tls='tree --dirsfirst -aCF'
 alias vdir='vdir --color=auto'
 
 ### fzf
@@ -30,9 +36,9 @@ alias fvim='FZF_DEFAULT_OPTS_FILE=$HOME/.config/fzf/nvim.fzfrc $HOMEBREW_PREFIX/
 
 ### lazygit
 if test -f "$HOME/.config/lazygit/local/theme.yml"
-  alias lg="lazygit -ucf $HOME/.config/lazygit/local/theme.yml"
+    alias lg="lazygit -ucf $HOME/.config/lazygit/local/theme.yml"
 else
-  alias lg="lazygit -ucf $HOME/.config/lazygit/config.yml"
+    alias lg="lazygit -ucf $HOME/.config/lazygit/config.yml"
 end
 
 ### nvim
@@ -42,14 +48,14 @@ alias nvchad="NVIM_APPNAME=nvim-nvchad $HOMEBREW_PREFIX/bin/nvim"
 alias lazyvim="NVIM_APPNAME=nvim-lazy $HOMEBREW_PREFIX/bin/nvim"
 
 ### tmux
-alias tnew='tmux new -s'                # Create a new tmux session
-alias tkill='tmux kill-session -t'      # Kill a tmux session
+alias tnew='tmux new -s' # Create a new tmux session
+alias tkill='tmux kill-session -t' # Kill a tmux session
 alias tkill-all='tmux list-sessions | awk \'{print $1}\' | xargs -n 1 tmux kill-session -t'
-alias tdetach='tmux detach'             # Detach from the session
-alias tattach='tmux attach -t'          # Attach to a session
-alias takeover='tmux detach -a'         # Detach other clients from the session
-alias watch-fake-clipboard="nohup bash $HOME/.config/tmux/script/fake-clipboard.sh /opt/me/data/clipboard/fake.txt &!"
+alias tdetach='tmux detach' # Detach from the session
+alias tattach='tmux attach -t' # Attach to a session
+alias tdetach-others='tmux detach -a' # Detach other clients from the session
 alias tmux-use-fake-clipboard="tmux set-environment ghc_use_fake_clipboard /opt/me/data/clipboard/fake.txt"
+alias watch-fake-clipboard="nohup bash $HOME/.config/tmux/script/fake-clipboard.sh /opt/me/data/clipboard/fake.txt &!"
 
 ### misc
 alias ghc-clock='tty-clock -DSbcnrs -C5'
@@ -58,16 +64,16 @@ alias reset-gpg-agent='gpgconf --kill gpg-agent'
 alias start-pfctl='sudo pfctl -ef /etc/pf.conf'
 
 ### platform specific
-if test (uname) = "Darwin"
-  alias ghc-reset-git-credential='echo -e "host=github.com\nprotocol=https\n" | git credential-osxkeychain erase'
+if test (uname) = Darwin
+    alias ghc-reset-git-credential='echo -e "host=github.com\nprotocol=https\n" | git credential-osxkeychain erase'
 else
-  alias chmod='chmod --preserve-root' # the `--preserve-root` option not worked in MacOS.
+    alias chmod='chmod --preserve-root' # the `--preserve-root` option not worked in MacOS.
 end
 
 ### wsl
 if test -e /proc/version
-  if grep -qEi "(Microsoft|WSL)" /proc/version
-    alias open="ghc-open"
-    alias pbpaste="ghc-pbpaste"
-  end
+    if grep -qEi "(Microsoft|WSL)" /proc/version
+        alias open="ghc-open"
+        alias pbpaste="ghc-pbpaste"
+    end
 end
