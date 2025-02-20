5d0672ec0028e320adb8da7f0ec1124d69ea4dfa
guanghechen <me.guanghechen@gmail.com>
2025-02-20 18:45:40 +0800

    improve(image): update queries

---
+++ queries/typst/images.scm
@@ -0,0 +1,11 @@
+(call
+  (ident) @ident
+  (#eq? @ident "image")
+  (group (string) @image.src)
+  (#offset! @image.src 0 1 0 -1)
+) @image
+
+(math
+  (#set! image.ext "math.typ")
+  (formula) @image
+) @image.content
