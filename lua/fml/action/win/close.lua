f735f78d6765a71bedb8ec3ebac7a4fcdb9b5dc3
guanghechen <me.guanghechen@gmail.com>
2025-02-25 16:19:12 +0800

    fix(keymap): close current window

--- lua/fml/action/win/close.lua
+++ lua/fml/action/win/close.lua
@@ -3,15 +3,15 @@ local M = {}
 
 ---@param context                       eve.command.IContext
 ---@return nil
+---@diagnostic disable-next-line: unused-local
 function M.close(context)
-  vim.api.nvim_tabpage_set_win(context.tabnr, context.winnr)
   vim.cmd.close()
 end
 
 ---@param context                       eve.command.IContext
 ---@return nil
+---@diagnostic disable-next-line: unused-local
 function M.close_others(context)
-  vim.api.nvim_tabpage_set_win(context.tabnr, context.winnr)
   vim.cmd.only()
 end
 
