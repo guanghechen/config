732408ce40f5848779a728a18d6226afc9fa9758
guanghechen <me.guanghechen@gmail.com>
2025-02-26 16:08:28 +0800

    improve(nix): install colordiff by default

--- nix/setup.sh
+++ nix/setup.sh
@@ -6,7 +6,7 @@ source $HOME/.config/guanghechen/nix/setup/path.sh
 sudo apt update
 sudo apt dist-upgrade -y
 sudo apt remove -y tmux
-sudo apt install -y curl file fontconfig gcc git locales make net-tools vim wget
+sudo apt install -y colordiff curl file fontconfig gcc git locales make net-tools vim wget
 sudo apt install -y build-essential libvips-dev unixodbc
 sudo apt autoremove
 sudo apt autoclean
