4f90477cadb4f85eb1a3bc8866a32bdb38a3323e
guanghechen <me.guanghechen@gmail.com>
2025-02-26 16:00:39 +0800

    improve(nix): install unixodbc by default

--- nix/setup.sh
+++ nix/setup.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt dist-upgrade -y
 sudo apt remove -y tmux
 sudo apt install -y curl file fontconfig gcc git locales make net-tools vim wget
-sudo apt install -y build-essential libvips-dev
+sudo apt install -y build-essential libvips-dev unixodbc
 sudo apt autoremove
 sudo apt autoclean
 
