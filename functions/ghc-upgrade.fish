9a90e246e4e4be463d9f1e441f05037ff0b5c7b1
guanghechen <me.guanghechen@gmail.com>
2025-03-02 14:19:10 +0800

    improve: auto run nix-remote by checking the ssh tag

--- functions/ghc-upgrade.fish
+++ functions/ghc-upgrade.fish
@@ -1,8 +1,13 @@
 function ghc-upgrade
-  if test (uname) = "Darwin"
-    bash $HOME/.config/guanghechen/osx/setup.sh
+  if set -q SSH_CONNECTION; or set -q SSH_CLIENT; or set -q SSH_TTY
+    bash $HOME/.config/guanghechen/nix-remote/setup.sh
+    return
   else
-    bash $HOME/.config/guanghechen/nix/setup.sh
+
+    if test (uname) = Darwin
+      bash $HOME/.config/guanghechen/osx/setup.sh
+    else
+      bash $HOME/.config/guanghechen/nix/setup.sh
+    end
   end
 end
-
