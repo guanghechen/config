75a3c4aec2129e433d4052f6304b219c647c36fe
guanghechen <me.guanghechen@gmail.com>
2025-03-05 10:16:43 +0800

    fix(win): since the cargo install could build failed, so let's use the winget directly

--- win/setup/winget.ps1
+++ win/setup/winget.ps1
@@ -5,25 +5,27 @@ winget install -e --source winget --id Microsoft.PowerShell
 winget install -e --source winget --id Microsoft.PowerToys
 winget install -e --source winget --id Neovim.Neovim
 
-# winget install -e --source winget --id dandavison.delta
-# winget install -e --source winget --id sharkdp.fd
-# winget install -e --source winget --id Schniz.fnm
 winget install -e --source winget --id ImageMagick.ImageMagick
 winget install -e --source winget --id junegunn.fzf
 winget install -e --source winget --id jqlang.jq
 winget install -e --source winget --id JesseDuffield.lazygit
-# winget install -e --source winget --id lsd-rs.lsd
-# winget install -e --source winget --id BurntSushi.ripgrep.MSVC
-# winget install -e --source winget --id sxyazi.yazi
-# winget install -e --source winget --id ajeetdsouza.zoxide
+
+winget install -e --source winget --id dandavison.delta
+winget install -e --source winget --id sharkdp.fd
+winget install -e --source winget --id Schniz.fnm
+winget install -e --source winget --id lsd-rs.lsd
+winget install -e --source winget --id BurntSushi.ripgrep.MSVC
+winget install -e --source winget --id sxyazi.yazi
+winget install -e --source winget --id ajeetdsouza.zoxide
 
 
-cargo install --locked git-delta
-cargo install --locked fd-find
-cargo install --locked fnm
-cargo install --locked lsd
-cargo install --locked ripgrep
-cargo install --locked yazi-fm yazi-cli
-cargo install --locked zoxide
+## Cargo version is updated too frequently and sometimes could build failed.
+# cargo install --locked git-delta
+# cargo install --locked fd-find
+# cargo install --locked fnm
+# cargo install --locked lsd
+# cargo install --locked ripgrep
+# cargo install --locked yazi-fm yazi-cli
+# cargo install --locked zoxide
 
 Write-Host "[setup winget] done." -ForegroundColor Green
