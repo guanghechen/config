09501d38f6397062ed2df962be8a9a807f623c0f
guanghechen <me.guanghechen@gmail.com>
2025-03-13 10:08:21 +0800

    improve: add eve.std proxy

---
+++ lua/eve/std/init.lua
@@ -0,0 +1,28 @@
+---@class eve.std.mods
+local mods = {
+  colors = true,
+  debug = true,
+  env = true,
+  json = true,
+  md5 = true,
+  tmux = true,
+}
+
+---@class eve.std
+---@field public colors                 eve.std.color
+---@field public debug                  eve.std.debug
+---@field public env                    eve.std.env
+---@field public json                   eve.std.json
+---@field public md5                    eve.std.md5
+---@field public tmux                   eve.std.tmux
+local M = setmetatable({}, {
+  __index = function(t, k)
+    if mods[k] then
+      ---@diagnostic disable-next-line: no-unknown
+      t[k] = require("eve.std." .. k)
+    end
+    return rawget(t, k)
+  end,
+})
+
+return M
