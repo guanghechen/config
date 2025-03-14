34e126f495f081a2b4b6511787b7a13290c5eda9
guanghechen <me.guanghechen@gmail.com>
2025-03-14 10:12:14 +0800

    improve: merge types

--- lua/eve/types/common.lua
+++ lua/eve/types/common.lua
@@ -17,6 +17,31 @@
 ---@alias eve.t.IValidate
 ---| fun(element: T): boolean
 
+---@class eve.t.IHighlight
+---@field public lnum                   integer
+---@field public coll                   integer
+---@field public colr                   integer
+---@field public hlname                 string
+
+---@class eve.t.IHighlightInline
+---@field public coll                   integer
+---@field public colr                   integer
+---@field public hlname                 string
+
+---@class eve.t.IKeymapOverridable
+---@field public bufnr                  ?integer
+---@field public nowait                 ?boolean
+---@field public noremap                ?boolean
+---@field public silent                 ?boolean
+
+---@class eve.t.IKeymap : eve.t.IKeymapOverridable
+---@field public disabled               boolean|nil
+---@field public modes                  eve.e.VimMode[]
+---@field public key                    string
+---@field public aliases                string[]|nil
+---@field public callback               fun(): nil
+---@field public desc                   string|nil
+
 ---@class eve.t.IMatchLocation
 ---@field public offset                 integer
 ---@field public lnum                   integer
@@ -26,3 +51,9 @@
 ---@class eve.t.IMatchPoint
 ---@field public l                      integer
 ---@field public r                      integer
+
+---@class eve.t.IQuickFixItem
+---@field public filename               string
+---@field public lnum                   ?integer
+---@field public col                    ?integer
+---@field public text                   ?string
