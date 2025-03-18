e077c99e2946b07724323f032fb7f33dbbb0ccf8
guanghechen <me.guanghechen@gmail.com>
2025-03-18 21:49:06 +0800

    fix: attempt to index local 'context'

--- lua/fml/action/tab/new.lua
+++ lua/fml/action/tab/new.lua
@@ -13,29 +13,31 @@ function M.new()
   return tabnr
 end
 
-function M.new_with_buf(context)
+function M.new_with_buf()
+  local bufnr = vim.api.nvim_get_current_buf() ---@type integer
+
   vim.cmd("$tabnew")
   vim.bo.buflisted = false
   vim.bo.bufhidden = "wipe"
 
   local tabnr = vim.api.nvim_get_current_tabpage() ---@type integer
-  local winnr = vim.api.nvim_tabpage_get_win(tabnr) ---@type integer
-  local bufnr = context.bufnr ---@type integer
-  local bufs = {} ---@type eve.state.tab.buf.state[]
+  eve.editor.set_tabtype(tabnr, eve.var.TabTypes.NORMAL)
 
-  if bufnr ~= nil and eve.editor.is_buf_valid(bufnr) and eve.editor.is_buf_sourcefile(bufnr) then
+  local bufs = {} ---@type eve.state.tab.buf.state[]
+  if eve.editor.is_buf_sourcefile(bufnr) then
     bufs[#bufs + 1] = { bufnr = bufnr, pinned = false } ---@type eve.state.tab.buf.state
   end
-
-  eve.editor.set_tabtype(tabnr, eve.var.TabTypes.NORMAL)
 
   local meta = eve.state.tab.Meta.new(tabnr, bufs)
   eve.state.tab.set(tabnr, meta)
   eve.state.tab.tab_history:push(tabnr)
 
-  if bufnr ~= nil and eve.editor.is_buf_valid(bufnr) then
-    vim.api.nvim_win_set_buf(winnr, bufnr)
-  end
+  local winnr = vim.api.nvim_tabpage_get_win(tabnr) ---@type integer
+  vim.schedule(function()
+    if bufnr ~= nil and eve.editor.is_buf_valid(bufnr) then
+      vim.api.nvim_win_set_buf(winnr, bufnr)
+    end
+  end)
   return tabnr
 end
 
