1f0b2d9643630e7d16e8cce7c55e9b33fcb7af06
guanghechen <me.guanghechen@gmail.com>
2025-03-25 11:33:43 +0800

    improve: tweak markdown image renderer

--- src/component/markdown/renderer/inner/ImageRendererInner.tsx
+++ src/component/markdown/renderer/inner/ImageRendererInner.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/css'
+import cn from 'clsx'
 import React from 'react'
 
 interface IProps {
@@ -12,6 +13,30 @@ interface IProps {
 }
 
 export class ImageRendererInner extends React.Component<IProps> {
+  public static displayName = 'ImageRendererInner'
+
+  public override render(): React.ReactElement {
+    const { src, alt, title, srcSet, sizes, loading, className } = this.props
+    const { width, height } = this.parseImageDimensions(src)
+    const figureStyle: React.CSSProperties = { width, height }
+
+    return (
+      <figure className={cn(className, cls)} style={figureStyle}>
+        <img
+          alt={alt}
+          src={src}
+          title={title}
+          srcSet={srcSet}
+          sizes={sizes}
+          loading={loading}
+          width={width}
+          height={height}
+        />
+        {title && <figcaption>{title}</figcaption>}
+      </figure>
+    )
+  }
+
   public override shouldComponentUpdate(nextProps: IProps): boolean {
     const props = this.props
     return (
@@ -25,14 +50,23 @@ export class ImageRendererInner extends React.Component<IProps> {
     )
   }
 
-  public override render(): React.ReactElement {
-    const { src, alt, title, srcSet, sizes, loading, className } = this.props
-    return (
-      <figure className={`${className} ${cls}`}>
-        <img alt={alt} src={src} title={title} srcSet={srcSet} sizes={sizes} loading={loading} />
-        {title && <figcaption>{title}</figcaption>}
-      </figure>
-    )
+  private parseImageDimensions(src: string): { width?: string; height?: string } {
+    try {
+      const url = new URL(src)
+      const width = url.searchParams.get('width')
+      const height = url.searchParams.get('height')
+
+      return {
+        width: width || undefined,
+        height: height || undefined,
+      }
+    } catch (_) {
+      const urlParams = new URLSearchParams(src.split('?')[1] || '')
+      return {
+        width: urlParams.get('width') || undefined,
+        height: urlParams.get('height') || undefined,
+      }
+    }
   }
 }
 
@@ -47,6 +81,7 @@ const cls = css({
     flex: '1 0 auto',
     boxSizing: 'border-box',
     maxWidth: '100%',
+    objectFit: 'contain',
     border: '1px solid var(--colorBorderImage)',
     boxShadow: '0 0 20px 1px rgba(126, 125, 150, 0.6)',
   },
