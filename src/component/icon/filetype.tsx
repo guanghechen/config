c44e7d55cd16fd2fa903876731cb610e29570835
guanghechen <me.guanghechen@gmail.com>
2025-03-26 21:37:47 +0800

    improve: tweak fileicon colors

--- src/component/icon/filetype.tsx
+++ src/component/icon/filetype.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 export const ChevronDownIcon: React.FC<{ className?: string }> = () => {
-  return <span className="mr-2 text-gray-500"> </span>
+  return <span className="mr-2 text-gray-400"> </span>
 }
 
 export const ChevronRightIcon: React.FC<{ className?: string }> = () => {
-  return <span className="mr-2 text-gray-500"> </span>
+  return <span className="mr-2 text-gray-400"> </span>
 }
 
 export const FolderIcon: React.FC<{ className?: string }> = () => {
-  return <span className="mr-2 text-blue-500"> </span>
+  return <span className="mr-2 text-blue-300"> </span>
 }
 
 export const FolderOpenIcon: React.FC<{ className?: string }> = () => {
-  return <span className="mr-2 text-blue-500"> </span>
+  return <span className="mr-2 text-blue-300"> </span>
 }
 
 export const FileTypeIcon: React.FC<{ extname: string }> = props => {
