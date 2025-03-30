d61b2310096a1e46d42b33bf1185d4f43986459c
guanghechen <me.guanghechen@gmail.com>
2025-03-30 17:04:23 +0800

    chore: support project scope tailwind lsp config

--- lua/ghc/lsp/lang/tailwindcss.lua
+++ lua/ghc/lsp/lang/tailwindcss.lua
@@ -8,10 +8,26 @@ local CONFIG_FILENAMES = {
   "tailwind.config.ts",
   "tailwind.config.js",
   "tailwind.config.mjs",
+  "vite.config.ts",
 }
 
 return function()
   local capabilities = get_capabilities()
+
+  ---@return string|nil
+  local function detectLspServer()
+    local _, binPath = locate_lsp_root(
+      eve.path.cwd() .. eve.env.PATH_SEP .. "a.css",
+      { "./node_modules/.bin/tailwindcss-language-server" }
+    )
+    if binPath ~= nil and vim.fn.executable(binPath) == 1 then
+      return binPath
+    end
+    return nil
+  end
+
+  local lspBinPath = detectLspServer()
+  eve.debug.log({ lspBinPath = lspBinPath })
 
   return {
     capabilities = capabilities,
@@ -19,6 +35,7 @@ return function()
     on_init = on_init,
     filetypes_exclude = { "markdown" },
     filetypes_include = { "css", "javascriptreact", "javascript.jsx", "typescriptreact", "typescript.tsx" },
+    cmd = lspBinPath and { lspBinPath, "--stdio" } or nil,
     root_dir = function(filename)
       return locate_lsp_root(filename, CONFIG_FILENAMES)
     end,
