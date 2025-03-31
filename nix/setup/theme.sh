28ca14b705bb49dc52c562b6e969771194c8a424
guanghechen <me.guanghechen@gmail.com>
2025-03-31 13:45:51 +0800

    improve(theme): read GUANGHECHEN_PREFER_THEME as the default theme

--- nix/setup/theme.sh
+++ nix/setup/theme.sh
@@ -5,5 +5,6 @@ source $HOME/.config/guanghechen/nix/setup/path.sh
 # printf "\n\e[34m  [setup config] gen themes...\e[0m\n"
 # fish -c "node ~/.config/guanghechen/config/theme/gen_themes.mjs"
 
-printf "\n\e[34m  [setup config] set default theme...\e[0m\n"
-fish -c "node ~/.config/guanghechen/config/theme/apply_theme.mjs catppuccin-mocha"
+THEME=${GUANGHECHEN_PREFER_THEME:-catppuccin-mocha}
+printf "\n\e[34m  [setup config] set default theme ($THEME)...\e[0m\n"
+fish -c "node ~/.config/guanghechen/config/theme/apply_theme.mjs $THEME"
--- win/setup/theme.ps1
+++ win/setup/theme.ps1
@@ -1,4 +1,5 @@
 if (fnm list | Select-String -Quiet "v20") {
-  Write-Host "[setup config] set default theme..." -ForegroundColor DarkBlue
-  node "$env:XDG_CONFIG_HOME\guanghechen\config\theme\apply_theme.mjs" catppuccin-mocha
+  $THEME = if ($env:GUANGHECHEN_PREFER_THEME) { $env:GUANGHECHEN_PREFER_THEME } else { "catppuccin-mocha" }
+  Write-Host "[setup config] set default theme ($THEME)..." -ForegroundColor DarkBlue
+  node "$env:XDG_CONFIG_HOME\guanghechen\config\theme\apply_theme.mjs" $THEME
 }
