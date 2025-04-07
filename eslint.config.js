d9f56ab942832f8e3631191059b45ed47a9a2e25
guanghechen <me.guanghechen@gmail.com>
2025-04-07 14:21:02 +0800

    chore: fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
 import { genConfigs } from '@guanghechen/eslint-config'
 import reactHooks from 'eslint-plugin-react-hooks'
+import path from 'node:path'
+import url from 'node:url'
+
+const WORKSPACE_DIR = path.dirname(url.fileURLToPath(import.meta.url))
 
 const configs = [
   {
     ignores: ['.vscode/', '**/__tmp__/', '**/doc/', '**/example/', '.prettierrc'],
   },
-  ...genConfigs({ tsconfigPath: './tsconfig.eslint.json' }),
+  ...genConfigs({ tsconfigPath: path.join(WORKSPACE_DIR, './tsconfig.eslint.json') }),
   reactHooks.configs['recommended-latest'],
   {
     files: ['src/**/*.{ts,cts,mts}'],
