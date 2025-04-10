74d19f171556daa1a7cf54ee415de47bd9b41b27
guanghechen <me.guanghechen@gmail.com>
2025-04-10 16:01:56 +0800

    fix theme apply script & add theme toggle script

--- functions/ghc.ps1
+++ functions/ghc.ps1
@@ -47,7 +47,26 @@ function ghc-proxy {
 function ghc-theme-apply {
   $script_path = "$env:XDG_CONFIG_HOME\guanghechen\config\theme\apply_theme.mjs"
   if (Test-Path -Path $script_path) {
-    $first_arg = $args.Trim() -replace '^\s+|\s+$', '' -replace '([A-Z])', { $_.Value.ToLower() }
+    $first_arg = if ($args -and $args.Count -gt 0) {
+      $args[0].ToString().Trim() -replace '^\s+|\s+$', '' -replace '([A-Z])', { $_.Value.ToLower() }
+    } else {
+      ""
+    }
+    node $script_path $first_arg
+  } else {
+    Write-Host "Cannot find the script file: $script_path." -foregroundcolor Red
+  }
+}
+
+# Toggle theme.
+function ghc-theme-toggle {
+  $script_path = "$env:XDG_CONFIG_HOME\guanghechen\config\theme\toggle_theme.mjs"
+  if (Test-Path -Path $script_path) {
+    $first_arg = if ($args -and $args.Count -gt 0) {
+      $args[0].ToString().Trim() -replace '^\s+|\s+$', '' -replace '([A-Z])', { $_.Value.ToLower() }
+    } else {
+      ""
+    }
     node $script_path $first_arg
   } else {
     Write-Host "Cannot find the script file: $script_path." -foregroundcolor Red
