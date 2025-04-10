822ae162e615804b39daaf4c94e15ba5302ad836
guanghechen <me.guanghechen@gmail.com>
2025-04-10 14:48:00 +0800

    improve: remember theme in the ~/.config/guanghechen/.setting.json instead

--- script/theme-toggle.sh
+++ script/theme-toggle.sh
@@ -1,12 +1,7 @@
 #! /usr/bin/env bash
 
 function _ghc_tmux_theme_toggle_ {
-  local theme=$(tmux show -gqv @GHC_TMUX_THEME)
-  if [ -z "$theme" ]; then
-    theme="catppuccin-mocha"
-    tmux set -g @GHC_TMUX_THEME "$theme"
-  fi
-  node $HOME/.config/guanghechen/config/theme/toggle_theme.mjs "$theme"
+  node $HOME/.config/guanghechen/config/theme/toggle_theme.mjs
 }
 
 _ghc_tmux_theme_toggle_
