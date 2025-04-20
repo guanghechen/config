8f0d85bb31f80b752ab115d6fc010ce48089f7fe
guanghechen <me.guanghechen@gmail.com>
2025-04-20 11:16:41 +0800

    improve(neovim): skip build if the .so/.dll already exist

--- rust/nvim_tools/build.ps1
+++ rust/nvim_tools/build.ps1
@@ -1,18 +1,17 @@
-# Set the directory name of the current script
 Set-Location -Path $PSScriptRoot
 
-# Build your Rust project using Cargo release build
-cargo build --release
+$luaTargetPath = Join-Path -Path $PSScriptRoot -ChildPath "../../lua/nvim_tools.dll"
+$binTargetPath = Join-Path -Path $PSScriptRoot -ChildPath "../../bin/win.nvim_tools.dll"
 
-$binDirectoryPath = Join-Path -Path $PSScriptRoot -ChildPath "../../bin"
+if (-not (Test-Path -Path $luaTargetPath)) {
+    cargo build --release
 
-# Check if the directory exists
-if (-not (Test-Path -Path $binDirectoryPath)) {
-    # Create the directory
-    New-Item -Path $binDirectoryPath -ItemType Directory | Out-Null
+    $binDirectoryPath = Join-Path -Path $PSScriptRoot -ChildPath "../../bin"
+    if (-not (Test-Path -Path $binDirectoryPath)) {
+        New-Item -Path $binDirectoryPath -ItemType Directory | Out-Null
+    }
+
+    Copy-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath "target/release/nvim_tools.dll") -Destination $luaTargetPath
+    Copy-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath "target/release/nvim_tools.dll") -Destination $binTargetPath
 }
-
-# Copy the file `nvim_tools.dll` from target/release/ to lua/ and bin/ in your current directory
-Copy-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath "target/release/nvim_tools.dll") -Destination (Join-Path -Path $PSScriptRoot -ChildPath "../../lua/nvim_tools.dll")
-Copy-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath "target/release/nvim_tools.dll") -Destination (Join-Path -Path $PSScriptRoot -ChildPath "../../bin/win.nvim_tools.dll")
 
--- rust/nvim_tools/build.sh
+++ rust/nvim_tools/build.sh
@@ -5,25 +5,38 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # Get the directory of the current script
 cd "$SCRIPT_DIR"                                           # Change to that directory
 
-cargo build --release
-mkdir -p ../../bin/
-
 [ "$CARGO_TARGET_DIR" = "" ] && CARGO_TARGET_DIR=target
 
 if [ "$(uname)" == "Darwin" ]; then
-  rm -f ../../lua/nvim_tools.so
-  cp "${CARGO_TARGET_DIR}/release/libnvim_tools.dylib" ../../lua/nvim_tools.so
-  cp "${CARGO_TARGET_DIR}/release/libnvim_tools.dylib" ../../bin/osx.nvim_tools.so
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  rm -f ../../lua/nvim_tools.so
-  cp "${CARGO_TARGET_DIR}/release/libnvim_tools.so" ../../lua/nvim_tools.so
-  cp "${CARGO_TARGET_DIR}/release/libnvim_tools.so" ../../bin/nix.nvim_tools.so
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-  rm -f ../../lua/nvim_tools.dll
-  cp "${CARGO_TARGET_DIR}/release/libnvim_tools.dll" ../../lua/nvim_tools.dll
-  cp "${CARGO_TARGET_DIR}/release/libnvim_tools.dll" ../../bin/win.nvim_tools.dll
-fi
+  if [ ! -f "../../lua/nvim_tools.so" ]; then
+    cargo build --release
+    rm -f ../../lua/nvim_tools.so
+    cp "${CARGO_TARGET_DIR}/release/libnvim_tools.dylib" ../../lua/nvim_tools.so
 
-rm -rf $CARGO_TARGET_DIR
+    mkdir -p ../../bin/
+    cp "${CARGO_TARGET_DIR}/release/libnvim_tools.dylib" ../../bin/osx.nvim_tools.so
+    rm -rf $CARGO_TARGET_DIR
+  fi
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  if [ ! -f "../../lua/nvim_tools.so" ]; then
+    cargo build --release
+    rm -f ../../lua/nvim_tools.so
+    cp "${CARGO_TARGET_DIR}/release/libnvim_tools.so" ../../lua/nvim_tools.so
+
+    mkdir -p ../../bin/
+    cp "${CARGO_TARGET_DIR}/release/libnvim_tools.so" ../../bin/nix.nvim_tools.so
+    rm -rf $CARGO_TARGET_DIR
+  fi
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+  if [ ! -f "../../lua/nvim_tools.dll" ]; then
+    cargo build --release
+    rm -f ../../lua/nvim_tools.dll
+    cp "${CARGO_TARGET_DIR}/release/libnvim_tools.dll" ../../lua/nvim_tools.dll
+
+    mkdir -p ../../bin/
+    cp "${CARGO_TARGET_DIR}/release/libnvim_tools.dll" ../../bin/win.nvim_tools.dll
+    rm -rf $CARGO_TARGET_DIR
+  fi
+fi
 
 echo "Build Done"
