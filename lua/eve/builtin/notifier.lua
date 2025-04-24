f8ff5907ae134b9b94f49fd0ebb2f806810c3328
guanghechen <me.guanghechen@gmail.com>
2025-04-24 14:08:43 +0800

    improve(notifier): some plugin will try to check if the vim.notify is table to assume it's nvim_notify

--- lua/eve/builtin/notifier.lua
+++ lua/eve/builtin/notifier.lua
@@ -123,6 +123,24 @@ end
 
 ---@class eve.builtin.notifier
 local M = {}
+setmetatable(M, {
+  ---@param msg                           string
+  ---@param level0                        integer
+  ---@param opts                          any
+  ---@return nil
+  __call = function(self, msg, level0, opts)
+    opts = opts or {}
+
+    local level = eve.notifier.resolve_level(level0) ---@type eve.builtin.notifier.LevelEnum
+    local group = type(opts.group) == "string" and opts.group or nil ---@type string|nil
+    local title = opts.title or eve.notifier.resolve_title(level) ---@type string
+    local message = type(opts.message) == "string" and opts.message or msg ---@type string
+    local timeout = opts.timeout or 3000 ---@type integer
+    local anonymous = type(opts.anonymous) == "boolean" and opts.anonymous or false ---@type boolean
+    local silent = type(opts.silent) == "boolean" and opts.silent or false ---@type boolean
+    self.notify(level, group, title, message, timeout, anonymous, silent)
+  end,
+})
 
 ---@type eve.std.collection.Scheduler
 local scheduler = eve.std.Scheduler.new({
--- lua/fml/dressing/notifier.lua
+++ lua/fml/dressing/notifier.lua
@@ -1,23 +1,4 @@
----@diagnostic disable-next-line: unused-local
-local original_notify = vim.notify ---@type fun(msg: string, level: integer, opts: any): nil
-
----@param msg                           string
----@param level0                        integer
----@param opts                          any
----@return nil
----@diagnostic disable-next-line: duplicate-set-field
-vim.notify = function(msg, level0, opts)
-  opts = opts or {}
-
-  local level = eve.notifier.resolve_level(level0) ---@type eve.builtin.notifier.LevelEnum
-  local group = type(opts.group) == "string" and opts.group or nil ---@type string|nil
-  local title = opts.title or eve.notifier.resolve_title(level) ---@type string
-  local message = type(opts.message) == "string" and opts.message or msg ---@type string
-  local timeout = opts.timeout or 3000 ---@type integer
-  local anonymous = type(opts.anonymous) == "boolean" and opts.anonymous or false ---@type boolean
-  local silent = type(opts.silent) == "boolean" and opts.silent or false ---@type boolean
-  eve.notifier.notify(level, group, title, message, timeout, anonymous, silent)
-end
+vim.notify = eve.notifier
 
 eve.state.observe({ eve.state.status.notification_level, eve.state.status.notification_paused }, function()
   eve.notifier.schedule()
