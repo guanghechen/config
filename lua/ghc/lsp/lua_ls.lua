1d1f2d37c175500fb99b6a5ebdbf5694c9ca0028
guanghechen <me.guanghechen@gmail.com>
2025-05-11 09:47:54 +0800

    improve(lsp): tweak lua lsp config

--- lua/ghc/lsp/lua_ls.lua
+++ lua/ghc/lsp/lua_ls.lua
@@ -44,7 +44,7 @@ return {
         library = {
           eve.path.join(vim.env.VIMRUNTIME, "lua"),
           eve.path.join(eve.env.HOME_NVIM_DATA, "lazy/lazy.nvim"),
-          eve.path.join(eve.env.HOME_NVIM_CONFIG, "lua"),
+          -- eve.path.join(eve.env.HOME_NVIM_CONFIG, "lua"),
           "${3rd}/luv/library",
         },
       },
