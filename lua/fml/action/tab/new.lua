da40860f7443293a2a9c02dda6b363bd29278c05
guanghechen <me.guanghechen@gmail.com>
2025-05-31 12:22:18 +0800

    fix(tab): fix the action of create tab with given buf

--- lua/fml/action/tab/new.lua
+++ lua/fml/action/tab/new.lua
@@ -8,6 +8,7 @@ function M.new()
   vim.bo.bufhidden = "wipe"
 
   local tabnr = vim.api.nvim_get_current_tabpage() ---@type integer
+  eve.tab.set_type(tabnr, eve.tab.Types.NORMAL)
   eve.tab.resolve(tabnr, false)
   return tabnr
 end
@@ -17,16 +18,19 @@ function M.new_with_buf()
   local bufnr = vim.api.nvim_get_current_buf() ---@type integer
 
   vim.cmd("$tabnew")
+  vim.bo.buflisted = false
+  vim.bo.bufhidden = "wipe"
 
   local tabnr = vim.api.nvim_get_current_tabpage() ---@type integer
   eve.tab.set_type(tabnr, eve.tab.Types.NORMAL)
 
+  local winnr = vim.api.nvim_tabpage_get_win(tabnr) ---@type integer
+  vim.api.nvim_win_set_buf(winnr, bufnr)
   if vim.bo[bufnr].buflisted then
     eve.tab.add_buf(tabnr, bufnr, false)
-  else
-    vim.bo.bufhidden = "wipe"
   end
 
+  eve.tab.resolve(tabnr, false)
   return tabnr
 end
 
