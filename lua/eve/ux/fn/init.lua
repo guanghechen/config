ecc16564df6e8df27ecd973512a03bbb4d84e4e4
guanghechen <me.guanghechen@gmail.com>
2025-06-12 17:53:01 +0800

    :coffin: improve: remove eve.ux.fn.select_files since we no longer need it

--- lua/eve/ux/fn/init.lua
+++ lua/eve/ux/fn/init.lua
@@ -2,7 +2,6 @@
 local __mods = {
   select = "eve.ux.fn.select",
   select_copy_filepath = "eve.ux.fn.select_copy_filepath",
-  select_files = "eve.ux.fn.select_files",
   select_encoding = "eve.ux.fn.select_encoding",
 }
 
@@ -11,7 +10,6 @@ local __mods = {
 ---
 ---@field public select                 fun(params: eve.ux.fn.select.IParams): eve.ux.ISelect
 ---@field public select_copy_filepath   fun(params: eve.ux.fn.select_copy_filepath.IParams): nil
----@field public select_files           fun(params: eve.ux.fn.select_files.IParams): eve.ux.IFileSelect
 ---@field public select_encoding        fun(params: eve.ux.fn.select_encoding.IParams): eve.ux.ISelect
 local M = setmetatable({ __mods = __mods }, {
   __index = function(t, k)
