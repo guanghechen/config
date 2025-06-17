1c25d2d9b8d7ebf5c718eb30796e326207d289d1
guanghechen <me.guanghechen@gmail.com>
2025-06-17 11:19:34 +0800

    :fire: improve: remove unused codes

--- lua/eve/ux/fn/init.lua
+++ lua/eve/ux/fn/init.lua
@@ -1,6 +1,5 @@
 ---@class eve.ux.fn.__mods
 local __mods = {
-  select = "eve.ux.fn.select",
   select_copy_filepath = "eve.ux.fn.select_copy_filepath",
   select_encoding = "eve.ux.fn.select_encoding",
 }
@@ -8,7 +7,6 @@ local __mods = {
 ---@class eve.ux.fn
 ---@field public __mods                 eve.ux.fn.__mods
 ---
----@field public select                 fun(params: eve.ux.fn.select.IParams): eve.ux.ISelect
 ---@field public select_copy_filepath   fun(params: eve.ux.fn.select_copy_filepath.IParams): nil
 ---@field public select_encoding        fun(params: eve.ux.fn.select_encoding.IParams): eve.ux.picker.ListComposer
 local M = setmetatable({ __mods = __mods }, {
