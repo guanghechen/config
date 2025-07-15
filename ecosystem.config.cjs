9763c6350f3c92c33dc150940ddfd24709cb3de0
guanghechen <me.guanghechen@gmail.com>
2025-07-15 10:51:49 +0800

    chore(copilot-api): add config to start the copilot-api

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,18 +1,32 @@
-const path = require('node:path')
+const path = require("node:path");
 
-const ROOT_CONFIG = path.resolve(__dirname, '..')
+const ROOT_CONFIG = path.resolve(__dirname, "..");
+const ROOT_SOURCECODES = process.env.ROOT_SOURCECODES;
+const USERHOME = path.dirname(ROOT_CONFIG);
 
 module.exports = {
   apps: [
     {
-      name: 'yozora',
-      cwd: path.normalize(path.resolve(ROOT_CONFIG, 'yozora')),
-      script: 'npm',
-      args: 'run start',
+      name: "yozora",
+      cwd: path.normalize(path.resolve(ROOT_CONFIG, "yozora")),
+      script: "npm",
+      args: "run start",
       watch: true,
       env: {
-        NODE_ENV: 'development'
-      }
-    }
-  ]
-}
+        NODE_ENV: "development",
+      },
+    },
+    !!ROOT_SOURCECODES && {
+      name: "copilot-api",
+      cwd: path.normalize(
+        path.resolve(ROOT_SOURCECODES, "github/ericc-ch/copilot-api"),
+      ),
+      script: "bun",
+      args: "run start start --port=4343",
+      env: {
+        NODE_ENV: "production",
+        HOST: "127.0.0.1",
+      },
+    },
+  ].filter(Boolean),
+};
