095c74af00ed0a9062362cf1969eca9023bb5f23
guanghechen <me.guanghechen@gmail.com>
2025-07-16 17:14:26 +0800

    improve(FileTypeIcon): support className

--- src/component/icon/filetype.tsx
+++ src/component/icon/filetype.tsx
@@ -1,3 +1,4 @@
+import cn from 'clsx'
 import React from 'react'
 
 export const ChevronDownIcon: React.FC<{ className?: string }> = () => {
@@ -16,21 +17,21 @@ export const FolderOpenIcon: React.FC<{ className?: string }> = () => {
   return <span className="mr-2 text-blue-300"> </span>
 }
 
-export const FileTypeIcon: React.FC<{ extname: string }> = props => {
-  const { extname } = props
+export const FileTypeIcon: React.FC<{ extname: string; className?: string }> = props => {
+  const { extname, className } = props
 
   switch (extname) {
     case '.md':
-      return <span className="mx-1 text-gray-700">󰍔 </span>
+      return <span className={cn('mx-1 text-gray-700', className)}>󰍔 </span>
     case '.json':
-      return <span className="mx-1 text-yellow-700">󰘦 </span>
+      return <span className={cn('mx-1 text-yellow-700', className)}>󰘦 </span>
     case '.pdf':
-      return <span className="mx-1 text-yellow-700"> </span>
+      return <span className={cn('mx-1 text-yellow-700', className)}> </span>
     case '.png':
     case '.jpg':
     case '.jpeg':
-      return <span className="mx-1 text-blue-700">󰉏 </span>
+      return <span className={cn('mx-1 text-blue-700', className)}>󰉏 </span>
     default:
-      return <span className="mx-1 text-pink-700">󰈚 </span>
+      return <span className={cn('mx-1 text-pink-700', className)}>󰈚 </span>
   }
 }
