fc61ae5199e267e2805cb3ebe5aeaa4f6293bbc0
guanghechen <me.guanghechen@gmail.com>
2025-07-17 15:58:33 +0800

    improve(win): update envs

--- win/setup.ps1
+++ win/setup.ps1
@@ -4,6 +4,10 @@ $env:XDG_CONFIG_HOME    = "$env:USERPROFILE\.config"
 $env:XDG_DATA_HOME      = "$env:USERPROFILE\.local\share"
 $env:XDG_STATE_HOME     = "$env:USERPROFILE\.local\state"
 $env:CLAUDE_CONFIG_DIR  = "$env:XDG_CONFIG_HOME\claude"
+$env:LG_CONFIG_FILE     = "$env:XDG_CONFIG_HOME\lazygit\config.yml,$env:XDG_CONFIG_HOME\lazygit\local\theme.yml"
+$env:NODE_OPTIONS       = "--max-old-space-size=8192"
+$env:PYTHONIOENCODING   = "utf8"
+$env:PYTHONUTF8         = 1
 $env:YAZI_CONFIG_HOME   = "$env:XDG_CONFIG_HOME\yazi"
 $env:YAZI_FILE_ONE      = "$env:APP_HOME_GIT\usr\bin\file.exe"
 
@@ -13,6 +17,10 @@ setx XDG_CONFIG_HOME      "$env:XDG_CONFIG_HOME"
 setx XDG_DATA_HOME        "$env:XDG_DATA_HOME"
 setx XDG_STATE_HOME       "$env:XDG_STATE_HOME"
 setx CLAUDE_CONFIG_DIR    "$env:CLAUDE_CONFIG_DIR"
+setx LG_CONFIG_FILE       "$env:LG_CONFIG_FILE"
+setx NODE_OPTIONS         "$env:NODE_OPTIONS"
+setx PYTHONIOENCODING     "$env:PYTHONIOENCODING"
+setx PYTHONUTF8           $env:PYTHONUTF8
 setx YAZI_CONFIG_HOME     "$env:YAZI_CONFIG_HOME"
 setx YAZI_FILE_ONE        "$env:YAZI_FILE_ONE"
 
