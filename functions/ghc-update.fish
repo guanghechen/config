0f1dfe846504b695cb567a6224217b40c51e6f4e
guanghechen <me.guanghechen@gmail.com>
2025-07-18 16:33:07 +0800

    improve(ghc-update): print prettier message while pull/clone repo

--- functions/ghc-update.fish
+++ functions/ghc-update.fish
@@ -1,33 +1,30 @@
 function ghc-update
-    set required_configs btop conda fish fzf guanghechen lazygit lsd nvim pm2 ripgrep tmux yazi yozora
-    set optional_configs alacritty alacritty-windows claude ghostty helix kitty neovide nvim-nvchad opencode plan pwsh tsuki wezterm
-    set base_path "$HOME/.config"
+  set required_configs btop conda fish fzf guanghechen lazygit lsd nvim pm2 ripgrep tmux yazi yozora
+  set optional_configs alacritty alacritty-windows claude ghostty helix kitty neovide nvim-nvchad opencode plan pwsh tsuki wezterm
+  set reporoot "$HOME/.config"
 
-    for config in $required_configs
-        set dir "$base_path/$config"
-        if test -d "$dir"
-            set cmd "cd $dir && git pull origin $config"
-        else
-            set cmd "git clone https://github.com/guanghechen/config.git --single-branch --branch=$config $dir"
-        end
-
-        set_color white
-        printf "\n%s\n" $cmd
-
-        set_color normal
-        fish -c "$cmd"
+  for branch in $required_configs
+    set repopath "$reporoot/$branch"
+    if test -d "$repopath"
+      printf "\e[34mfetching $branch into $repopath\e[0m\n"
+      set cmd "git -C $repopath pull origin $branch"
+    else
+      printf "\e[34mcloning $branch into $repopath\e[0m\n"
+      set cmd "git clone https://github.com/guanghechen/config.git --single-branch --branch=$branch $repopath"
     end
 
-    for config in $optional_configs
-        set dir "$base_path/$config"
-        if test -d "$dir"
-            set cmd "cd $dir && git pull origin $config"
+    fish -c "$cmd"
+    printf "\n"
+  end
 
-            set_color white
-            printf "\n%s\n" $cmd
+  for branch in $optional_configs
+    set repopath "$reporoot/$branch"
+    if test -d "$repopath"
+      printf "\e[34mfetching $branch into $repopath\e[0m\n"
+      set cmd "git -C $repopath pull origin $branch"
 
-            set_color normal
-            fish -c "$cmd"
-        end
+      fish -c "$cmd"
+      printf "\n"
     end
+  end
 end
