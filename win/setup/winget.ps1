e5bebf3bfc58a1024ee90b9fd6b006bcca0eef8d
guanghechen <me.guanghechen@gmail.com>
2025-07-25 13:27:06 +0800

    chore(win): install komorebi

--- win/setup/winget.ps1
+++ win/setup/winget.ps1
@@ -19,6 +19,7 @@ winget install -e --source winget --id lsd-rs.lsd
 winget install -e --source winget --id BurntSushi.ripgrep.MSVC
 winget install -e --source winget --id sxyazi.yazi
 winget install -e --source winget --id ajeetdsouza.zoxide
+winget install -e --source winget --id LGUG2Z.komorebi
 
 
 ## Cargo version is updated too frequently and sometimes could build failed.
