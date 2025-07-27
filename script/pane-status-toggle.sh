38ada6c821bc09c08049b67e0b09a85286a81979
guanghechen <me.guanghechen@gmail.com>
2025-07-27 12:27:37 +0800

    feat: support toggle different pane status format

--- script/pane-status-toggle.sh
+++ script/pane-status-toggle.sh
@@ -2,21 +2,15 @@
 
 function _ghc_tmux_pane_status_toggle_ {
   local mode=$(tmux show -gqv @GHC_PSL_MODE)
-  if [ -z "$mode" ]; then
-    mode="01"
-    tmux set -g @GHC_PSL_MODE "$mode"
-  elif [ "$mode" == "00" ]; then
-    mode="01"
-    tmux set -g @GHC_PSL_MODE "$mode"
-  elif [ "$mode" == "01" ]; then
-    mode="00"
-    tmux set -g @GHC_PSL_MODE "$mode"
-  fi
 
-  if [ "$mode" == "00" ]; then
-    tmux set-option pane-border-status off
+  if [ -z "$mode" ]; then
+    tmux set -g @GHC_PSL_MODE "01"
+  elif [ "$mode" == "00" ]; then
+    tmux set -g @GHC_PSL_MODE "01"
   elif [ "$mode" == "01" ]; then
-    tmux set-option pane-border-status top
+    tmux set -g @GHC_PSL_MODE "02"
+  elif [ "$mode" == "02" ]; then
+    tmux set -g @GHC_PSL_MODE "00"
   fi
 
   tmux source-file "$HOME/.config/tmux/conf/variable.tmux.conf"
--- script/status-load.sh
+++ script/status-load.sh
@@ -1,34 +1,45 @@
 #! /usr/bin/env bash
 
 function _ghc_tmux_status_load_ {
-  local status_mode=$(tmux show -gqv @GHC_SL_MODE)
-
   # Determine platform
-  if [[ "$(uname)" == "Darwin" ]]; then
-    platform="mac"
-  elif grep -qEi "(Microsoft|WSL)" /proc/version; then
-    platform="wsl"
-  else
-    platform="nix"
+  # if [[ "$(uname)" == "Darwin" ]]; then
+  #   platform="mac"
+  # elif grep -qEi "(Microsoft|WSL)" /proc/version; then
+  #   platform="wsl"
+  # else
+  #   platform="nix"
+  # fi
+
+  local status_mode=$(tmux show -gqv @GHC_SL_MODE)
+  if [ -z "$status_mode" ]; then
+    status_mode="01"
+    tmux set -g @GHC_SL_MODE "$status_mode"
   fi
 
-  if [ -z "$status_mode" ]; then
-    if [[ "$(uname)" == "Darwin" ]]; then
-      status_mode="02"
-    else
-      status_mode="01"
-    fi
-    tmux set -g @GHC_SL_MODE "$status_mode"
+  local panestatus_mode=$(tmux show -gqv @GHC_PSL_MODE)
+  if [ -z "$panestatus_mode" ]; then
+    panestatus_mode="01"
+    tmux set -g @GHC_PSL_MODE "$panestatus_mode"
   fi
 
   if [ "$status_mode" == "00" ]; then
     tmux set-option status off
   elif [ "$status_mode" == "01" ]; then
-    tmux source-file "$HOME/.config/tmux/conf/$platform/theme01.tmux.conf"
+    tmux source-file "$HOME/.config/tmux/conf/theme/status01.tmux.conf"
     tmux set-option status on
   elif [ "$status_mode" == "02" ]; then
-    tmux source-file "$HOME/.config/tmux/conf/$platform/theme02.tmux.conf"
+    tmux source-file "$HOME/.config/tmux/conf/theme/status02.tmux.conf"
     tmux set-option status on
+  fi
+
+  if [ "$panestatus_mode" == "00" ]; then
+    tmux set-option pane-border-status off
+  elif [ "$panestatus_mode" == "01" ]; then
+    tmux source-file "$HOME/.config/tmux/conf/theme/panestatus01.tmux.conf"
+    tmux set-option pane-border-status top
+  elif [ "$panestatus_mode" == "02" ]; then
+    tmux source-file "$HOME/.config/tmux/conf/theme/panestatus02.tmux.conf"
+    tmux set-option pane-border-status top
   fi
 }
 
