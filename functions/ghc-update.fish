cd62ec3315059322585f1bc01ab3c23a7c78cd3d
guanghechen <me.guanghechen@gmail.com>
2025-07-27 16:25:58 +0800

    improve: sync skhd and yabai as optional configs

--- functions/ghc-update.fish
+++ functions/ghc-update.fish
@@ -1,30 +1,30 @@
 function ghc-update
-  set required_configs btop conda fish fzf guanghechen lazygit lsd nvim pm2 ripgrep tmux yazi yozora
-  set optional_configs alacritty alacritty-windows claude ghostty helix kitty neovide nvim-nvchad opencode plan pwsh tsuki wezterm
-  set reporoot "$HOME/.config"
+    set required_configs btop conda fish fzf guanghechen lazygit lsd nvim pm2 ripgrep tmux yazi yozora
+    set optional_configs alacritty alacritty-windows claude ghostty helix kitty neovide nvim-nvchad opencode plan pwsh skhd tsuki wezterm yabai
+    set reporoot "$HOME/.config"
 
-  for branch in $required_configs
-    set repopath "$reporoot/$branch"
-    if test -d "$repopath"
-      printf "\e[34mfetching $branch into $repopath\e[0m\n"
-      set cmd "git -C $repopath pull origin $branch"
-    else
-      printf "\e[34mcloning $branch into $repopath\e[0m\n"
-      set cmd "git clone https://github.com/guanghechen/config.git --single-branch --branch=$branch $repopath"
+    for branch in $required_configs
+        set repopath "$reporoot/$branch"
+        if test -d "$repopath"
+            printf "\e[34mfetching $branch into $repopath\e[0m\n"
+            set cmd "git -C $repopath pull origin $branch"
+        else
+            printf "\e[34mcloning $branch into $repopath\e[0m\n"
+            set cmd "git clone https://github.com/guanghechen/config.git --single-branch --branch=$branch $repopath"
+        end
+
+        fish -c "$cmd"
+        printf "\n"
     end
 
-    fish -c "$cmd"
-    printf "\n"
-  end
+    for branch in $optional_configs
+        set repopath "$reporoot/$branch"
+        if test -d "$repopath"
+            printf "\e[34mfetching $branch into $repopath\e[0m\n"
+            set cmd "git -C $repopath pull origin $branch"
 
-  for branch in $optional_configs
-    set repopath "$reporoot/$branch"
-    if test -d "$repopath"
-      printf "\e[34mfetching $branch into $repopath\e[0m\n"
-      set cmd "git -C $repopath pull origin $branch"
-
-      fish -c "$cmd"
-      printf "\n"
+            fish -c "$cmd"
+            printf "\n"
+        end
     end
-  end
 end
