b46c0688459c634d7c249c8c8589e89b8e8e2e3f
guanghechen <me.guanghechen@gmail.com>
2025-07-28 13:00:51 +0800

    improve: refine ghc-update since we migrated to the git worktree

--- functions/ghc-update.fish
+++ functions/ghc-update.fish
@@ -1,16 +1,27 @@
 function ghc-update
-    set required_configs btop conda fish fzf guanghechen lazygit lsd nvim pm2 ripgrep tmux yazi yozora
-    set optional_configs alacritty alacritty-windows claude ghostty helix kitty neovide nvim-nvchad opencode plan pwsh skhd tsuki wezterm yabai
     set reporoot "$HOME/.config"
+    set repomain "$reporoot/guanghechen"
+
+    if test -d "$repomain/.git"
+        git -C $repomain fetch origin
+        git -C $repomain merge origin/guanghechen --ff-only
+    else
+        git clone https://github.com/guanghechen/config.git --branch=guanghechen $repomain
+    end
+
+    #----------------------------------------------------------------------------------------------#
+
+    set required_configs btop conda fish fzf lazygit lsd nvim pm2 ripgrep tmux yazi yozora
+    set optional_configs alacritty alacritty-windows claude ghostty helix kitty komorebi neovide nvim-lazy nvim-nvchad plan pwsh skhd tsuki wezterm yabai yasb
 
     for branch in $required_configs
         set repopath "$reporoot/$branch"
         if test -d "$repopath"
-            printf "\e[34mfetching $branch into $repopath\e[0m\n"
-            set cmd "git -C $repopath pull origin $branch"
+            printf "\e[34mmerging origin/$branch into $repopath\e[0m\n"
+            set cmd "git -C '$repopath' merge origin/$branch"
         else
-            printf "\e[34mcloning $branch into $repopath\e[0m\n"
-            set cmd "git clone https://github.com/guanghechen/config.git --single-branch --branch=$branch $repopath"
+            printf "\e[34madd new worktree of $branch into $repopath\e[0m\n"
+            set cmd "git -C '$repomain' worktree add '$repopath' $branch"
         end
 
         fish -c "$cmd"
@@ -20,8 +31,8 @@ function ghc-update
     for branch in $optional_configs
         set repopath "$reporoot/$branch"
         if test -d "$repopath"
-            printf "\e[34mfetching $branch into $repopath\e[0m\n"
-            set cmd "git -C $repopath pull origin $branch"
+            printf "\e[34mmerging origin/$branch into $repopath\e[0m\n"
+            set cmd "git -C '$repopath' merge origin/$branch"
 
             fish -c "$cmd"
             printf "\n"
