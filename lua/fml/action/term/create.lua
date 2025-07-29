c8bfeb7d93c2fb2f63b09dbe1121ce0cc6e92d69
guanghechen <me.guanghechen@gmail.com>
2025-07-29 21:13:07 +0800

    improve(terminal): tweak toggle

--- lua/fml/action/term/create.lua
+++ lua/fml/action/term/create.lua
@@ -101,31 +101,27 @@ function M.toggle()
   local cwd = std.path.cwd()
   local terminal = eve.ux.widget.Terminal ---@type eve.ux.widget.Terminal
 
-  if eve.term.size() > 0 then
-    terminal:toggle()
+  local _, termmeta = eve.term.find_index_by_type("shell") ---@type integer, eve.builtin.term.IMeta|nil
+  if termmeta == nil then
+    terminal:toggle_and_focus({
+      uuid = oxi.fn.uuid(),
+      type = "shell",
+      name = "shell",
+      cwd = cwd,
+      autofocus = true,
+      permanent = true,
+      selected_text = eve.buf.retrieve_selected_text(),
+    })
   else
-    local _, termmeta = eve.term.find_index_by_type("shell") ---@type integer, eve.builtin.term.IMeta|nil
-    if termmeta == nil then
-      terminal:toggle_and_focus({
-        uuid = oxi.fn.uuid(),
-        type = "shell",
-        name = "shell",
-        cwd = cwd,
-        autofocus = true,
-        permanent = true,
-        selected_text = eve.buf.retrieve_selected_text(),
-      })
-    else
-      terminal:toggle_and_focus({
-        uuid = termmeta.uuid,
-        type = termmeta.type,
-        name = termmeta.name,
-        cwd = termmeta.cwd,
-        autofocus = true,
-        permanent = true,
-        selected_text = eve.buf.retrieve_selected_text(),
-      })
-    end
+    terminal:toggle_and_focus({
+      uuid = termmeta.uuid,
+      type = termmeta.type,
+      name = termmeta.name,
+      cwd = termmeta.cwd,
+      autofocus = true,
+      permanent = true,
+      selected_text = eve.buf.retrieve_selected_text(),
+    })
   end
 end
 
