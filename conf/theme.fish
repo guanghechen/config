12514014a9205583a09e271855c526ec770bbfdd
guanghechen <me.guanghechen@gmail.com>
2025-08-09 20:54:11 +0800

    improve: tweak theme

--- conf/theme.fish
+++ conf/theme.fish
@@ -3,41 +3,51 @@
 # set_color --print-colors
 #
 
-
-set -U  fish_color_autosuggestion                white
-set -U  fish_color_cancel                        --reverse
+# Core syntax highlighting
 set -U  fish_color_command                       brblue
-set -U  fish_color_comment                       white
-set -U  fish_color_completion                    white
-set -U  fish_color_dir                           brcyan
-set -U  fish_color_end                           bryellow
-set -U  fish_color_error                         red
-set -U  fish_color_escape                        brcyan
-set -U  fish_color_gray                          white
-set -U  fish_color_history_current               --bold
-set -U  fish_color_host_remote                   normal
-set -U  fish_color_keyword                       normal
-set -U  fish_color_operator                      bryellow
-set -U  fish_color_option                        normal
+set -U  fish_color_keyword                       brmagenta
+set -U  fish_color_param                         cyan
+set -U  fish_color_option                        green
 set -U  fish_color_quote                         brgreen
-set -U  fish_color_match                         bryellow
-set -U  fish_color_normal                        normal
-set -U  fish_color_param                         brcyan
 set -U  fish_color_redirection                   brmagenta
-set -U  fish_color_search_match                  --background=bryellow
-set -U  fish_color_search_selection              --background=bryellow
-set -U  fish_color_status                        brred
+set -U  fish_color_end                           bryellow
+set -U  fish_color_operator                      yellow
+
+# Interactive elements
+set -U  fish_color_autosuggestion                brblack
+set -U  fish_color_completion                    normal
+set -U  fish_color_comment                       brblack
+set -U  fish_color_gray                          brblack
+
+# Status and feedback
+set -U  fish_color_error                         brred
+set -U  fish_color_status                        red
+set -U  fish_color_cancel                        --reverse
+set -U  fish_color_escape                        cyan
+set -U  fish_color_normal                        normal
+set -U  fish_color_dir                           brblue
 set -U  fish_color_valid_path                    --underline
+
+# Search and selection
+set -U  fish_color_search_match                  --background=brorange
+set -U  fish_color_search_selection              --background=brblue
+set -U  fish_color_match                         --background=brblue
+set -U  fish_color_history_current               --bold
+
+# Remote host
+set -U  fish_color_host_remote                   yellow
+
+# Pager colors
 set -U  fish_pager_color_background              normal
 set -U  fish_pager_color_completion              normal
-set -U  fish_pager_color_description             white
-set -U  fish_pager_color_prefix                  normal --bold --underline
-set -U  fish_pager_color_progress                brwhite --background=cyan
+set -U  fish_pager_color_description             yellow
+set -U  fish_pager_color_prefix                  cyan --bold
+set -U  fish_pager_color_progress                brwhite --background=blue
 set -U  fish_pager_color_secondary_background    normal
-set -U  fish_pager_color_secondary_completion    normal
-set -U  fish_pager_color_secondary_description   normal
-set -U  fish_pager_color_secondary_prefix        normal
-set -U  fish_pager_color_selected_background     --background=bryellow
-set -U  fish_pager_color_selected_completion     normal
-set -U  fish_pager_color_selected_description    normal
-set -U  fish_pager_color_selected_prefix         normal
+set -U  fish_pager_color_secondary_completion    brblack
+set -U  fish_pager_color_secondary_description   brblack
+set -U  fish_pager_color_secondary_prefix        brblack
+set -U  fish_pager_color_selected_background     --background=blue
+set -U  fish_pager_color_selected_completion     brwhite
+set -U  fish_pager_color_selected_description    brwhite
+set -U  fish_pager_color_selected_prefix         brwhite
