88c7df9c27d531f39032b213cfd2cc0672b5c489
guanghechen <me.guanghechen@gmail.com>
2025-08-17 22:30:24 +0800

    fix: tweak scrollbar style

--- shared/constant/classes.ts
+++ shared/constant/classes.ts
@@ -1,4 +1,4 @@
 export const PRESET_CLASSES = {
   scrollbar:
-    '[&::-webkit-scrollbar-thumb]:rounded [&::-webkit-scrollbar-thumb]:bg-gray-300 [&::-webkit-scrollbar-thumb]:hover:bg-gray-400 dark:[&::-webkit-scrollbar-thumb]:bg-gray-600 dark:[&::-webkit-scrollbar-thumb]:hover:bg-gray-500 [&::-webkit-scrollbar-track]:bg-gray-100 dark:[&::-webkit-scrollbar-track]:bg-gray-800 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar]:h-2',
+    '[&::-webkit-scrollbar-thumb]:rounded [&::-webkit-scrollbar-thumb]:bg-gray-300 [&::-webkit-scrollbar-thumb]:hover:bg-gray-400 dark:[&::-webkit-scrollbar-thumb]:bg-gray-500 dark:[&::-webkit-scrollbar-thumb]:hover:bg-gray-400 [&::-webkit-scrollbar-track]:bg-gray-100 dark:[&::-webkit-scrollbar-track]:bg-gray-900 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar]:h-2',
 }
