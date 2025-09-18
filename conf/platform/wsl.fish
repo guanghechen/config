b1a06bfc0cad6531a14d12c99cb740fdcf1e6ebe
guanghechen <me.guanghechen@gmail.com>
2025-09-18 14:30:28 +0800

    improve(wsl): refine the open and start function

--- conf/platform/wsl.fish
+++ conf/platform/wsl.fish
@@ -7,8 +7,6 @@ fish_add_path --append "/mnt/c/Program Files/PowerShell/7/"
 alias chmod='chmod --preserve-root' # the `--preserve-root` option not worked in MacOS.
 alias code='env -u TMUX -u TERM /mnt/c/app/vscode/bin/code'
 alias cursor='env -u TMUX -u TERM /mnt/c/app/cursor/resources/app/bin/cursor'
-alias open="explorer.exe"
-alias start="cmd.exe /start"
 alias pbpaste="powershell.exe Get-Clipboard >"
 
 ## Abbr
@@ -19,4 +17,25 @@ if command -v ipconfig.exe >/dev/null
     set -gx ghc_vpn_host_ip (ipconfig.exe | grep 'IPv4 Address' | awk '{print $NF}' | grep 192 | head -1 | sed 's/[^0-9.]//g')
 else
     set -gx ghc_vpn_host_ip (cat /etc/resolv.conf | grep nameserver | awk '{print $2}' | grep -v '::' | head -1)
+end
+
+## functions
+function open
+    if test (count $argv) -eq 0
+        echo "Usage: open <path>"
+        return 1
+    end
+
+    set winpath (wslpath -w $argv[1])
+    explorer.exe "$winpath"
+end
+
+function start
+    if test (count $argv) -eq 0
+        echo "Usage: open <path>"
+        return 1
+    end
+
+    set winpath (wslpath -w $argv[1])
+    cmd.exe /c start "" "$winpath"
 end
