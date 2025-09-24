4e6433f229b20ffe4b6069810effec56b7fcad83
guanghechen <me.guanghechen@gmail.com>
2025-09-24 17:19:10 +0800

    improve: install git-lfs by default

--- nix/setup/homebrew.sh
+++ nix/setup/homebrew.sh
@@ -37,4 +37,4 @@ brew install btop fastfetch httpie
 
 ### Install usual tools
 printf "\n\e[94m  [setup homebrew] installing usual tools...\n"
-brew install automake bat duf ffmpeg fzf hyperfine jq lsd scc tldr tree tty-clock unzip zoxide
+brew install automake bat duf ffmpeg fzf git-lfs hyperfine jq lsd scc tldr tree tty-clock unzip zoxide
