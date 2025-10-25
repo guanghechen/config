1a5b56e9aa910f57700d1291130299c13ce7d8cb
guanghechen <me.guanghechen@gmail.com>
2025-10-25 13:33:05 +0800

    improve: fix lint

--- functions/ghc-upgrade.fish
+++ functions/ghc-upgrade.fish
@@ -1,13 +1,12 @@
 function ghc-upgrade
-  if set -q SSH_CONNECTION; or set -q SSH_CLIENT; or set -q SSH_TTY
-    bash $HOME/.config/guanghechen/nix-remote/setup.sh
-    return
-  else
-
-    if test (uname) = Darwin
-      bash $HOME/.config/guanghechen/osx/setup.sh
+    if set -q SSH_CONNECTION; or set -q SSH_CLIENT; or set -q SSH_TTY
+        bash $HOME/.config/guanghechen/nix-remote/setup.sh
+        return
     else
-      bash $HOME/.config/guanghechen/nix/setup.sh
+        if test (uname) = Darwin
+            bash $HOME/.config/guanghechen/osx/setup.sh
+        else
+            bash $HOME/.config/guanghechen/nix/setup.sh
+        end
     end
-  end
 end
