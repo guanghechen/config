74e277f37ce179567de209fb9a4f6acb37937ac3
guanghechen <me.guanghechen@gmail.com>
2025-11-10 13:25:35 +0800

    fix(lsp): fix the missing taplo lsp config warnings

---
+++ lsp/taplo.lua
@@ -0,0 +1,38 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/5a49a97f9d3de5c39a2b18d583035285b3640cb0/lsp/taplo.lua
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#taplo
+
+---@param params                        lsp.InitializeParams
+---@param config                        table
+local function before_init(params, config)
+  eve.lsp.before_init(params, config)
+end
+
+---@param client                        vim.lsp.Client
+---@param bufnr                         integer
+local function on_attach(client, bufnr)
+  eve.lsp.on_attach(client, bufnr)
+end
+
+---@param client                        vim.lsp.Client
+---@param bufnr                         integer
+local function on_detach(client, bufnr)
+  eve.lsp.on_detach(client, bufnr)
+end
+
+---@param client                        vim.lsp.Client
+---@param config                        any
+local function on_init(client, config)
+  eve.lsp.on_init(client, config)
+end
+
+---@type vim.lsp.Config
+return {
+  capabilities = eve.lsp.get_capabilities(),
+  cmd = { "taplo", "lsp", "stdio" },
+  filetypes = { "toml" },
+  root_markers = { ".taplo.toml", "taplo.toml", ".git" },
+  before_init = before_init,
+  on_attach = on_attach,
+  on_detach = on_detach,
+  on_init = on_init,
+}
