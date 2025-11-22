ef6821455b3ca8e5a842a1e035bf69f7a9478836
guanghechen <me.guanghechen@gmail.com>
2025-11-22 18:29:19 +0800

    fix admonition renderer

--- src/component/icon/admonition.tsx
+++ src/component/icon/admonition.tsx
@@ -103,7 +103,7 @@ export const AdmonitionDangerIcon: React.FC<IIconProps> = props => {
 }
 
 /**
- * Hint icon (Idea lightbulb outlined)
+ * Hint icon (Circle exclamation)
  */
 export const AdmonitionHintIcon: React.FC<IIconProps> = props => {
   const { fill = 'currentColor', className } = props
@@ -116,7 +116,7 @@ export const AdmonitionHintIcon: React.FC<IIconProps> = props => {
       fill={fill}
       className={className}
     >
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-4h2v2h-2zm1-10c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z" />
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-14h2v8h-2zm0 10h2v2h-2z" />
     </svg>
   )
 }
