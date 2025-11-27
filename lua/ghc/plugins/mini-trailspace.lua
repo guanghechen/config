930a02c337e284a5dc1b27aa004d1e06ce9ae6fd
guanghechen <me.guanghechen@gmail.com>
2025-11-27 22:27:34 +0800

    feat: use the mini.trailspace

---
+++ lua/ghc/plugins/mini-trailspace.lua
@@ -0,0 +1,9 @@
+---@see https://github.com/nvim-mini/mini.trailspace/tree/f8083ca969e1b2098480c10f3c3c4d2ce3586680
+
+return {
+  name = "mini.trailspace",
+  event = "VeryLazy",
+  opts = {
+    only_in_normal_buffers = true,
+  },
+}
