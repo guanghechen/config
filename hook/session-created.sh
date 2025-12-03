96867ca6bc76953864e85cf69a29e62fa8b781d4
guanghechen <me.guanghechen@gmail.com>
2025-12-03 11:31:39 +0800

    improve: mark the __agent__|_popup@* session aggressive-resize on

--- hook/session-created.sh
+++ hook/session-created.sh
@@ -6,11 +6,10 @@ function _ghc_tmux_hook_session_created {
 
   # Check if the session name starts with "_popup" or is the agent session
   if [[ "${new_session_name}" == _popup@* ]] || [[ "${new_session_name}" == __agent__ ]]; then
-    # Hide the status line and enable detach-on-destroy
+    tmux set-option -t "${new_session_name}" aggressive-resize on
     tmux set-option -t "${new_session_name}" status off
     tmux set-option -t "${new_session_name}" detach-on-destroy on
   else
-    # Show the status line
     tmux set-option -t "${new_session_name}" status on
     tmux rename-window -t "${new_session_name}:1" "${new_session_name}" 2>/dev/null || true
   fi
