5e0b82e60be4620dbc85c5b9d9721c450904c878
guanghechen <me.guanghechen@gmail.com>
2025-12-06 17:26:45 +0800

    improve(ghc-theme-apply): add completions for theme names

---
+++ completions/ghc-theme-apply.fish
@@ -0,0 +1,21 @@
+complete ghc-theme-apply --no-files
+complete ghc-theme-apply --arguments "
+  catppuccin-frappe
+  catppuccin-latte
+  catppuccin-macchiato
+  catppuccin-mocha
+  gruvbox-dark
+  gruvbox-light
+  nord
+  onehalf-dark
+  onehalf-light
+  rosepine-dawn
+  rosepine-main
+  rosepine-moon
+  tokyonight-day
+  tokyonight-moon
+  tokyonight-night
+  tokyonight-storm
+  vsc-dark-modern
+  vsc-light-modern
+"
