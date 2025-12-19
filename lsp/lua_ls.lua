f0c3a937fd17d6df913d0656ed51b460f6752bbc
guanghechen <me.guanghechen@gmail.com>
2025-12-19 15:40:12 +0800

    :zap: improve(lsp): remove redundant workspace library entries for lua_ls
    Remove nvim_config and lazy.nvim from workspace.library to prevent
    lua-language-server from analyzing the same files twice, significantly
    reducing LSP startup time.

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,14 +1,6 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/5a49a97f9d3de5c39a2b18d583035285b3640cb0/lsp/lua_ls.lua
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
 
-local nvim_config = nil ---@type string|nil
-local workspace = dot.path.workspace() ---@type string
-if workspace == dot.path.locate_app_config_home("nvim") then
-  nvim_config = dot.path.join(workspace, "lua")
-elseif workspace == dot.path.locate_app_config_home("nvim-nvchad") then
-  nvim_config = dot.path.join(workspace, "lua")
-end
-
 ---@param params                        lsp.InitializeParams
 ---@param config                        table
 local function before_init(params, config)
@@ -84,12 +76,10 @@ return {
       },
       workspace = {
         checkThirdParty = false,
-        library = vim.tbl_filter(ark.fn.boolean, {
+        library = {
           dot.path.join(vim.env.VIMRUNTIME, "lua"),
-          dot.path.join(ark.env.HOME_NVIM_DATA, "lazy/lazy.nvim"),
-          nvim_config,
           "${3rd}/luv/library",
-        }),
+        },
       },
     },
   },
