f869978111404378671a1a9a6dd54c5e00aa9ec0
guanghechen <me.guanghechen@gmail.com>
2025-12-23 19:33:54 +0800

    :bug: fix(cmp): auto show docs with preselect

--- lua/ghc/plugins/blink-cmp.lua
+++ lua/ghc/plugins/blink-cmp.lua
@@ -111,7 +111,7 @@ return {
         },
         list = {
           selection = {
-            preselect = false,
+            preselect = true,
             auto_insert = true,
           },
         },
