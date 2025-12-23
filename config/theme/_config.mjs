91898abc0d6d695b8afe35bd66af7a9968065ede
guanghechen <me.guanghechen@gmail.com>
2025-12-23 20:30:32 +0800

    :truck: rename(nvim): update theme namespace paths and type annotations
    - Change nvim theme path from lua/dot to lua/dark
    - Change nvim-nvchad theme path from lua/dot to lua/ark
    - Update type annotation from dot.t.theme to ark.t.theme

--- config/theme/_config.mjs
+++ config/theme/_config.mjs
@@ -121,7 +121,7 @@ export const apps = [
   {
     name: 'nvim',
     home: path.join(XDG_CONFIG_HOME, 'nvim'),
-    themes: 'lua/dot/theme/scheme/',
+    themes: 'lua/ark/theme/scheme/',
     extname: '.lua',
     local: null,
     active: app => is_directory(app.home),
@@ -137,7 +137,7 @@ export const apps = [
   {
     name: 'nvim-nvchad',
     home: path.join(XDG_CONFIG_HOME, 'nvim-nvchad'),
-    themes: 'lua/dot/theme/scheme/',
+    themes: 'lua/ark/theme/scheme/',
     extname: '.lua',
     local: null,
     active: app => is_directory(app.home),
