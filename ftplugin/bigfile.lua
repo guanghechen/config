4de0a273e29966db0866ef35557b1fd824c592f7
guanghechen <me.guanghechen@gmail.com>
2025-12-24 11:33:17 +0800

    :zap: improve(bigfile): enhance performance optimizations for large files
    Disable additional features to improve editing performance: miniindentscope,
    swapfile, treesitter highlighting; reduce undolevels; set manual foldmethod
    and disable spell checking.

--- ftplugin/bigfile.lua
+++ ftplugin/bigfile.lua
@@ -1,16 +1,26 @@
 local __module_name__ = "ftplugin.bigfile"
 
-local bufnr = vim.api.nvim_get_current_buf()
-local filepath = vim.api.nvim_buf_get_name(bufnr)
+local bufnr = vim.api.nvim_get_current_buf() ---@type integer
+local winnr = vim.api.nvim_get_current_win() ---@type integer
+local filepath = vim.api.nvim_buf_get_name(bufnr) ---@type string
 
 vim.b[bufnr].completion = false
 vim.b[bufnr].minihipatterns_disable = true
+vim.b[bufnr].miniindentscope_disable = true
+
+vim.bo[bufnr].swapfile = false
+vim.bo[bufnr].undolevels = 100
+
+vim.wo[winnr].foldmethod = "manual"
+vim.wo[winnr].spell = false
+
+pcall(vim.treesitter.stop, bufnr)
 
 vim.schedule(function()
   if not vim.api.nvim_buf_is_valid(bufnr) then
     return
   end
-  local filetype = vim.filetype.match({ buf = bufnr }) or ""
+  local filetype = vim.filetype.match({ buf = bufnr }) or "" ---@type string
   vim.bo[bufnr].syntax = filetype
 end)
 
