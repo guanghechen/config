76367abb7abe8b348be601e1addd9ea8ee0e211c
guanghechen <me.guanghechen@gmail.com>
2025-12-28 22:15:22 +0800

    :truck: move(command): relocate explorer commands from ghc to fml
    Move dot.widget.explorer command implementations to fml/command.lua
    since they depend only on dot module. Keep diffview-specific toggle
    in ghc/command.lua as it requires ghc.action.diffview.

--- lua/fml/command.lua
+++ lua/fml/command.lua
@@ -370,6 +370,52 @@ command
     end,
   })
 
+--[explorer] ---------------------------------------------------------------------------------------
+command
+  .implement({
+    uuid = K.explorer.focus.uuid,
+    action = function()
+      dot.widget.explorer.focus()
+    end,
+  })
+  .implement({
+    uuid = K.explorer.focus_cwd.uuid,
+    action = function()
+      dot.widget.explorer.focus_cwd()
+    end,
+  })
+  .implement({
+    uuid = K.explorer.focus_workspace.uuid,
+    action = function()
+      dot.widget.explorer.focus_workspace()
+    end,
+  })
+  .implement({
+    uuid = K.explorer.hide.uuid,
+    action = function()
+      dot.widget.explorer.hide()
+    end,
+  })
+  .implement({
+    uuid = K.explorer.refresh.uuid,
+    action = function()
+      dot.widget.explorer.refresh()
+    end,
+  })
+  .implement({
+    uuid = K.explorer.reveal.uuid,
+    action = function()
+      dot.widget.explorer.reveal()
+    end,
+  })
+  .implement({
+    uuid = K.explorer.toggle.uuid,
+    tabtype = ark.vim.tab.Types.NORMAL,
+    action = function()
+      dot.widget.explorer.toggle()
+    end,
+  })
+
 --[find] -------------------------------------------------------------------------------------------
 command
   .implement({
