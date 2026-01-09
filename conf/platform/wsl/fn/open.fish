ca18358d15a12af0e80e1585910a0f423e41d7e8
guanghechen <me.guanghechen@gmail.com>
2026-01-09 10:04:17 +0800

    :sparkles: feat(wsl): support URL and file:// protocol in open/start functions

--- conf/platform/wsl/fn/open.fish
+++ conf/platform/wsl/fn/open.fish
@@ -1,9 +1,18 @@
 function open
     if test (count $argv) -eq 0
-        echo "Usage: open <path>"
+        echo "Usage: open <url|path>"
         return 1
     end
 
-    set winpath (wslpath -w $argv[1])
-    explorer.exe "$winpath"
+    set -l target $argv[1]
+    if string match -qr '^https?://' -- $target
+        explorer.exe "$target"
+    else if string match -qr '^file://' -- $target
+        set -l filepath (string replace 'file://' '' -- $target)
+        set -l winpath (wslpath -w $filepath)
+        explorer.exe "file://$winpath"
+    else
+        set -l winpath (wslpath -w $target)
+        explorer.exe "$winpath"
+    end
 end
--- conf/platform/wsl/fn/start.fish
+++ conf/platform/wsl/fn/start.fish
@@ -1,9 +1,18 @@
 function start
     if test (count $argv) -eq 0
-        echo "Usage: start <path>"
+        echo "Usage: start <url|path>"
         return 1
     end
 
-    set winpath (wslpath -w $argv[1])
-    cmd.exe /c start "" "$winpath"
+    set -l target $argv[1]
+    if string match -qr '^https?://' -- $target
+        cmd.exe /c start "" "$target"
+    else if string match -qr '^file://' -- $target
+        set -l filepath (string replace 'file://' '' -- $target)
+        set -l winpath (wslpath -w $filepath)
+        cmd.exe /c start "" "file://$winpath"
+    else
+        set -l winpath (wslpath -w $target)
+        cmd.exe /c start "" "$winpath"
+    end
 end
