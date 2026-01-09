bff2badd6b3288191839e1d367e51f21a1ae11ef
guanghechen <me.guanghechen@gmail.com>
2026-01-09 12:53:44 +0800

    :sparkles: feat(alias): add tagent abbreviation for tmux agent template

--- conf/alias.fish
+++ conf/alias.fish
@@ -75,6 +75,7 @@ end
 alias tnew='tmux new -s' # Create a new tmux session
 alias tkill='tmux kill-session -t' # Kill a tmux session
 alias tkill-all='tmux list-sessions | awk -F: \'{print $1}\' | xargs -I {} tmux kill-session -t "{}"'
+abbr -a tagent "bash $HOME/.config/tmux/templates/agent.sh"
 abbr -a tbtop "bash $HOME/.config/tmux/templates/btop.sh"
 abbr -a tcap "tmux capture-pane -ep -t %"
 alias tdetach='tmux detach' # Detach from the session
