3190be419a5ecb4466adc72ef96432d503ef417b
guanghechen <me.guanghechen@gmail.com>
2026-01-09 10:46:56 +0800

    :art: improve(alias): clean up abbr formatting and add tcap placeholder
    - Remove unnecessary spacing in abbr definitions
    - Add % placeholder to tcap for easier pane targeting

--- conf/alias.fish
+++ conf/alias.fish
@@ -76,6 +76,7 @@ alias tnew='tmux new -s' # Create a new tmux session
 alias tkill='tmux kill-session -t' # Kill a tmux session
 alias tkill-all='tmux list-sessions | awk -F: \'{print $1}\' | xargs -I {} tmux kill-session -t "{}"'
 abbr -a tbtop "bash $HOME/.config/tmux/templates/btop.sh"
+abbr -a tcap "tmux capture-pane -ep -t %"
 alias tdetach='tmux detach' # Detach from the session
 alias tattach='tmux attach -t' # Attach to a session
 alias tdetach-others='tmux detach -a' # Detach other clients from the session
