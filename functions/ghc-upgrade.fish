5aac1ad37b13232df241c360d39f3587d2e3902d
guanghechen <me.guanghechen@gmail.com>
2026-01-30 18:33:14 +0800

    :recycle: refactor(ghc-upgrade): use dynamic config root and edition detection
    - Support XDG_CONFIG_HOME and GHC_CONFIG_ROOT for config path
    - Detect edition via node script when available
    - Add win edition support with PowerShell
    - Simplify logic with switch statement

--- functions/ghc-upgrade.fish
+++ functions/ghc-upgrade.fish
@@ -1,12 +1,35 @@
 function ghc-upgrade
-    if set -q SSH_CONNECTION; or set -q SSH_CLIENT; or set -q SSH_TTY
-        bash $HOME/.config/guanghechen/nix-remote/setup.sh
-        return
-    else
-        if test "$GHC_ENV_PLATFORM" = osx
-            bash $HOME/.config/guanghechen/osx/setup.sh
+    set -l config_root "$HOME/.config/guanghechen"
+    if set -q XDG_CONFIG_HOME
+        set config_root "$XDG_CONFIG_HOME/guanghechen"
+    end
+    if set -q GHC_CONFIG_ROOT
+        set config_root "$GHC_CONFIG_ROOT"
+    end
+
+    set -l edition ""
+    if type -q node
+        set edition (node "$config_root/config/_shared/setting.mjs" --print-edition 2>/dev/null | string trim)
+    end
+
+    if test -z "$edition"
+        if set -q SSH_CONNECTION; or set -q SSH_CLIENT; or set -q SSH_TTY
+            set edition nix-remote
+        else if test (uname) = Darwin
+            set edition osx
         else
-            bash $HOME/.config/guanghechen/nix/setup.sh
+            set edition nix
         end
+    end
+
+    switch $edition
+        case nix-remote
+            bash "$config_root/nix-remote/setup.sh"
+        case osx
+            bash "$config_root/osx/setup.sh"
+        case win
+            pwsh -File "$config_root/win/setup.ps1"
+        case '*'
+            bash "$config_root/nix/setup.sh"
     end
 end
