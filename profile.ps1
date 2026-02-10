7da7396d76822c9a34bf1356c97d98f33da21ebc
guanghechen <me.guanghechen@gmail.com>
2026-02-10 15:58:21 +0800

    :recycle: refactor(prompt): inline starship config into profile.ps1
    remove separate prompt.ps1 file

--- profile.ps1
+++ profile.ps1
@@ -18,6 +18,8 @@ if (Test-Path $localEnvPath) {
 
 . "$env:XDG_CONFIG_HOME\pwsh\env.ps1"
 . "$env:XDG_CONFIG_HOME\pwsh\app.ps1"
-. "$env:XDG_CONFIG_HOME\pwsh\prompt.ps1"
+# Starship prompt
+$env:STARSHIP_CONFIG = "$env:XDG_CONFIG_HOME\starship\pwsh.toml"
+Invoke-Expression (&starship init powershell)
 ## Setup fnm
 fnm env --use-on-cd --shell power-shell | Out-String | Invoke-Expression
--- prompt.ps1
+++
@@ -1,2 +0,0 @@
-$env:STARSHIP_CONFIG = "$env:XDG_CONFIG_HOME\starship\pwsh.toml"
-Invoke-Expression (&starship init powershell)
