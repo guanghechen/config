b4014bf838bae7869f64ae9dec6846ef3000080f
guanghechen <me.guanghechen@gmail.com>
2026-02-10 14:41:33 +0800

    :wrench: chore(init): use dedicated starship config and remove duplicate header
    - Use ~/.config/starship/yazi.toml instead of local starship.toml
    - Remove redundant user@host header widget (already shown by starship)

--- init.lua
+++ init.lua
@@ -1,16 +1,3 @@
-local function get_starship_config()
-	local sep = package.config:sub(1, 1)
-	local home = os.getenv("HOME")
-	if home then
-		return home .. sep .. ".config" .. sep .. "yazi" .. sep .. "starship.toml"
-	end
-	local appdata = os.getenv("APPDATA")
-	if appdata then
-		return appdata .. sep .. "yazi" .. sep .. "config" .. sep .. "starship.toml"
-	end
-	return nil
-end
-
 require("git"):setup()
 require("full-border"):setup({
 	type = ui.Border.ROUNDED,
@@ -18,15 +5,8 @@ require("full-border"):setup({
 require("starship"):setup({
 	hide_flags = false,
 	flags_after_prompt = true,
-	config_file = get_starship_config(),
+	config_file = "~/.config/starship/yazi.toml",
 })
-
-Header:children_add(function()
-	if ya.target_family() ~= "unix" then
-		return ""
-	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
-end, 500, Header.LEFT)
 
 function Status:name()
 	local h = self._current.hovered
