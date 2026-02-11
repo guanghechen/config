9ed28901c429cfd03ee1fc3e4d9c218a1573c056
guanghechen <me.guanghechen@gmail.com>
2026-02-11 16:00:15 +0800

    :bug: fix(setup/nvim): use NVIM_APPNAME for nvchad and fix line continuation style

--- setup/nix/app/nvim.bash
+++ setup/nix/app/nvim.bash
@@ -3,19 +3,19 @@
 source "$HOME/.config/guanghechen/setup/nix/bot/env.bash"
 
 (
-  cd "$HOME/.config/nvim" \
-    && conda activate "$GHC_APP_PYTHON_ENV" \
-    && fnm use "$GHC_APP_EDITION_NODE" \
-    && bash rust/build.sh \
-    && nvim --headless -u ./init-update.lua
+  cd "$HOME/.config/nvim" &&
+    conda activate "$GHC_APP_PYTHON_ENV" &&
+    fnm use "$GHC_APP_EDITION_NODE" &&
+    bash rust/build.sh &&
+    nvim --headless -u ./init-update.lua
 )
 
 if [ -d "$HOME/.config/nvim-nvchad/" ]; then
   (
-    cd "$HOME/.config/nvim-nvchad" \
-      && conda activate "$GHC_APP_PYTHON_ENV" \
-      && fnm use "$GHC_APP_EDITION_NODE" \
-      && bash rust/build.sh \
-      && nvchad --headless -u ./init-update.lua
+    cd "$HOME/.config/nvim-nvchad" &&
+      conda activate "$GHC_APP_PYTHON_ENV" &&
+      fnm use "$GHC_APP_EDITION_NODE" &&
+      bash rust/build.sh &&
+      NVIM_APPNAME=nvim-nvchad nvim --headless -u ./init-update.lua
   )
 fi
