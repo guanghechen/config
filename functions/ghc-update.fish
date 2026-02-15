e171e3dc94c3b4fcd6b8decfb25eebfd5b03ba53
guanghechen <me.guanghechen@gmail.com>
2026-02-15 15:32:10 +0800

    :recycle: refactor(fish): replace ghc-update function with kit repo sync abbr

--- functions/ghc-update.fish
+++
@@ -1,8 +0,0 @@
-function ghc-update
-    set script_path "$XDG_CONFIG_HOME/guanghechen/cli/sync-xdg-config.mjs"
-    if test -f "$script_path"
-        node "$script_path"
-    else
-        printf "\e[91m  Cannot find %s.\e[0m\n" "$script_path"
-    end
-end
