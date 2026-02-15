3aad7034af6fbe4bf25d211eedd18418cf43ee08
guanghechen <me.guanghechen@gmail.com>
2026-02-15 15:33:29 +0800

    :recycle: refactor(ghc-update): replace node script with kit repo sync

--- functions/ghc-update.ps1
+++ functions/ghc-update.ps1
@@ -1,8 +1,3 @@
 function ghc-update {
-  $script_path = Join-Path $env:XDG_CONFIG_HOME "guanghechen/cli/sync-xdg-config.mjs"
-  if (Test-Path $script_path) {
-    node $script_path
-  } else {
-    Write-Host " Cannot find $script_path." -ForegroundColor Red
-  }
+  kit repo sync
 }
