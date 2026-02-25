c7279607a180780bd920dec897eaafa8493923d3
guanghechen <me.guanghechen@gmail.com>
2026-02-25 12:08:14 +0800

    :bug: fix(keymap): prevent smart tab hang on stalled suggestions

--- conf/keymap.fish
+++ conf/keymap.fish
@@ -1,7 +1,23 @@
 function __smart_tab
     if commandline --showing-suggestion
+        set -l max_steps 200
+        set -l step 0
+        set -l last_buffer (commandline)
+
         while commandline --showing-suggestion
+            if test $step -ge $max_steps
+                break
+            end
+            set step (math $step + 1)
+
             commandline -f forward-single-char
+
+            set -l current_buffer (commandline)
+            if test "$current_buffer" = "$last_buffer"
+                # Prevent infinite loop when forward-single-char makes no progress.
+                break
+            end
+            set last_buffer "$current_buffer"
 
             set -l last_char (commandline --cut-at-cursor | string sub -s -1)
             if test "$last_char" = " "
