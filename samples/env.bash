1e68e61d6111379ea4220600eb7ed5f62c7541ad
guanghechen <me.guanghechen@gmail.com>
2026-02-25 12:20:54 +0800

    :wrench: chore(env): expand local environment variable template

--- samples/env.bash
+++ samples/env.bash
@@ -1,2 +1,14 @@
-# Sample local env (copy to local/env.bash)
-# export API_KEY="..."
+# # Local
+#
+# export d_wireshark_vsc_log=""
+# export GHC_WINDOWS_USERNAME=""
+#
+# # lemon
+# export YOZ_SERVER_PORT=7171
+#
+# export ANTHROPIC_AUTH_TOKEN="token"
+# export GEMINI_API_KEY="token"
+# export OPENAI_CODEX_AUTH_TOKEN="token"
+#
+# export GOOGLE_CLOUD_PROJECT="project"
+# export TAVILY_API_KEY="tavily api key"
