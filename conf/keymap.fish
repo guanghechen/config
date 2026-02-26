f95eb141dbd1e824601594086d5331e25f6297f6
guanghechen <me.guanghechen@gmail.com>
2026-02-26 10:20:21 +0800

    :wrench: chore(keymap): simplify autosuggestion key bindings

--- conf/keymap.fish
+++ conf/keymap.fish
@@ -1,39 +1,7 @@
-function __smart_tab
-    if commandline --showing-suggestion
-        set -l max_steps 200
-        set -l step 0
-        set -l last_buffer (commandline)
-
-        while commandline --showing-suggestion
-            if test $step -ge $max_steps
-                break
-            end
-            set step (math $step + 1)
-
-            commandline -f forward-single-char
-
-            set -l current_buffer (commandline)
-            if test "$current_buffer" = "$last_buffer"
-                # Prevent infinite loop when forward-single-char makes no progress.
-                break
-            end
-            set last_buffer "$current_buffer"
-
-            set -l last_char (commandline --cut-at-cursor | string sub -s -1)
-            if test "$last_char" = " "
-                commandline -f backward-delete-char
-                break
-            end
-        end
-        return
-    end
-    commandline -f complete
-end
-
-bind -M insert \cy accept-autosuggestion
-bind -M default \cy accept-autosuggestion
-bind -M insert tab __smart_tab
-bind -M insert ctrl-i __smart_tab
+bind -M insert ctrl-y accept-autosuggestion
+bind -M default ctrl-y accept-autosuggestion
+bind -M insert ctrl-w forward-bigword
+bind -M default ctrl-w forward-bigword
 
 for mode in default insert
     bind --mode $mode \e\[70\;6u fzf-file # Ctrl+Shift+F
