69b18741b75be2c7e5a26046ce7bf6ae4264e23a
guanghechen <me.guanghechen@gmail.com>
2026-03-05 13:56:13 +0800

    :bug: fix(markdown): preserve ordered list marker styles

--- src/container/markdown/renderer/list.tsx
+++ src/container/markdown/renderer/list.tsx
@@ -2,6 +2,22 @@ import type { List } from '@yozora/ast'
 import React from 'react'
 import { NodesRenderer } from '../NodesRenderer'
 
+function resolveListStyleType(orderType: List['orderType']): React.CSSProperties['listStyleType'] {
+  switch (orderType) {
+    case 'A':
+      return 'upper-alpha'
+    case 'a':
+      return 'lower-alpha'
+    case 'I':
+      return 'upper-roman'
+    case 'i':
+      return 'lower-roman'
+    case '1':
+    default:
+      return 'decimal'
+  }
+}
+
 /**
  * Render `list`.
  *
@@ -15,9 +31,12 @@ export class ListRenderer extends React.Component<List> {
     const { ordered, orderType, start, children } = this.props
 
     if (ordered) {
+      const listStyleType = resolveListStyleType(orderType)
+
       return (
         <ol
-          className="yozora-list yozora-list-order list-outside list-decimal p-0 m-0 mb-4 pl-4"
+          className="yozora-list yozora-list-order list-outside p-0 m-0 mb-4 pl-4"
+          style={{ listStyleType }}
           type={orderType}
           start={start}
         >
