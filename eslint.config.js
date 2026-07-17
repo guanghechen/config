187777d182b316232e5f696306abb2fc517a245b
guanghechen <me.guanghechen@gmail.com>
2026-07-17 15:05:15 +0800

    :arrow_up: chore(deps): upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,29 @@ const configs = [
     ignores: ['.vscode/', '**/__tmp__/', '**/doc/', '**/example/', '.prettierrc'],
   },
   ...genConfigs({ tsconfigPath: './tsconfig.eslint.json' }),
-  reactHooks.configs['recommended-latest'],
+  reactHooks.configs.flat['recommended-latest'],
   {
-    files: ['src/**/*.{ts,cts,mts}'],
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+  {
+    files: ['**/*.{ts,cts,mts,tsx,ctsx,mtsx}'],
     rules: {
+      '@stylistic/arrow-spacing': ['error', { after: true, before: true }],
+      '@stylistic/type-annotation-spacing': [
+        'error',
+        { after: true, before: false, overrides: { arrow: 'ignore' } },
+      ],
       '@typescript-eslint/no-unused-vars': 'off',
       'no-plusplus': 'off',
       'no-unused-vars': 'off',
+    },
+  },
+  {
+    files: ['**/*.d.ts'],
+    rules: {
+      'spaced-comment': 'off',
     },
   },
   {
