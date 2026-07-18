f2b273681f811cb05ada3f7d635558d46a26e7e0
guanghechen <me.guanghechen@gmail.com>
2026-07-18 12:27:03 +0800

    :recycle: refactor(popup): colocate popup stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './popup/style.css'
 import { App } from './popup/App'
 
 const rootElement = document.getElementById('root')
