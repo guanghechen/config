af46083ee6cc06c7d22eb734c50c91b8ae27ccb6
guanghechen <me.guanghechen@gmail.com>
2026-07-30 11:04:13 +0800

    :bug: fix(setup): stop self-updating pnpm

--- setup/nix/env/pnpm.bash
+++ setup/nix/env/pnpm.bash
@@ -3,8 +3,7 @@
 source "$HOME/.config/guanghechen/setup/nix/bot/env.bash"
 
 if command -v pnpm &>/dev/null; then
-  printf "\e[96m  [setup pnpm] pnpm is already installed, upgrading...\e[0m\n"
-  pnpm self-update
+  printf "\e[93m  [setup pnpm] pnpm is already installed. (skipped)\e[0m\n"
 else
   printf "\e[96m  [setup pnpm] installing pnpm...\e[0m\n"
   curl -fsSL https://get.pnpm.io/install.sh | sh -
--- setup/win/env/pnpm.ps1
+++ setup/win/env/pnpm.ps1
@@ -1,6 +1,5 @@
 if (Get-Command pnpm -ErrorAction SilentlyContinue) {
-  Write-Host "  [setup pnpm] pnpm is already installed, upgrading..." -ForegroundColor Cyan
-  pnpm self-update
+  Write-Host "  [setup pnpm] pnpm is already installed. (skipped)" -ForegroundColor Yellow
 } else {
   Write-Host "  [setup pnpm] installing pnpm..." -ForegroundColor Cyan
   iwr https://get.pnpm.io/install.ps1 -useb | iex
