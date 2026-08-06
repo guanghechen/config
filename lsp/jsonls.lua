b0209385ad42b0becfd60ee65ae92ed697253049
guanghechen <me.guanghechen@gmail.com>
2026-08-06 12:48:36 +0800

    :wrench: chore(lsp): curate json schema associations

--- lsp/jsonls.lua
+++ lsp/jsonls.lua
@@ -25,6 +25,108 @@ local function on_init(client, config)
   era.m.lsp.event.on_init(client, config)
 end
 
+local schemas = {
+  -- Web / JavaScript / TypeScript
+  {
+    fileMatch = { "package.json" },
+    url = "https://www.schemastore.org/package.json",
+  },
+  {
+    fileMatch = { "tsconfig*.json" },
+    url = "https://www.schemastore.org/tsconfig.json",
+  },
+  {
+    fileMatch = { "jsconfig.json" },
+    url = "https://www.schemastore.org/jsconfig.json",
+  },
+  {
+    fileMatch = { ".eslintrc", ".eslintrc.json", ".eslintrc.yml", ".eslintrc.yaml" },
+    url = "https://www.schemastore.org/eslintrc.json",
+  },
+  {
+    fileMatch = { "biome.json", "biome.jsonc" },
+    url = "https://biomejs.dev/schemas/latest/schema.json",
+  },
+  {
+    fileMatch = { "deno.json", "deno.jsonc" },
+    url = "https://raw.githubusercontent.com/denoland/deno/main/cli/schemas/config-file.v1.json",
+  },
+  {
+    fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+    url = "https://www.schemastore.org/babelrc.json",
+  },
+  {
+    fileMatch = { ".prettierrc", ".prettierrc.json", ".prettierrc.yml", ".prettierrc.yaml" },
+    url = "https://www.schemastore.org/prettierrc.json",
+  },
+  {
+    fileMatch = { ".stylelintrc", ".stylelintrc.yml", ".stylelintrc.yaml", ".stylelintrc.json" },
+    url = "https://www.schemastore.org/stylelintrc.json",
+  },
+  {
+    fileMatch = { ".htmlvalidate.json" },
+    url = "https://html-validate.org/schemas/config.json",
+  },
+  {
+    fileMatch = {
+      ".cspell.json",
+      "cspell.json",
+      ".cSpell.json",
+      "cSpell.json",
+      "cspell.config.json",
+      "cspell.config.yaml",
+      "cspell.config.yml",
+      "cspell.yaml",
+      "cspell.yml",
+    },
+    url = "https://raw.githubusercontent.com/streetsidesoftware/cspell/main/packages/cspell-types/cspell.schema.json",
+  },
+  {
+    fileMatch = { "manifest.json", "*.webmanifest" },
+    url = "https://www.schemastore.org/web-manifest-combined.json",
+  },
+
+  -- C / C++
+  {
+    fileMatch = { "CMakePresets.json", "CMakeUserPresets.json" },
+    url = "https://raw.githubusercontent.com/Kitware/CMake/master/Help/manual/presets/schema.json",
+  },
+  {
+    fileMatch = { "compile_commands.json" },
+    url = "https://www.schemastore.org/compile-commands.json",
+  },
+
+  -- Rust
+  {
+    fileMatch = { "rust-project.json" },
+    url = "https://www.schemastore.org/rust-project.json",
+  },
+
+  -- C#
+  {
+    fileMatch = { "appsettings.json", "appsettings.*.json" },
+    url = "https://www.schemastore.org/appsettings.json",
+  },
+  {
+    fileMatch = { "global.json" },
+    url = "https://www.schemastore.org/global.json",
+  },
+  {
+    fileMatch = { "launchsettings.json" },
+    url = "https://www.schemastore.org/launchsettings.json",
+  },
+  {
+    fileMatch = { "dotnet-tools.json" },
+    url = "https://www.schemastore.org/dotnet-tools.json",
+  },
+
+  -- Go
+  {
+    fileMatch = { ".golangci.yml", ".golangci.yaml", ".golangci.toml", ".golangci.json" },
+    url = "https://golangci-lint.run/jsonschema/golangci.jsonschema.json",
+  },
+}
+
 ---@type vim.lsp.Config
 return {
   capabilities = era.m.lsp.event.get_capabilities(),
@@ -39,20 +141,7 @@ return {
       format = {
         enable = true,
       },
-      schemas = {
-        {
-          description = "ESLint configuration files",
-          fileMatch = { ".eslintrc", ".eslintrc.json", ".eslintrc.yml", ".eslintrc.yaml" },
-          name = ".eslintrc",
-          url = "https://www.schemastore.org/eslintrc.json",
-        },
-        {
-          description = "NPM configuration file",
-          fileMatch = { "package.json" },
-          name = "package.json",
-          url = "https://www.schemastore.org/package.json",
-        },
-      },
+      schemas = schemas,
       suggest = {
         json5 = true,
       },
