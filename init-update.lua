a81e485139d1df403b8846cf183282060f9fc444
guanghechen <me.guanghechen@gmail.com>
2026-08-19 22:53:56 +0800

    :wrench: chore(update): stop installing Mason packages

--- init-update.lua
+++ init-update.lua
@@ -6,23 +6,14 @@ dot.setup_context(storage)
 
 require("era.plugin")
 
--- Install missing plugins first, then run mason installation
 local Action = require("era.m.plugin.action")
 Action.install():map(function()
-  -- Explicitly load mason plugin after installation completes
-  require("era.m.plugin.loader").load("mason.nvim")
-
-  local action = require("era.plugin.mason")
-  stl.stdout.info("[guanghechen]", "Installing Mason packages...")
-  action.install_all(false, function()
-    stl.stdout.success("[guanghechen]", "All Mason packages installed successfully!")
-    local ok, err = pcall(function()
-      vim.cmd("qa!")
-    end)
-
-    if not ok then
-      stl.stdout.error("[guanghechen]", "Error during exit: " .. tostring(err))
-      os.exit(1)
-    end
+  local ok, err = pcall(function()
+    vim.cmd("qa!")
   end)
+
+  if not ok then
+    stl.stdout.error("[guanghechen]", "Error during exit: " .. tostring(err))
+    os.exit(1)
+  end
 end)
